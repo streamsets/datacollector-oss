e3ce2b7a8b9398e961d8552852e7f58d24049e5d
Alejandro Abdelnur <tucu@streamsets.com>
2014-11-20 10:06:22 -0800

    Adding random error processor

---
+++ stage-lib/src/main/java/com/streamsets/pipeline/lib/basics/RandomErrorProcessor.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.lib.basics;
+
+import com.streamsets.pipeline.api.Batch;
+import com.streamsets.pipeline.api.Record;
+import com.streamsets.pipeline.api.StageDef;
+import com.streamsets.pipeline.api.StageException;
+import com.streamsets.pipeline.api.base.SingleLaneProcessor;
+
+import java.util.Iterator;
+import java.util.Random;
+
+@StageDef(name = "randomErrorProcessor", version = "1.0.0", label = "Random Error",
+          description = "Randomly 50/50 sends records to error")
+public class RandomErrorProcessor extends SingleLaneProcessor {
+  private Random random;
+
+  @Override
+  protected void init() throws StageException {
+    random = new Random();
+  }
+
+  @Override
+  public void process(Batch batch, SingleLaneBatchMaker batchMaker) throws
+      StageException {
+    Iterator<Record> it = batch.getRecords();
+    while (it.hasNext()) {
+      if (random.nextBoolean()) {
+        batchMaker.addRecord(it.next());
+      } else {
+        getContext().toError(it.next(), "Random error");
+      }
+    }
+  }
+
+}
