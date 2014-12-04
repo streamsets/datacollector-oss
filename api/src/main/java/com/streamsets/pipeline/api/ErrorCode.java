52aca18a276a52c7c62ca9b5f907b4d194d1d4b5
Alejandro Abdelnur <tucu@streamsets.com>
2014-12-04 13:30:25 -0800

    Javadoc to ErrorCode

--- api/src/main/java/com/streamsets/pipeline/api/ErrorCode.java
+++ api/src/main/java/com/streamsets/pipeline/api/ErrorCode.java
@@ -18,12 +18,58 @@
 package com.streamsets.pipeline.api;
 
 /**
- * Implementations must be enums for Localization to kick in.
+ * Typifies error codes with built-in localization support. Pipeline exceptions use error codes.
+ * <p/>
+ * Error code implementations are typically enums, i.e.:
+ * <p/>
+ * <pre>
+ *
+ * public enum BaseError implements ErrorCode {
+ *   BASE_0001("Stage '{}', there should be 1 output lane but there are '{}'");
+ *
+ *   private final String msg;
+ *
+ *   BaseError(String msg) {
+ *     this.msg = msg;
+ *   }
+ *
+ *   @Override
+ *   public String getCode() {
+ *     return name();
+ *   }
+ *
+ *   @Override
+ *   public String getMessage() {
+ *     return msg;
+ *   }
+ *
+ * }
+ * </pre>
+ * <p/>
+ * Built in localization looks for a <code>Properties</code> based <code>ResourceBundle</code> matching the
+ * <code>ErrorCode</code> implementation. The * <code>ErrorCode</code>'s <code>code</code> is used as the key within the
+ * <code>ResourceBundle</code>. If the bundle is not available, or the key is not defined within the bundle, the
+ * <code>ErrorCode</code>'s <code>message</code> will be used.
+ * <p/>
+ * Typically, the message can be a template, using <code>{}</code> as positional placeholders for values.
+ * {@link com.streamsets.pipeline.api.StageException} take an <code>ErrorCode</code> plus variable arguments on its
+ * constructors and generates the exception message using the <code>ErrorCode</code> message as template then variable
+ * argument as the values for it.
  */
 public interface ErrorCode {
 
+  /**
+   * Returns the error code.
+   *
+   * @return the error code.
+   */
   public String getCode();
 
+  /**
+   * Returns the built-in default message for the error code.
+   *
+   * @return the default message template
+   */
   public String getMessage();
 
 }
