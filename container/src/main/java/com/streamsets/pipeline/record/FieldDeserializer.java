007198ed21cf90257fdd1b5b82f46e2fff0d14d7
Alejandro Abdelnur <tucu@streamsets.com>
2014-12-03 21:47:39 -0800

    Updating field deserializer to work recursively

--- container/src/main/java/com/streamsets/pipeline/record/FieldDeserializer.java
+++ container/src/main/java/com/streamsets/pipeline/record/FieldDeserializer.java
@@ -23,16 +23,48 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.streamsets.pipeline.api.Field;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class FieldDeserializer extends JsonDeserializer<Field> {
 
   @Override
+  @SuppressWarnings("unchecked")
   public Field deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
-    Map map = jp.readValueAs(Map.class);
-    Field.Type type = Field.Type.valueOf((String) map.get("type"));
-    Object value = map.get("value");
-    return Field.create(type, value);
+    return parse(jp.readValueAs(Map.class));
+  }
+
+  @SuppressWarnings("unchecked")
+  private Field parse(Map<String, Object> map) {
+    Field field = null;
+    if (map != null) {
+      Field.Type type = Field.Type.valueOf((String) map.get("type"));
+      Object value = map.get("value");
+      if (value != null) {
+        switch (type) {
+          case MAP:
+            Map<String, Field> fMap = new HashMap<>();
+            for (Map.Entry<String, Object> entry : ((Map<String, Object>) value).entrySet()) {
+              fMap.put(entry.getKey(), parse((Map<String, Object>) entry.getValue()));
+            }
+            value = fMap;
+            break;
+          case LIST:
+            List<Field> fList = new ArrayList<>();
+            for (Map<String, Object> element : (List<Map<String, Object>>) value) {
+              fList.add(parse(element));
+            }
+            value = fList;
+            break;
+          default:
+            break;
+        }
+      }
+      field = Field.create(type, value);
+    }
+    return field;
   }
 
 }
