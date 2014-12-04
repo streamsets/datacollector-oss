bb44b0c522ab8b0b91568eb12a987b43a98f96a6
Alejandro Abdelnur <tucu@streamsets.com>
2014-12-04 14:09:55 -0800

    Removing sandbox from master branch

--- sandbox/README.txt
+++
@@ -1,3 +0,0 @@
-StreamSets sandbox project
-
-For the purposes of playing/learning 3rd party libraries only.
--- sandbox/webapp/src/main/webapp/build/app/common/_declarations.js
+++
@@ -1,5 +0,0 @@
-/**
- * Module definition for common components used by other all other app modules.
- */
-
-angular.module('pipelineAgentApp.common', []);
