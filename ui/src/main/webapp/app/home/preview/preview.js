39c1309dd139e7e87f0a1d11fb0a4d1e67d8604a
Madhukar Devaraju <madhu@streamsets.com>
2014-12-08 13:23:15 -0800

    [ui] Added support to show stage errors in preview mode.

--- ui/src/main/webapp/app/home/preview/preview.js
+++ ui/src/main/webapp/app/home/preview/preview.js
@@ -190,6 +190,7 @@ angular
         } else if (outputLane && stageOutput.output[outputLane] && stageOutput.output) {
           stagePreviewData.output = stageOutput.output[outputLane];
           stagePreviewData.errorRecords = stageOutput.errorRecords;
+          stagePreviewData.stageErrors = stageOutput.stageErrors;
         }
       });
 
