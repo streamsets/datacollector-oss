eaa741e4fca0ae3179f8414745a8b34fb88d2d2f
Alejandro Abdelnur <tucu@streamsets.com>
2014-12-16 14:45:18 +0100

    LocalizableMessage warnings happen once per proccess startup

--- api/src/main/java/com/streamsets/pipeline/container/LocalizableMessage.java
+++ api/src/main/java/com/streamsets/pipeline/container/LocalizableMessage.java
@@ -8,13 +8,21 @@ package com.streamsets.pipeline.container;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class LocalizableMessage implements LocalizableString {
   private static final Logger LOG = LoggerFactory.getLogger(LocalizableMessage.class);
   private static final Object[] NULL_ONE_ARG = {null};
+
+  private static final Set<String> MISSING_BUNDLE_WARNS =
+      Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
+  private static final Set<String> MISSING_KEY_WARNS =
+      Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
 
   private final ClassLoader classLoader;
   private final String bundle;
@@ -51,10 +59,16 @@ public class LocalizableMessage implements LocalizableString {
           if (rb.containsKey(id)) {
             templateToUse = rb.getString(id);
           } else {
-            LOG.warn("ResourceBundle '{}' does not have key '{}'", bundle, id);
+            if (!MISSING_KEY_WARNS.contains(bundle + " " + id)) {
+              MISSING_KEY_WARNS.add(bundle + " " + id);
+              LOG.warn("ResourceBundle '{}' does not have key '{}' via ClassLoader '{}'", bundle, id, classLoader);
+            }
           }
         } catch (MissingResourceException ex) {
-          LOG.warn("ResourceBundle '{}' not found via ClassLoader '{}'", bundle, classLoader);
+          if (!MISSING_BUNDLE_WARNS.contains(bundle)) {
+            MISSING_BUNDLE_WARNS.add(bundle);
+            LOG.warn("ResourceBundle '{}' not found via ClassLoader '{}'", bundle, classLoader);
+          }
         }
       }
     }
