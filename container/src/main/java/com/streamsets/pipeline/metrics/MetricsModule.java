542c753ada199020595496872bba9d4787e78bc7
Alejandro Abdelnur <tucu@streamsets.com>
2014-12-19 14:41:05 +0100

    Adding codehale JMX modelers to /jmx output

--- container/src/main/java/com/streamsets/pipeline/metrics/MetricsModule.java
+++ container/src/main/java/com/streamsets/pipeline/metrics/MetricsModule.java
@@ -6,17 +6,28 @@
 package com.streamsets.pipeline.metrics;
 
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jvm.BufferPoolMetricSet;
+import com.codahale.metrics.jvm.FileDescriptorRatioGauge;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
+import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
 import dagger.Module;
 import dagger.Provides;
 
 import javax.inject.Singleton;
+import java.lang.management.ManagementFactory;
 
 @Module(library = true)
 public class MetricsModule {
 
   @Provides @Singleton MetricRegistry provideMetrics() {
-    return new MetricRegistry();
+    MetricRegistry metrics = new MetricRegistry();
+    metrics.register("jvm.memory", new MemoryUsageGaugeSet());
+    metrics.register("jvm.garbage", new GarbageCollectorMetricSet());
+    metrics.register("jvm.threads", new ThreadStatesGaugeSet());
+    metrics.register("jvm.files", new FileDescriptorRatioGauge());
+    metrics.register("jvm.buffers", new BufferPoolMetricSet(ManagementFactory.getPlatformMBeanServer()));
+    return metrics;
   }
-
 
 }
