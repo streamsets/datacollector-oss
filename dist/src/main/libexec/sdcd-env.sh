21e0e1c262297dc1c2aba68fab5ad372995ee7cb
Alejandro Abdelnur <tucu@streamsets.com>
2015-01-13 13:02:44 -0800

    Adding initd scripts and removing alternate SDC_HOME support from the distro (for b1)

--- dist/src/main/libexec/sdcd-env.sh
+++ dist/src/main/libexec/sdcd-env.sh
@@ -6,23 +6,30 @@
 #
 
 #
-# This script is sourced when using the bin/sdc script to start the data collector
+# This script is sourced when using the data collector initd scripts
+# Refer to the initd/README file for details
 #
+
+# user that will run the data collector, it must exist in the system
+#
+export SDC_USER=sdc
+
+# group of the user that will run the data collector, it must exist in the system
+#
+export SDC_GROUP=sdc
 
 # directory where the data collector will store pipelines and their runtime information
 #
-#export SDC_DATA=/var/run/sdc
+export SDC_DATA=/var/run/sdc
 
 # directory where the data collector write its logs
 #
-#export SDC_LOG=/var/log/sdc
+export SDC_LOG=/var/log/sdc
 
 # directory where the data collector will read its configuration
 #
-#export SDC_CONF=/etc/sdc
+export SDC_CONF=/etc/sdc
 
-export SDC_PRE_CLASSPATH=${SDC_PRE_CLASSPATH}
-
-export SDC_POST_CLASSPATH=${SDC_POST_CLASSPATH}
-
+# JVM options for the data collector process
+#
 export SDC_JAVA_OPTS="-Xmx1024m ${SDC_JAVA_OPTS}"
