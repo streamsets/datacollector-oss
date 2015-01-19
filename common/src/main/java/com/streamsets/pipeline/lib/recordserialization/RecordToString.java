8348244613513ec0729b7d7172370fde069e19c7
Alejandro Abdelnur <tucu@streamsets.com>
2015-01-19 15:38:08 -0800

    Record to string serialization interface and dummy impls for CSV/TSV/JSON/XML

---
+++ common/src/main/java/com/streamsets/pipeline/lib/recordserialization/RecordToString.java
@@ -0,0 +1,21 @@
+/**
+ * (c) 2014 StreamSets, Inc. All rights reserved. May not
+ * be copied, modified, or distributed in whole or part without
+ * written consent of StreamSets, Inc.
+ */
+package com.streamsets.pipeline.lib.recordserialization;
+
+import com.streamsets.pipeline.api.Record;
+
+import java.util.Map;
+
+public interface RecordToString {
+
+  //if set to an EMPTY MAP it means the string representation supports nested structures and the resulting data
+  // should mimic the field structure with the exact same names. for impls like CVS, TSV an InvalidArgumentException
+  // should be thrown in that case
+  public void setFieldPathToNameMapping(Map<String, String> fieldPathToNameMap);
+
+  public String toString(Record record);
+
+}
---
+++ common/src/main/java/com/streamsets/pipeline/lib/recordserialization/XmlRecordToString.java
@@ -0,0 +1,24 @@
+/**
+ * (c) 2014 StreamSets, Inc. All rights reserved. May not
+ * be copied, modified, or distributed in whole or part without
+ * written consent of StreamSets, Inc.
+ */
+package com.streamsets.pipeline.lib.recordserialization;
+
+import com.streamsets.pipeline.api.Record;
+
+import java.util.Map;
+
+public class XmlRecordToString implements RecordToString {
+
+  @Override
+  public void setFieldPathToNameMapping(Map<String, String> fieldPathToNameMap) {
+
+  }
+
+  @Override
+  public String toString(Record record) {
+    return "<record/>";
+  }
+
+}
