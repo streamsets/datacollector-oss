81c2a152b9d35e2ef0d36bfe78b2f0e52bfa317a
Madhukar Devaraju <madhu@streamsets.com>
2015-01-20 10:40:27 -0800

    [ui] Fixed bug in loading default pipeline based on local strorage

--- ui/src/main/webapp/app/home/home.js
+++ ui/src/main/webapp/app/home/home.js
@@ -333,7 +333,10 @@ angular
 
         //Determine Active Config based on localStorage or based on last status updated config.
         if($scope.$storage.activeConfigInfo && $scope.$storage.activeConfigInfo.name) {
-          $scope.activeConfigInfo = $scope.$storage.activeConfigInfo;
+          var localStorageConfigInfoName = $scope.$storage.activeConfigInfo;
+          $scope.activeConfigInfo = $scope.$storage.activeConfigInfo = _.find($scope.pipelines, function(pipelineDefn) {
+            return pipelineDefn.name === localStorageConfigInfoName;
+          });
         } else if(pipelineStatus && pipelineStatus.name) {
           $scope.activeConfigInfo = _.find($scope.pipelines, function(pipelineDefn) {
             return pipelineDefn.name === pipelineStatus.name;
