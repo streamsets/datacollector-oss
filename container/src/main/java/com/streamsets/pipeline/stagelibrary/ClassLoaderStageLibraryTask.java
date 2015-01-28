e34cb55e4feb0b87f583a2ad79812a0c660924dd
Madhukar Devaraju <madhu@streamsets.com>
2015-01-28 13:29:42 -0800

    SDC-89 Expression property & tooltip suggestions
    Fixed below 2 issues :
    
    6. Can we add Name and Value labels for the Constants? And change the
    ghost text from Key => Name? (key-value is quite geeky. )
    [This is built in support from UI. Will assign to madhu to fix this]
    7. Required Fields tooltip. I think you wanted help with this one.
    Um...How's about: "Records that do not include these fields are dropped
    from the pipeline"?
    [This must be fixed in the UI, assigning to madhu]

--- container/src/main/java/com/streamsets/pipeline/stagelibrary/ClassLoaderStageLibraryTask.java
+++ container/src/main/java/com/streamsets/pipeline/stagelibrary/ClassLoaderStageLibraryTask.java
@@ -128,7 +128,7 @@ public class ClassLoaderStageLibraryTask extends AbstractTask implements StageLi
 
   //Group name needs to be empty for UI to show the config in General Group.
   private static final ConfigDefinition REQUIRED_FIELDS_CONFIG = new ConfigDefinition(
-      ConfigDefinition.REQUIRED_FIELDS, ConfigDef.Type.MODEL, "Required Fields", "Record required fields",
+      ConfigDefinition.REQUIRED_FIELDS, ConfigDef.Type.MODEL, "Required Fields", "Records that do not include these fields are dropped from the pipeline",
       null, false, "", null, new ModelDefinition(ModelType.FIELD_SELECTOR_MULTI_VALUED, null, null, null, null, null), "",
       new String[] {}, Integer.MAX_VALUE);
 
