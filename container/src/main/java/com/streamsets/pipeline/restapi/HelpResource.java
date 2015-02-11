5dec25507d35ed516721d3d702c2999039255be7
Alejandro Abdelnur <tucu@streamsets.com>
2015-02-11 13:50:58 -0800

    Help reference REST API, helprefs are defined in etc/helprefs.properties

---
+++ container/src/main/java/com/streamsets/pipeline/restapi/HelpResource.java
@@ -0,0 +1,43 @@
+/**
+ * (c) 2014 StreamSets, Inc. All rights reserved. May not
+ * be copied, modified, or distributed in whole or part without
+ * written consent of StreamSets, Inc.
+ */
+package com.streamsets.pipeline.restapi;
+
+import com.streamsets.pipeline.main.RuntimeInfo;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Properties;
+
+@Path("/v1/helpref")
+public class HelpResource {
+  private final RuntimeInfo runtimeInfo;
+
+  @Inject
+  public HelpResource(RuntimeInfo runtimeInfo) {
+    this.runtimeInfo = runtimeInfo;
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getHelpRefs() throws IOException {
+    File helpRefFile = new File(runtimeInfo.getConfigDir(), "helpref.properties");
+    try (Reader reader = new FileReader(helpRefFile)) {
+      Properties helpRefs = new Properties();
+      helpRefs.load(reader);
+      return Response.ok().type(MediaType.APPLICATION_JSON).entity(helpRefs).build();
+    }
+  }
+
+
+}
