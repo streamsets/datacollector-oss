31ddc03403ded0349415af33913640f4b5c3a4fd
Alejandro Abdelnur <tucu@streamsets.com>
2015-02-10 16:25:29 -0800

    Adding error record EL functions

--- common/src/main/java/com/streamsets/pipeline/el/ELRecordSupport.java
+++ common/src/main/java/com/streamsets/pipeline/el/ELRecordSupport.java
@@ -39,24 +39,109 @@ public class ELRecordSupport {
     return value;
   }
 
-  public static String getId() {
-    String id = null;
+  private enum HeaderProperty {
+    ID, STAGE_CREATOR, STAGES_PATH, ERROR_STAGE, ERROR_CODE, ERROR_MESSAGE, ERROR_DATA_COLLECTOR_ID,
+    ERROR_PIPELINE_NAME, ERROR_TIME
+  }
+
+  private static <T> T getFromHeader(HeaderProperty prop) {
+    Object value = null;
     Record record = (Record) ELEvaluator.getVariablesInScope().getContextVariable(RECORD_CONTEXT_VAR);
     if (record != null) {
-      id = record.getHeader().getSourceId();
+      switch (prop) {
+        case ID:
+          value = record.getHeader().getSourceId();
+          break;
+        case STAGE_CREATOR:
+          value = record.getHeader().getStageCreator();
+          break;
+        case STAGES_PATH:
+          value = record.getHeader().getStagesPath();
+          break;
+        case ERROR_STAGE:
+          value = record.getHeader().getErrorStage();
+          break;
+        case ERROR_CODE:
+          value = record.getHeader().getErrorCode();
+          break;
+        case ERROR_MESSAGE:
+          value = record.getHeader().getErrorMessage();
+          break;
+        case ERROR_DATA_COLLECTOR_ID:
+          value = record.getHeader().getErrorDataCollectorId();
+          break;
+        case ERROR_PIPELINE_NAME:
+          value = record.getHeader().getErrorPipelineName();
+          break;
+        case ERROR_TIME:
+          value = record.getHeader().getErrorTimestamp();
+          break;
+      }
     }
-    return id;
+    return (T) value;
+  }
+
+  public static String getId() {
+    return getFromHeader(HeaderProperty.ID);
+  }
+
+  public static String getStageCreator() {
+    return getFromHeader(HeaderProperty.STAGE_CREATOR);
+  }
+
+  public static String getStagesPath() {
+    return getFromHeader(HeaderProperty.STAGES_PATH);
+  }
+
+  public static String getErrorStage() {
+    return getFromHeader(HeaderProperty.ERROR_STAGE);
+  }
+
+  public static String getErrorCode() {
+    return getFromHeader(HeaderProperty.ERROR_CODE);
+  }
+
+  public static String getErrorMessage() {
+    return getFromHeader(HeaderProperty.ERROR_MESSAGE);
+  }
+
+  public static String getErrorDataCollectorId() {
+    return getFromHeader(HeaderProperty.ERROR_DATA_COLLECTOR_ID);
+  }
+
+  public static String getErrorPipelineName() {
+    return getFromHeader(HeaderProperty.ERROR_PIPELINE_NAME);
+  }
+
+  public static long getErrorTime() {
+    return getFromHeader(HeaderProperty.ERROR_TIME);
   }
 
   private static final Method RECORD_TYPE;
   private static final Method RECORD_VALUE;
   private static final Method RECORD_ID;
+  private static final Method RECORD_STAGE_CREATOR;
+  private static final Method RECORD_STAGES_PATH;
+  private static final Method ERROR_STAGE;
+  private static final Method ERROR_CODE;
+  private static final Method ERROR_MESSAGE;
+  private static final Method ERROR_DATA_COLLECTOR_ID;
+  private static final Method ERROR_PIPELINE_NAME;
+  private static final Method ERROR_TIME;
 
   static {
     try {
       RECORD_TYPE = ELRecordSupport.class.getMethod("getType", String.class);
       RECORD_VALUE = ELRecordSupport.class.getMethod("getValue", String.class);
       RECORD_ID = ELRecordSupport.class.getMethod("getId");
+      RECORD_STAGE_CREATOR = ELRecordSupport.class.getMethod("getStageCreator");
+      RECORD_STAGES_PATH = ELRecordSupport.class.getMethod("getStagesPath");
+      ERROR_STAGE = ELRecordSupport.class.getMethod("getErrorStage");
+      ERROR_CODE = ELRecordSupport.class.getMethod("getErrorCode");
+      ERROR_MESSAGE = ELRecordSupport.class.getMethod("getErrorMessage");
+      ERROR_DATA_COLLECTOR_ID = ELRecordSupport.class.getMethod("getErrorDataCollectorId");
+      ERROR_PIPELINE_NAME = ELRecordSupport.class.getMethod("getErrorPipelineName");
+      ERROR_TIME = ELRecordSupport.class.getMethod("getErrorTime");
     } catch (Exception ex) {
       throw new RuntimeException(ex);
     }
@@ -67,6 +152,14 @@ public class ELRecordSupport {
     elEvaluator.registerFunction("record", "type", RECORD_TYPE);
     elEvaluator.registerFunction("record", "value", RECORD_VALUE);
     elEvaluator.registerFunction("record", "id", RECORD_ID);
+    elEvaluator.registerFunction("record", "creator", RECORD_STAGE_CREATOR);
+    elEvaluator.registerFunction("record", "path", RECORD_STAGES_PATH);
+    elEvaluator.registerFunction("error", "stage", ERROR_STAGE);
+    elEvaluator.registerFunction("error", "code", ERROR_CODE);
+    elEvaluator.registerFunction("error", "message", ERROR_MESSAGE);
+    elEvaluator.registerFunction("error", "collectorId", ERROR_DATA_COLLECTOR_ID);
+    elEvaluator.registerFunction("error", "pipeline", ERROR_PIPELINE_NAME);
+    elEvaluator.registerFunction("error", "time", ERROR_TIME);
     for (Field.Type type : Field.Type.values()) {
       elEvaluator.registerConstant(type.toString(), type);
     }
--- common/src/test/java/com/streamsets/pipeline/el/TestELRecordSupport.java
+++ common/src/test/java/com/streamsets/pipeline/el/TestELRecordSupport.java
@@ -22,6 +22,8 @@ public class TestELRecordSupport {
 
     Record.Header header = Mockito.mock(Record.Header.class);
     Mockito.when(header.getSourceId()).thenReturn("id");
+    Mockito.when(header.getStageCreator()).thenReturn("creator");
+    Mockito.when(header.getStagesPath()).thenReturn("path");
     Record record = Mockito.mock(Record.class);
     Mockito.when(record.getHeader()).thenReturn(header);
     Mockito.when(record.get(Mockito.eq(""))).thenReturn(Field.create(1));
@@ -33,6 +35,37 @@ public class TestELRecordSupport {
     Assert.assertTrue(eval.eval(variables, "${record:value('') eq 1}", Boolean.class));
     Assert.assertNull(eval.eval(variables, "${record:value('/x')}"));
     Assert.assertEquals("id", eval.eval(variables, "${record:id()}"));
+    Assert.assertEquals("creator", eval.eval(variables, "${record:creator()}"));
+    Assert.assertEquals("path", eval.eval(variables, "${record:path()}"));
   }
+
+  @Test
+  public void testErrorRecordFunctions() throws Exception {
+    ELEvaluator eval = new ELEvaluator();
+    ELRecordSupport.registerRecordFunctions(eval);
+
+
+    ELEvaluator.Variables variables = new ELEvaluator.Variables();
+
+    Record.Header header = Mockito.mock(Record.Header.class);
+    Mockito.when(header.getErrorStage()).thenReturn("stage");
+    Mockito.when(header.getErrorCode()).thenReturn("code");
+    Mockito.when(header.getErrorMessage()).thenReturn("message");
+    Mockito.when(header.getErrorDataCollectorId()).thenReturn("collector");
+    Mockito.when(header.getErrorPipelineName()).thenReturn("pipeline");
+    Mockito.when(header.getErrorTimestamp()).thenReturn(10l);
+    Record record = Mockito.mock(Record.class);
+    Mockito.when(record.getHeader()).thenReturn(header);
+
+    ELRecordSupport.setRecordInContext(variables, record);
+
+    Assert.assertEquals("stage", eval.eval(variables, "${error:stage()}"));
+    Assert.assertEquals("code", eval.eval(variables, "${error:code()}"));
+    Assert.assertEquals("message", eval.eval(variables, "${error:message()}"));
+    Assert.assertEquals("collector", eval.eval(variables, "${error:collectorId()}"));
+    Assert.assertEquals("pipeline", eval.eval(variables, "${error:pipeline()}"));
+    Assert.assertEquals(10l, eval.eval(variables, "${error:time()}"));
+  }
+
 
 }
