139775d00418f1d1a3b9049a4ad65e0c956506c6
Madhukar Devaraju <madhu@streamsets.com>
2015-02-15 22:25:40 -0800

    [ui] Integration Tests

--- ui/src/main/webapp/e2e-tests/protractor.conf.js
+++ ui/src/main/webapp/e2e-tests/protractor.conf.js
@@ -1,3 +1,7 @@
+var env = {
+  baseUrl: 'http://localhost:18630/'
+};
+
 exports.config = {
   allScriptsTimeout: 11000,
 
@@ -9,11 +13,29 @@ exports.config = {
     'browserName': 'chrome'
   },
 
-  baseUrl: 'http://localhost:8080/',
+  baseUrl: env.baseUrl,
 
   framework: 'jasmine',
 
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
+  },
+
+  onPrepare: function() {
+    browser.driver.get(env.baseUrl + 'login.html');
+
+    browser.driver.findElement(by.id('usernameId')).sendKeys('admin');
+    browser.driver.findElement(by.id('passwordId')).sendKeys('admin');
+    browser.driver.findElement(by.id('loginId')).click();
+
+    // Login takes some time, so wait until it's done.
+    // For the test app's login, we know it's done when it redirects to
+    // index.html.
+    browser.driver.wait(function() {
+      return browser.driver.getCurrentUrl().then(function(url) {
+        //return /index/.test(url);
+        return true;
+      });
+    });
   }
 };
--- ui/src/main/webapp/e2e-tests/scenarios.js
+++ ui/src/main/webapp/e2e-tests/scenarios.js
@@ -1,4 +1,9 @@
-describe('My app', function() {
+describe('StreamSets Data Collector App', function() {
+
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+    browser.manage().timeouts().pageLoadTimeout(10000);
+  });
 
   browser.get('/');
 
@@ -13,11 +18,52 @@ describe('My app', function() {
       browser.get('/');
     });
 
-
     it('should render home view when user navigates to /', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/Use this document as a way to quickly start any new project./);
+      expect(browser.getTitle()).toEqual('StreamSets Data Collector');
     });
+
+    it('should show create pipeline button when no pipeline list is empty', function() {
+      element.all(by.repeater('pipeline in pipelines')).then(function(pipelines) {
+        expect(pipelines.length).toEqual(0);
+      });
+
+      element.all(by.css('.create-pipeline-btn')).then(function(elements) {
+        var createBtnElement = elements[elements.length - 1];
+        createBtnElement.isDisplayed().then(function (isVisible) {
+          expect(isVisible).toBeTruthy();
+        });
+      });
+    });
+
+
+    it('should be able to create new pipeline', function() {
+      element.all(by.css('.create-pipeline-btn')).then(function(elements) {
+        var createBtnElement = elements[elements.length - 1];
+        createBtnElement.click();
+
+        //Fill Create Pipeline Modal Dialog values
+        element(by.model('newConfig.name')).sendKeys('samplePipeline');
+        element(by.model('newConfig.description')).sendKeys('pipeline description');
+        element(by.css('button[type="submit"]')).click();
+
+
+        browser.sleep(500);
+
+        //Toggle Library Pane
+        element(by.css('[ng-click="toggleLibraryPanel()"]')).click();
+
+        //Test pipeline creation by checking list of pipelines
+        element.all(by.repeater('pipeline in pipelines')).then(function(pipelines) {
+          expect(pipelines.length).toEqual(1);
+          expect(pipelines[0].element(by.binding('pipeline.name')).getText()).toEqual('samplePipeline');
+        });
+
+      });
+    });
+
+
+
+
 
   });
 });
