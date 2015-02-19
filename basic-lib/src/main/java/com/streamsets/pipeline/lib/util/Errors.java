bc0d3f2d2375a0b950284d91189af1a210073d92
Alejandro Abdelnur <tucu@streamsets.com>
2015-02-19 13:45:12 -0800

    SDC-204. basic-lib util Errors refactoring

---
+++ basic-lib/src/main/java/com/streamsets/pipeline/lib/util/Errors.java
@@ -0,0 +1,29 @@
+/**
+ * (c) 2014 StreamSets, Inc. All rights reserved. May not
+ * be copied, modified, or distributed in whole or part without
+ * written consent of StreamSets, Inc.
+ */
+package com.streamsets.pipeline.lib.util;
+
+import com.streamsets.pipeline.api.ErrorCode;
+
+public enum Errors implements ErrorCode {
+  RECORD_00("Error parsing a JSON string '{}': {}"),
+  ;
+
+  private final String msg;
+  Errors(String msg) {
+    this.msg = msg;
+  }
+
+  @Override
+  public String getCode() {
+    return name();
+  }
+
+  @Override
+  public String getMessage() {
+    return msg;
+  }
+
+}
