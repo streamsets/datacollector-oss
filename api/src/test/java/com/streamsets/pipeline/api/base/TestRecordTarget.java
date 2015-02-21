324f2e81c12c01c4c2c13a411dcace73655cecbd
Alejandro Abdelnur <tucu@streamsets.com>
2015-02-20 16:54:51 -0800

    SDC-213. Introduce OnRecordError system option for all stages, part 4 (testcases for base API)

--- api/src/test/java/com/streamsets/pipeline/api/base/TestRecordTarget.java
+++ api/src/test/java/com/streamsets/pipeline/api/base/TestRecordTarget.java
@@ -5,16 +5,15 @@
  */
 package com.streamsets.pipeline.api.base;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.streamsets.pipeline.api.Batch;
 import com.streamsets.pipeline.api.BatchMaker;
 import com.streamsets.pipeline.api.ErrorCode;
 import com.streamsets.pipeline.api.OnRecordError;
+import com.streamsets.pipeline.api.Target;
+import com.streamsets.pipeline.api.Record;
 import com.streamsets.pipeline.api.Stage;
 import com.streamsets.pipeline.api.StageException;
-import com.streamsets.pipeline.api.Processor;
-import com.streamsets.pipeline.api.Record;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -23,26 +22,24 @@ import org.mockito.Mockito;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TestRecordProcessor {
+public class TestRecordTarget {
 
   @Test
-  public void testProcessor() throws Exception {
+  public void testTarget() throws Exception {
     Record record1 = Mockito.mock(Record.class);
     Record record2 = Mockito.mock(Record.class);
     Batch batch = Mockito.mock(Batch.class);
     Mockito.when(batch.getRecords()).thenReturn(ImmutableSet.of(record1, record2).iterator());
-    final BatchMaker batchMaker = Mockito.mock(BatchMaker.class);
 
     final List<Record> got = new ArrayList<Record>();
 
-    Processor processor = new RecordProcessor() {
+    Target target = new RecordTarget() {
       @Override
-      protected void process(Record record, BatchMaker bm) throws StageException {
-        Assert.assertEquals(batchMaker, bm);
+      protected void write(Record record) throws StageException {
         got.add(record);
       }
     };
-    processor.process(batch, batchMaker);
+    target.write(batch);
 
     Assert.assertEquals(2, got.size());
     Assert.assertEquals(record1, got.get(0));
@@ -70,13 +67,12 @@ public class TestRecordProcessor {
     final Record record2 = Mockito.mock(Record.class);
     Batch batch = Mockito.mock(Batch.class);
     Mockito.when(batch.getRecords()).thenReturn(ImmutableSet.of(record1, record2).iterator());
-    final BatchMaker batchMaker = Mockito.mock(BatchMaker.class);
 
     final List<Record> got = new ArrayList<Record>();
 
-    Processor processor = new RecordProcessor() {
+    Target target = new RecordTarget() {
       @Override
-      protected void process(Record record, BatchMaker bm) throws StageException {
+      protected void write(Record record) throws StageException {
         if (record == record1) {
           got.add(record);
         } else {
@@ -86,11 +82,11 @@ public class TestRecordProcessor {
     };
 
     Stage.Info info = Mockito.mock(Stage.Info.class);
-    Processor.Context context = Mockito.mock(Processor.Context.class);
+    Target.Context context = Mockito.mock(Target.Context.class);
     Mockito.when(context.getOnErrorRecord()).thenReturn(OnRecordError.DISCARD);
-    processor.init(info, context);
-    processor.process(batch, batchMaker);
-    processor.destroy();
+    target.init(info, context);
+    target.write(batch);
+    target.destroy();
     Assert.assertEquals(1, got.size());
     Assert.assertEquals(record1, got.get(0));
     Mockito.verify(context, Mockito.never()).toError((Record) Mockito.any(), (Exception) Mockito.any());
@@ -106,9 +102,9 @@ public class TestRecordProcessor {
 
     final List<Record> got = new ArrayList<Record>();
 
-    Processor processor = new RecordProcessor() {
+    Target target = new RecordTarget() {
       @Override
-      protected void process(Record record, BatchMaker bm) throws StageException {
+      protected void write(Record record) throws StageException {
         if (record == record1) {
           got.add(record);
         } else {
@@ -118,11 +114,11 @@ public class TestRecordProcessor {
     };
 
     Stage.Info info = Mockito.mock(Stage.Info.class);
-    Processor.Context context = Mockito.mock(Processor.Context.class);
+    Target.Context context = Mockito.mock(Target.Context.class);
     Mockito.when(context.getOnErrorRecord()).thenReturn(OnRecordError.TO_ERROR);
-    processor.init(info, context);
-    processor.process(batch, batchMaker);
-    processor.destroy();
+    target.init(info, context);
+    target.write(batch);
+    target.destroy();
     Assert.assertEquals(1, got.size());
     Assert.assertEquals(record1, got.get(0));
     ArgumentCaptor<Record> captor = ArgumentCaptor.forClass(Record.class);
@@ -140,9 +136,9 @@ public class TestRecordProcessor {
 
     final List<Record> got = new ArrayList<Record>();
 
-    Processor processor = new RecordProcessor() {
+    Target target = new RecordTarget() {
       @Override
-      protected void process(Record record, BatchMaker bm) throws StageException {
+      protected void write(Record record) throws StageException {
         if (record == record1) {
           got.add(record);
         } else {
@@ -152,10 +148,10 @@ public class TestRecordProcessor {
     };
 
     Stage.Info info = Mockito.mock(Stage.Info.class);
-    Processor.Context context = Mockito.mock(Processor.Context.class);
+    Target.Context context = Mockito.mock(Target.Context.class);
     Mockito.when(context.getOnErrorRecord()).thenReturn(OnRecordError.STOP_PIPELINE);
-    processor.init(info, context);
-    processor.process(batch, batchMaker);
+    target.init(info, context);
+    target.write(batch);
   }
 
   private void testOnErrorHandlingOtherException(OnRecordError onRecordError) throws Exception {
@@ -167,18 +163,18 @@ public class TestRecordProcessor {
 
     final List<Record> got = new ArrayList<Record>();
 
-    Processor processor = new RecordProcessor() {
+    Target target = new RecordTarget() {
       @Override
-      protected void process(Record record, BatchMaker bm) throws StageException {
+      protected void write(Record record) throws StageException {
         throw new RuntimeException();
       }
     };
 
     Stage.Info info = Mockito.mock(Stage.Info.class);
-    Processor.Context context = Mockito.mock(Processor.Context.class);
+    Target.Context context = Mockito.mock(Target.Context.class);
     Mockito.when(context.getOnErrorRecord()).thenReturn(onRecordError);
-    processor.init(info, context);
-    processor.process(batch, batchMaker);
+    target.init(info, context);
+    target.write(batch);
   }
 
   @Test(expected = Exception.class)
