9ab738ded5f85495e5361bcf440e6d4a21c6ad48
Alejandro Abdelnur <tucu@streamsets.com>
2015-02-23 14:38:11 -0800

    SDC-249. Refactor stages to use the DStage classes. NullTarget

---
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/destination/devnull/NullDTarget.java
@@ -0,0 +1,30 @@
+/**
+ * (c) 2014 StreamSets, Inc. All rights reserved. May not
+ * be copied, modified, or distributed in whole or part without
+ * written consent of StreamSets, Inc.
+ */
+package com.streamsets.pipeline.stage.destination.devnull;
+
+import com.streamsets.pipeline.api.ErrorStage;
+import com.streamsets.pipeline.api.GenerateResourceBundle;
+import com.streamsets.pipeline.api.HideConfig;
+import com.streamsets.pipeline.api.StageDef;
+import com.streamsets.pipeline.api.Target;
+import com.streamsets.pipeline.configurablestage.DTarget;
+
+@StageDef(
+    version = "1.0.0",
+    label = "Trash",
+    description = "Discards records",
+    icon="trash.png"
+)
+@HideConfig(requiredFields = true, onErrorRecord = true)
+@ErrorStage
+@GenerateResourceBundle
+public class NullDTarget extends DTarget {
+
+  @Override
+  protected Target createTarget() {
+    return new NullTarget();
+  }
+}
--- basic-lib/src/main/java/com/streamsets/pipeline/stage/destination/devnull/NullTarget.java
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/destination/devnull/NullTarget.java
@@ -6,22 +6,9 @@
 package com.streamsets.pipeline.stage.destination.devnull;
 
 import com.streamsets.pipeline.api.Batch;
-import com.streamsets.pipeline.api.ErrorStage;
-import com.streamsets.pipeline.api.GenerateResourceBundle;
-import com.streamsets.pipeline.api.HideConfig;
-import com.streamsets.pipeline.api.StageDef;
 import com.streamsets.pipeline.api.StageException;
 import com.streamsets.pipeline.api.base.BaseTarget;
 
-@GenerateResourceBundle
-@StageDef(
-    version = "1.0.0",
-    label = "Trash",
-    description = "Discards records",
-    icon="trash.png"
-)
-@HideConfig(requiredFields = true, onErrorRecord = true)
-@ErrorStage
 public class NullTarget extends BaseTarget {
 
   @Override
