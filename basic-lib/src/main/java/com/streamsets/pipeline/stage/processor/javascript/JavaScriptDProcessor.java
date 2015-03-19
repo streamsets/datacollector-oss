3f13ff72a5b1ec748f255128dd9248dffbff9333
Madhukar Devaraju <madhu@streamsets.com>
2015-03-18 17:22:13 -0700

    Added sample script code for JavaScript Processor

--- basic-lib/src/main/java/com/streamsets/pipeline/stage/processor/javascript/JavaScriptDProcessor.java
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/processor/javascript/JavaScriptDProcessor.java
@@ -41,7 +41,7 @@ public class JavaScriptDProcessor extends DProcessor {
   @ConfigDef(
       required = true,
       type = ConfigDef.Type.TEXT,
-      defaultValue = "",
+      defaultValue = "/**\n * Sample JavaScript code\n */\n\nfor(var i=0; i < records.length; i++) {\n  var record = records[i];\n  \n  /*\n  //Change Field Value\n  try { \n  \trecord.set('/a', Field.create(record.get('/b').getValueAsLong() + \n                                  record.get('/c').getValueAsLong()));\n  } catch(e) {\n    //Ignore Exception to avoid sending record to error\n  }\n  \n  //Add Primitive Field\n  record.set('/simpleField', Field.create('field string Value'));\n  \n  //Add Map Field\n  record.set('/mapField', Field.create({\n    mapField1 : Field.create('map field value 1'),\n    mapField2 : Field.create('map field value 2')\n  }));\n  \n  //Add Array Field\n  var list = new java.util.ArrayList();\n  list.add(Field.create('sdf'));\n  record.set('/arrayField', Field.create(list));\n  \n  //To write record to error sink\n  //err.write(record, 'Error Message');\n  */\n  \n  out.write(record);\n}  \n  \n",
       label = "Script",
       displayPosition = 20,
       group = "JAVASCRIPT",
