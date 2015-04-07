cd88e993198c6a68a4c0d71495dfc4e3f05b38ec
Brock Noland <brockn@gmail.com>
2015-04-07 10:26:44 -0700

    SDC-375 - Jenkins and Gerrit integration
    Change-Id: I730a780a61be5daf1f632e3c6c138e78c1933a6c
    Reviewed-on: http://review.streamsets.net:8080/29
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Brock Noland <brock@streamsets.com>

--- dev-support/ci-run-all-unit-tests.sh
+++ dev-support/ci-run-all-unit-tests.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
-set -x
 export JAVA_HOME=/opt/jdk-7
 export M2_HOME=/opt/maven-3.2
 export NODE_HOME=/opt/node-0.10
 export PATH=$JAVA_HOME/bin:$M2_HOME/bin:$MAVEN_HOME/bin:$NODE_HOME/bin:$PATH
+set -x
+env
+git log --oneline | head
+git show
 mvn clean install -Drelease -DskipTests
 mvn package -Drelease -fae -Pall-libs
