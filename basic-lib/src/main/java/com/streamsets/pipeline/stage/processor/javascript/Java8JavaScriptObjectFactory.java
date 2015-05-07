944926e9156fa65caf43fba5d81ba5478a84b26c
Adam Kunicki <adam@streamsets.com>
2015-05-07 15:31:04 -0700

    SDC-584. Fix failing javascript tests on Nashorn (JDK8) engine.
    Change-Id: Ib216ffecc80ce43955da0c5ce19a3d187254ae7f
    Reviewed-on: https://review.streamsets.net/202
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Brock Noland <brock@streamsets.com>

--- basic-lib/src/main/java/com/streamsets/pipeline/stage/processor/javascript/Java8JavaScriptObjectFactory.java
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/processor/javascript/Java8JavaScriptObjectFactory.java
@@ -2,126 +2,64 @@ package com.streamsets.pipeline.stage.processor.javascript;
 
 import com.streamsets.pipeline.api.Field;
 import com.streamsets.pipeline.stage.processor.scripting.ScriptObjectFactory;
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
 import javax.script.ScriptEngine;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.math.BigDecimal;
-import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class Java8JavaScriptObjectFactory extends ScriptObjectFactory {
-  private static final Class NASHHORN_SCRIPT_ENGINE_CLASS;
-  private static final Class CONTEXT_CLASS;
-  private static final Class GLOBAL_CLASS;
-  private static final Class SCRIPT_OBJECT_CLASS;
-  private static final Class NATIVE_ARRAY_CLASS;
-  private static final java.lang.reflect.Field NASHHORN_SCRIPT_ENGINE_CONTEXT_FIELD;
-  private static final Method CONTEXT_NEW_GLOBAL_METHOD;
-  private static final Method CONTEXT_SET_GLOBAL_METHOD;
-  private static final Method GLOBAL_NEW_OBJECT_METHOD;
-  private static final Method SCRIPT_OBJECT_ENTRYSET_METHOD;
-  private static final Method SCRIPT_OBJECT_PUT_METHOD;
-  private static final Method NATIVE_ARRAY_AS_OBJECT_ARRAY_METHOD;
-  private static final Constructor ARRAY_CONSTRUCTOR;
-  private static final String REFLECTION_ERROR_MESSAGE = "Error performing reflection on " +
-    System.getProperty("java.version") + ". Please report: ";
-  private final Object context;
-  private final Object global;
-
-  // we are doing this reflection voodoo because the Java compile does not let you use sun....internal packages
-  // PRRRRRRRRHHHHHHHHHHH Oracle.
-  static {
-    try {
-      //
-      NASHHORN_SCRIPT_ENGINE_CLASS = ClassLoader.getSystemClassLoader().loadClass(
-        "jdk.nashorn.api.scripting.NashornScriptEngine");
-      CONTEXT_CLASS = ClassLoader.getSystemClassLoader().loadClass(
-        "jdk.nashorn.internal.runtime.Context");
-      SCRIPT_OBJECT_CLASS = ClassLoader.getSystemClassLoader().loadClass(
-        "jdk.nashorn.internal.runtime.ScriptObject");
-      NATIVE_ARRAY_CLASS = ClassLoader.getSystemClassLoader().loadClass(
-        "jdk.nashorn.internal.objects.NativeArray");
-      GLOBAL_CLASS = ClassLoader.getSystemClassLoader().loadClass(
-        "jdk.nashorn.internal.objects.Global");
-      NASHHORN_SCRIPT_ENGINE_CONTEXT_FIELD = NASHHORN_SCRIPT_ENGINE_CLASS.getDeclaredField("nashornContext");
-      NASHHORN_SCRIPT_ENGINE_CONTEXT_FIELD.setAccessible(true);
-      CONTEXT_SET_GLOBAL_METHOD = CONTEXT_CLASS.getMethod("setGlobal", SCRIPT_OBJECT_CLASS);
-      CONTEXT_NEW_GLOBAL_METHOD = CONTEXT_CLASS.getMethod("newGlobal");
-      GLOBAL_NEW_OBJECT_METHOD = GLOBAL_CLASS.getMethod("newObject");
-      SCRIPT_OBJECT_ENTRYSET_METHOD = SCRIPT_OBJECT_CLASS.getMethod("entrySet");
-      SCRIPT_OBJECT_PUT_METHOD = SCRIPT_OBJECT_CLASS.getMethod("put", Object.class, Object.class, Boolean.TYPE);
-      NATIVE_ARRAY_AS_OBJECT_ARRAY_METHOD = NATIVE_ARRAY_CLASS.getMethod("asObjectArray");
-      ARRAY_CONSTRUCTOR = NATIVE_ARRAY_CLASS.getDeclaredConstructor(Object[].class);
-      ARRAY_CONSTRUCTOR.setAccessible(true);
-    } catch (Exception ex) {
-      throw new RuntimeException(REFLECTION_ERROR_MESSAGE + ex, ex);
-    }
-  }
   public Java8JavaScriptObjectFactory(ScriptEngine engine) {
     super(engine);
-    try {
-      context = NASHHORN_SCRIPT_ENGINE_CONTEXT_FIELD.get(engine);
-      global = CONTEXT_NEW_GLOBAL_METHOD.invoke(context);
-      CONTEXT_SET_GLOBAL_METHOD.invoke(null, global);
-    } catch(Exception ex) {
-      throw new RuntimeException(REFLECTION_ERROR_MESSAGE + ex, ex);
-    }
   }
+
   @Override
   public Object createArray(List elements) {
-    try {
-      return ARRAY_CONSTRUCTOR.newInstance(new Object[]{elements.toArray(new Object[elements.size()])});
-    } catch (Exception ex) {
-      throw new RuntimeException(REFLECTION_ERROR_MESSAGE + ex, ex);
-    }
+    // Nashorn can pass Java objects to JavaScript directly.
+    return elements;
   }
 
   @Override
   public Object createMap() {
-    try {
-      return GLOBAL_NEW_OBJECT_METHOD.invoke(global);
-    } catch (Exception ex) {
-      throw new RuntimeException(REFLECTION_ERROR_MESSAGE + ex, ex);
-    }
+    // Nashorn can pass Java objects to JavaScript directly.
+    return new HashMap();
   }
 
   @Override
   public void putInMap(Object obj, Object key, Object value) {
-    try {
-      SCRIPT_OBJECT_PUT_METHOD.invoke(obj, key, value, true);
-    } catch (Exception ex) {
-      throw new RuntimeException(REFLECTION_ERROR_MESSAGE + ex, ex);
-    }
+      ((ScriptObjectMirror) obj).put(key.toString(), value);
   }
 
   @Override
   protected Field scriptToField(Object scriptObject) {
     Field field;
     if (scriptObject != null) {
-      if (NATIVE_ARRAY_CLASS.isInstance(scriptObject)) {
-        try {
-          Object[] values = (Object[]) NATIVE_ARRAY_AS_OBJECT_ARRAY_METHOD.invoke(scriptObject);
-          List<Field> fieldArray = new ArrayList<>(values.length);
-          for (Object element : values) {
-            fieldArray.add(scriptToField(element));
+      if (scriptObject instanceof ScriptObjectMirror) {
+        ScriptObjectMirror scriptMap = (ScriptObjectMirror) scriptObject;
+
+        boolean isArray = false;
+        if (((ScriptObjectMirror) scriptObject).isArray()) {
+          isArray = true;
+        }
+
+        if (isArray) {
+          List<Field> fields = new ArrayList<>(scriptMap.entrySet().size());
+          for (Map.Entry<String, Object> entry : scriptMap.entrySet()) {
+            fields.add(scriptToField(entry.getValue()));
           }
-          field = Field.create(fieldArray);
-        } catch (Exception ex) {
-          throw new RuntimeException(ex);
+          field = Field.create(fields);
+        } else {
+          Map<String, Field> fields = new LinkedHashMap<>();
+          for (Map.Entry<String, Object> entry : scriptMap.entrySet()) {
+            fields.put(entry.getKey(), scriptToField(entry.getValue()));
+          }
+          field = Field.create(fields);
         }
-      } else if (SCRIPT_OBJECT_CLASS.isInstance(scriptObject)) {
-        Map<Object, Object> scriptMap = new ScriptObjectMap(scriptObject);
-        Map<String, Field> fieldMap = new LinkedHashMap<>();
-        for (Map.Entry<Object, Object> entry : scriptMap.entrySet()) {
-          fieldMap.put(entry.getKey().toString(), scriptToField(entry.getValue()));
-        }
-        field = Field.create(fieldMap);
       } else {
         field = convertPrimitiveObject(scriptObject);
       }
@@ -129,21 +67,6 @@ public class Java8JavaScriptObjectFactory extends ScriptObjectFactory {
       field = Field.create((String)null);
     }
     return field;
-  }
-
-  private static class ScriptObjectMap extends AbstractMap<Object, Object> {
-    private final Object scriptMap;
-    private ScriptObjectMap(Object scriptMap) {
-      this.scriptMap = scriptMap;
-    }
-    @Override
-    public Set<Entry<java.lang.Object, java.lang.Object>> entrySet() {
-      try {
-        return (Set<Entry<Object, Object>>)SCRIPT_OBJECT_ENTRYSET_METHOD.invoke(scriptMap);
-      } catch (Exception ex) {
-        throw new RuntimeException(REFLECTION_ERROR_MESSAGE + ex, ex);
-      }
-    }
   }
 
   @Override
