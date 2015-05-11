4629b43730fcfe83d45b74b0b157880bccf12081
Alejandro Abdelnur <tucu@streamsets.com>
2015-05-11 10:28:33 -0700

    SDC-598. Build using JDk7 fails because of references to JDK8 JavaScript classes
    Change-Id: I5ad890677645c3aff5ba07bbda3bcb2ea71fb927
    Reviewed-on: https://review.streamsets.net/217
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Adam Kunicki <adam@streamsets.com>
    Reviewed-by: Alejandro Abdelnur <tucu@streamsets.com>

--- basic-lib/src/main/java/com/streamsets/pipeline/stage/processor/javascript/Java8JavaScriptObjectFactory.java
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/processor/javascript/Java8JavaScriptObjectFactory.java
@@ -2,37 +2,67 @@ package com.streamsets.pipeline.stage.processor.javascript;
 
 import com.streamsets.pipeline.api.Field;
 import com.streamsets.pipeline.stage.processor.scripting.ScriptObjectFactory;
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
 import javax.script.ScriptEngine;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Java8JavaScriptObjectFactory extends ScriptObjectFactory {
+
+  private static final Class SCRIPT_OBJECT_MIRROR_CLASS;
+  private static final Method IS_ARRAY_METHOD;
+  private static final Method ENTRY_SET_METHOD;
+  private static final String REFLECTION_ERROR_MESSAGE = "Error performing reflection on " +
+                                                         System.getProperty("java.version") + ". Please report: ";
+  static {
+    try {
+      SCRIPT_OBJECT_MIRROR_CLASS = ClassLoader.getSystemClassLoader().loadClass(
+          "jdk.nashorn.api.scripting.ScriptObjectMirror");
+      //noinspection unchecked
+      IS_ARRAY_METHOD = SCRIPT_OBJECT_MIRROR_CLASS.getMethod("isArray");
+      //noinspection unchecked
+      ENTRY_SET_METHOD = SCRIPT_OBJECT_MIRROR_CLASS.getMethod("entrySet");
+
+    } catch (Exception ex) {
+      throw new RuntimeException(REFLECTION_ERROR_MESSAGE + ex, ex);
+    }
+  }
+
+
+
   public Java8JavaScriptObjectFactory(ScriptEngine engine) {
     super(engine);
   }
 
   @Override
+  @SuppressWarnings("unchecked")
   protected Field scriptToField(Object scriptObject) {
     Field field;
     if (scriptObject != null) {
-      if (scriptObject instanceof ScriptObjectMirror) {
-        ScriptObjectMirror scriptMap = (ScriptObjectMirror) scriptObject;
-        if (scriptMap.isArray()) {
-          List<Field> fields = new ArrayList<>(scriptMap.entrySet().size());
-          for (Map.Entry<String, Object> entry : scriptMap.entrySet()) {
-            fields.add(scriptToField(entry.getValue()));
+      if (SCRIPT_OBJECT_MIRROR_CLASS.isInstance(scriptObject)) {
+        try {
+          Set set = (Set) ENTRY_SET_METHOD.invoke(scriptObject);
+          if ((boolean) IS_ARRAY_METHOD.invoke(scriptObject)) {
+            List<Field> fields = new ArrayList<>(set.size());
+            for (Object obj : set) {
+              Map.Entry<String, Object> entry = (Map.Entry<String, Object>) obj;
+              fields.add(scriptToField(entry.getValue()));
+            }
+            field = Field.create(fields);
+          } else {
+            Map<String, Field> fields = new LinkedHashMap<>();
+            for (Object obj : set) {
+              Map.Entry<String, Object> entry = (Map.Entry<String, Object>) obj;
+              fields.put(entry.getKey(), scriptToField(entry.getValue()));
+            }
+            field = Field.create(fields);
           }
-          field = Field.create(fields);
-        } else {
-          Map<String, Field> fields = new LinkedHashMap<>();
-          for (Map.Entry<String, Object> entry : scriptMap.entrySet()) {
-            fields.put(entry.getKey(), scriptToField(entry.getValue()));
-          }
-          field = Field.create(fields);
+        } catch (Exception ex) {
+          throw new RuntimeException(ex);
         }
       } else if(scriptObject instanceof Map) {
         Map mapScriptObject = (Map)scriptObject;
