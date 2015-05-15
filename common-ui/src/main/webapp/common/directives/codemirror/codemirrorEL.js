cfd9b592536f7f7b193555b84cfde5cafb399efb
Madhukar Devaraju <madhu@streamsets.com>
2015-05-15 10:02:50 -0700

    SDC-639. When SQL Text field is left blank autocomplete shows up in upper left corner
    Change-Id: I832212b25d73e3897e0e208380b6810ff0b5396d
    Reviewed-on: https://review.streamsets.net/271
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>

--- common-ui/src/main/webapp/common/directives/codemirror/codemirrorEL.js
+++ common-ui/src/main/webapp/common/directives/codemirror/codemirrorEL.js
@@ -178,8 +178,7 @@ angular.module('dataCollectorApp.codemirrorDirectives')
           // The ui-codemirror directive allows us to receive a reference to the Codemirror instance on demand.
           scope.$broadcast('CodeMirror', function(cm) {
             cm.on('change', function(instance, change) {
-              var isVisible = $(instance. getWrapperElement()).is(':visible');
-              if (isVisible && change.origin !== 'complete') {
+              if (change.origin !== 'complete' && change.origin !== 'setValue') {
                 instance.showHint({ hint: window.CodeMirror.hint.dictionaryHint, completeSingle: false });
               }
               $timeout(function() {});
