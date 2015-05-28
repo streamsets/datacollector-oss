4fd96c387f272fcc8c9f469346d759a1ac0e52c9
Alejandro Abdelnur <tucu@streamsets.com>
2015-05-28 14:07:07 -0700

    SDC-788. DirectorySpool buffer limit check is incorrect
    Change-Id: Ic2eedd1ec2ff22d7941b286d0786e2c746b33d70
    Reviewed-on: https://review.streamsets.net/373
    Reviewed-by: Adam Kunicki <adam@streamsets.com>

--- basic-lib/src/main/java/com/streamsets/pipeline/stage/origin/spooldir/Errors.java
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/origin/spooldir/Errors.java
@@ -18,6 +18,8 @@ public enum Errors implements ErrorCode {
 
   SPOOLDIR_05("Unsupported charset '{}'"),
 
+  SPOOLDIR_06("Buffer Limit must be equal or greater than 64KB and equal or less than 1024MB"),
+
   SPOOLDIR_10("Unsupported data format '{}'"),
   SPOOLDIR_11("Directory path cannot be empty"),
   SPOOLDIR_12("Directory '{}' does not exist"),
