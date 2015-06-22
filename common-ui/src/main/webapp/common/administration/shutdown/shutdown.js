10852c03774a4d7cfdc4903d96ad7a7249c3d5b5
Madhukar Devaraju <madhu@streamsets.com>
2015-06-22 10:33:42 -0700

    SDC-910. Show loading in Stop pipeline and shutdown modal dialog until request is completed
    Change-Id: I5abd7e2237051f2a0252f19a02177ab022fdf9fd
    Reviewed-on: https://review.streamsets.net/479
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>

--- common-ui/src/main/webapp/common/administration/shutdown/shutdown.js
+++ common-ui/src/main/webapp/common/administration/shutdown/shutdown.js
@@ -7,15 +7,19 @@ angular
   .controller('ShutdownModalInstanceController', function ($scope, $modalInstance, api, $window) {
     angular.extend($scope, {
       issues: [],
+      isShuttingDown: false,
       isShutdownSucceed: false,
 
       shutdown: function() {
+        $scope.isShuttingDown = true;
         api.admin.shutdownCollector()
           .success(function() {
             $scope.isShutdownSucceed = true;
+            $scope.isShuttingDown = false;
           })
           .error(function(data) {
             $scope.issues = [data];
+            $scope.isShuttingDown = false;
           });
       },
 
