87e43ae0adfbec679f4776307c503a162b481e66
Madhukar Devaraju <madhu@streamsets.com>
2015-07-08 16:51:53 -0700

    SDC-1009. Field Converter: Incorrect label for datetime format
    Change-Id: I61ddda449d5e147f5b4664f3edb26e5adb85f8fc
    Reviewed-on: https://review.streamsets.net/580
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>

--- commonlib/src/main/java/com/streamsets/pipeline/config/DateFormat.java
+++ commonlib/src/main/java/com/streamsets/pipeline/config/DateFormat.java
@@ -14,7 +14,7 @@ public enum DateFormat implements Label {
   YYYY_MM_DD("yyyy-MM-dd", "YYYY-MM-DD"),
   DD_MM_YYYY("dd-MMM-YYYY", "DD-MMM-YYYY"),
   YYYY_MM_DD_HH_MM_SS("yyyy-MM-dd HH:mm:ss", "YYYY-MM-DD hh:mm:ss"),
-  YYYY_MM_DD_HH_MM_SS_SSS("yyyy-MM-dd HH:mm:ss.SSS", "YYYY-MM-DD hh:mm:ss"),
+  YYYY_MM_DD_HH_MM_SS_SSS("yyyy-MM-dd HH:mm:ss.SSS", "YYYY-MM-DD hh:mm:ss.sss"),
   YYYY_MM_DD_HH_MM_SS_SSS_Z("yyyy-MM-dd HH:mm:ss.SSS Z", "YYYY-MM-DD hh:mm:ss.sss Z"),
   YYYY_MM_DD_T_HH_MM_Z("yyyy-MM-dd'T'HH:mm'Z'", "YYYY-MM-DD'T'hh:mm'Z'"),
   OTHER(null, "Other ...");
