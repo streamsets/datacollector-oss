35dea7d762c5580720a6f5a89cae415edb277c53
Brock Noland <brockn@gmail.com>
2015-07-08 14:29:53 -0700

    SDC-1015. Add stage name to VALIDATION_0071
    Change-Id: I2168e277bde159c2df954a860fd80202145df680
    Reviewed-on: https://review.streamsets.net/575
    Reviewed-by: Alejandro Abdelnur <tucu@streamsets.com>

--- container/src/main/java/com/streamsets/pipeline/validation/ValidationError.java
+++ container/src/main/java/com/streamsets/pipeline/validation/ValidationError.java
@@ -81,7 +81,7 @@ public enum ValidationError implements ErrorCode {
   VALIDATION_0064("Error resolving memory limit: {}"),
 
   VALIDATION_0070("Pipeline does not define its execution mode"),
-  VALIDATION_0071("The stage does not support '{}' execution mode"),
+  VALIDATION_0071("Stage '{}' does not support '{}' execution mode"),
   VALIDATION_0072("Data collector is in standalone mode, cannot run pipeline cluster mode"),
   VALIDATION_0073("Data collector is in cluster mode, cannot run pipeline standalone mode"),
   VALIDATION_0074("The Stage library '{}' does not support '{}' execution mode for Stage type '{}'"),
