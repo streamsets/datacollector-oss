f9bd7291566a43629e95e3ec3d96fd5067621721
Madhukar Devaraju <madhu@streamsets.com>
2015-07-21 16:16:55 -0700

    SDC-1129. RESTful API exception message returning as HTML text instead of JSON data
    Change-Id: I91fded4b0773d2cf5e0983c90a9e590fdef820fc
    Reviewed-on: https://review.streamsets.net/670
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>

--- container/src/main/java/com/streamsets/dc/restapi/configuration/ExceptionToHttpErrorProvider.java
+++ container/src/main/java/com/streamsets/dc/restapi/configuration/ExceptionToHttpErrorProvider.java
@@ -3,7 +3,7 @@
  * be copied, modified, or distributed in whole or part without
  * written consent of StreamSets, Inc.
  */
-package com.streamsets.pipeline.restapi.configuration;
+package com.streamsets.dc.restapi.configuration;
 
 import com.streamsets.pipeline.api.StageException;
 import com.streamsets.pipeline.util.ContainerError;
--- container/src/main/java/com/streamsets/dc/restapi/configuration/RuntimeInfoInjector.java
+++ container/src/main/java/com/streamsets/dc/restapi/configuration/RuntimeInfoInjector.java
@@ -3,7 +3,7 @@
  * be copied, modified, or distributed in whole or part without
  * written consent of StreamSets, Inc.
  */
-package com.streamsets.pipeline.restapi.configuration;
+package com.streamsets.dc.restapi.configuration;
 
 import com.streamsets.pipeline.main.RuntimeInfo;
 import org.glassfish.hk2.api.Factory;
