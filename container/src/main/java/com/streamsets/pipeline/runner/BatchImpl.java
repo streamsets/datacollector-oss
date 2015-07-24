34e9cfd2efe75958013559eb1461201294688985
Adam Kunicki <adam@streamsets.com>
2015-07-24 13:39:51 -0700

    SDC-1153. Allow getting batch iterator more than once.
    Change-Id: I79d4360a510a9ed0743566c6da34d1456a74bbcd
    Reviewed-on: https://review.streamsets.net/705
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Alejandro Abdelnur <tucu@streamsets.com>

--- container/src/main/java/com/streamsets/pipeline/runner/BatchImpl.java
+++ container/src/main/java/com/streamsets/pipeline/runner/BatchImpl.java
@@ -5,11 +5,11 @@
  */
 package com.streamsets.pipeline.runner;
 
-import com.google.common.base.Preconditions;
 import com.streamsets.pipeline.api.Batch;
 import com.streamsets.pipeline.api.Record;
 import com.streamsets.pipeline.api.impl.Utils;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -17,13 +17,11 @@ public class BatchImpl implements Batch {
   private final String instanceName;
   private final List<Record> records;
   private final String sourceOffset;
-  private boolean got;
 
   public BatchImpl(String instanceName, String sourceOffset, List<Record> records) {
     this.instanceName = instanceName;
-    this.records = records;
+    this.records = Collections.unmodifiableList(records);
     this.sourceOffset = sourceOffset;
-    got = false;
   }
 
   @Override
@@ -33,8 +31,6 @@ public class BatchImpl implements Batch {
 
   @Override
   public Iterator<Record> getRecords() {
-    Preconditions.checkState(!got, "The record iterator can be obtained only once");
-    got = true;
     return records.iterator();
   }
 
@@ -44,7 +40,7 @@ public class BatchImpl implements Batch {
 
   @Override
   public String toString() {
-    return Utils.format("BatchImpl[instance='{}' size='{}', iterated='{}']", instanceName, records.size(), got);
+    return Utils.format("BatchImpl[instance='{}' size='{}']", instanceName, records.size());
   }
 
 }
