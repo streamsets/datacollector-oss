a01db4d959cf05e12486c96bfccf662f06e86efb
Alejandro Abdelnur <tucu@streamsets.com>
2015-07-24 13:00:37 -0700

    SDC-1152. LiveFile should handle EXT4 inode reuse for refresh detection
    Change-Id: I87d46f758e9ce5fc8258db90a3e08f2d390c7f72
    Reviewed-on: https://review.streamsets.net/702
    Reviewed-by: Alejandro Abdelnur <tucu@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- commonlib/src/main/java/com/streamsets/pipeline/lib/io/LiveFile.java
+++ commonlib/src/main/java/com/streamsets/pipeline/lib/io/LiveFile.java
@@ -8,20 +8,31 @@ package com.streamsets.pipeline.lib.io;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.streamsets.pipeline.api.impl.Utils;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
  * A <code>LiveFile</code> is a File reference that keeps track of its iNode and it can resync its name,
  * using the iNode as the anchor, in case of a rename. IMPORTANT: The rename must be within the same directory.
+ * <p/>
+ * <b>NOTE:</b> EXT4 filesystems reuse iNodes immediately, so if you delete a file and create a new file the iNode
+ * of the old file will most likely be used for the new file. To be able to handle this case and detect a file has
+ * been renamed (as opposed to deleted followed by a complete different file being created reusing the iNode) we
+ * hash the head (1024 bytes) of the file (Brocks idea).
  * <p/>
  * The primary use case for this class is for handling log files which may be rotated (renamed) while the file is
  * being accessed. By keeping track of the iNode, it is possible to get intermittent access to the same file (i.e.
@@ -30,7 +41,11 @@ import java.util.Map;
  * A <code>LiveFile</code> is immutable.
  */
 public class LiveFile {
+  private static final int HEAD_LEN = 1024;
+
   private final Path path;
+  private final String headHash;
+  private final int headLen;
   private final String iNode;
 
   /**
@@ -42,12 +57,32 @@ public class LiveFile {
   public LiveFile(Path path) throws IOException {
     Utils.checkNotNull(path, "path");
     this.path = path.toAbsolutePath();
-    iNode = Files.readAttributes(path, BasicFileAttributes.class).fileKey().toString();
+    BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
+    headLen = (int) Math.min(HEAD_LEN, attrs.size());
+    headHash = computeHash(path, headLen);
+    iNode = attrs.fileKey().toString();
   }
 
-  private LiveFile(Path path,  String inode) {
+  private LiveFile(Path path,  String inode, String headHash, int headLen) {
     this.path = path.toAbsolutePath();
     iNode = inode;
+    this.headHash = headHash;
+    this.headLen = headLen;
+  }
+
+
+  String computeHash(Path path, int len) throws IOException {
+    byte[] buffer = new byte[len];
+    try (InputStream is = new FileInputStream(path.toFile())) {
+      IOUtils.readFully(is, buffer);
+    }
+    try {
+      MessageDigest digest = MessageDigest.getInstance("MD5");
+      buffer = digest.digest(buffer);
+      return Base64.encodeBase64String(buffer);
+    } catch (NoSuchAlgorithmException ex) {
+      throw new IOException(ex);
+    }
   }
 
   /**
@@ -70,7 +105,7 @@ public class LiveFile {
 
   @Override
   public int hashCode() {
-    return path.hashCode() + iNode.hashCode();
+    return path.hashCode() + iNode.hashCode() + headHash.hashCode();
   }
 
   @Override
@@ -83,13 +118,13 @@ public class LiveFile {
     }
     if (obj instanceof LiveFile) {
       LiveFile other = (LiveFile) obj;
-      return path.equals(other.path) && iNode.equals(other.iNode);
+      return path.equals(other.path) && iNode.equals(other.iNode) && headHash.equals(other.headHash);
     }
     return false;
   }
 
   public String toString() {
-    return String.format("LiveFile[path=%s, iNode=%s]", path, iNode);
+    return String.format("LiveFile[path=%s, iNode=%s, headHash=%s]", path, iNode, headHash);
   }
 
   private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
@@ -103,6 +138,8 @@ public class LiveFile {
   public String serialize() {
     Map map = new LinkedHashMap();
     map.put("path", path.toString());
+    map.put("headHash", headHash);
+    map.put("headLen", headLen);
     map.put("inode", iNode);
     try {
       return OBJECT_MAPPER.writeValueAsString(map);
@@ -124,8 +161,10 @@ public class LiveFile {
     try {
       Map map = OBJECT_MAPPER.readValue(str, Map.class);
       Path path = Paths.get((String) map.get("path"));
+      String headHash = (map.containsKey("headHash")) ? (String) map.get("headHash") : "";
+      int headLen = (map.containsKey("headLen")) ? (int) map.get("headLen") : 0;
       String inode = (String) map.get("inode");
-      return new LiveFile(path, inode);
+      return new LiveFile(path, inode, headHash, headLen);
     } catch (RuntimeException|JsonParseException ex) {
       throw new IllegalArgumentException(Utils.format("Invalid LiveFile serialized string '{}': {}", str,
                                                       ex.getMessage()), ex);
@@ -133,28 +172,37 @@ public class LiveFile {
   }
 
   /**
-   * Refreshes the <code>LiveFile</code>, if the file was renamed, the path will have the new name..
+   * Refreshes the <code>LiveFile</code>, if the file was renamed, the path will have the new name.
    *
    * @return the refreshed file if the file has been renamed, or itself if the file has not been rename or the file
    * does not exist in the directory anymore.
    * @throws IOException thrown if the LiveFile could not be refreshed
    */
-  public LiveFile refresh() throws IOException, NoSuchFileException {
+  public LiveFile refresh() throws IOException {
     LiveFile refresh = this;
     boolean changed;
     try {
-      String iNodeStr = Files.readAttributes(path, BasicFileAttributes.class).fileKey().toString();
-      changed = !this.iNode.equals(iNodeStr);
+      BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
+      String iNodeCurrent = attrs.fileKey().toString();
+      int headLenCurrent = (int) Math.min(headLen, attrs.size());
+      String headHashCurrent = computeHash(path, headLenCurrent);
+      changed = !this.iNode.equals(iNodeCurrent) || !this.headHash.equals(headHashCurrent);
     } catch (NoSuchFileException ex) {
       changed = true;
     }
     if (changed) {
-      boolean found = false;
       try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path.getParent())) {
         for (Path path : directoryStream) {
-          String fileiNodeStr = Files.readAttributes(path, BasicFileAttributes.class).fileKey().toString();
-          if (fileiNodeStr.equals(iNode)) {
-            refresh = new LiveFile(path, iNode);
+          BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
+          String iNode = attrs.fileKey().toString();
+          int headLen = (int) Math.min(this.headLen, attrs.size());
+          String headHash = computeHash(path, headLen);
+          if (iNode.equals(this.iNode) && headHash.equals(this.headHash)) {
+            if (headLen == 0) {
+              headLen = (int) Math.min(HEAD_LEN, attrs.size());
+              headHash = computeHash(path, headLen);
+            }
+            refresh = new LiveFile(path, iNode, headHash, headLen);
             break;
           }
         }
