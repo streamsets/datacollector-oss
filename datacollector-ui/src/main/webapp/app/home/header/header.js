bb7deee943edd73e10c4ba61dcb5b5b7d9138a19
Madhukar Devaraju <madhu@streamsets.com>
2015-08-12 15:31:14 -0700

    SDC-1445. In cluster mode, CONNECT_ERROR state is ignored by UI
    Change-Id: I449ece6713f184d0501b8b36cb8a96217a5d612c
    Reviewed-on: https://review.streamsets.net/996
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>
    (cherry picked from commit dbf3ba47980a6679b5708ba53622ccca7598a72e)
    Reviewed-on: https://review.streamsets.net/997

--- datacollector-ui/src/main/webapp/app/home/header/header.js
+++ datacollector-ui/src/main/webapp/app/home/header/header.js
@@ -298,7 +298,7 @@ angular
         function() {
           api.pipelineAgent.getPreviewStatus(previewerId)
             .success(function(data) {
-              if(data && _.contains(['INVALID', 'VALIDATION_ERROR', 'START_ERROR', 'RUN_ERROR', 'VALID'], data.status)) {
+              if(data && _.contains(['INVALID', 'VALIDATION_ERROR', 'START_ERROR', 'RUN_ERROR', 'CONNECT_ERROR', 'VALID'], data.status)) {
                 fetchValidateConfigData(previewerId, defer);
               } else {
                 checkForValidateConfigStatus(previewerId, defer);
