a3621cb58f06e8469b4dfc92f0f22bbea32cdc7a
Brock Noland <brockn@gmail.com>
2015-08-14 14:35:05 -0700

    SDC-1418 - helpref.properties should be placed in a jar
    Change-Id: Icfe5dd8b6eb74890dfe8a42c6bd3fb9a7bd22efa
    Reviewed-on: https://review.streamsets.net/1023
    Reviewed-by: Loretta Chen <loretta@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- container/src/main/java/com/streamsets/datacollector/restapi/HelpResource.java
+++ container/src/main/java/com/streamsets/datacollector/restapi/HelpResource.java
@@ -5,25 +5,24 @@
  */
 package com.streamsets.datacollector.restapi;
 
-import com.streamsets.datacollector.main.RuntimeInfo;
-
+import com.streamsets.pipeline.api.impl.Utils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Authorization;
 
 import javax.annotation.security.DenyAll;
 import javax.annotation.security.PermitAll;
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URL;
 import java.util.Map;
 import java.util.Properties;
 
@@ -31,25 +30,32 @@ import java.util.Properties;
 @Api(value = "definitions")
 @DenyAll
 public class HelpResource {
-  private final RuntimeInfo runtimeInfo;
-
-  @Inject
-  public HelpResource(RuntimeInfo runtimeInfo) {
-    this.runtimeInfo = runtimeInfo;
+  private static final String HELP_REF_NAME = "helpref.properties";
+  private static final Properties HELP_REF_PROPERTIES;
+  static {
+    try {
+      URL helpRef = Utils.checkNotNull(HelpResource.class.getClassLoader().getResource(HELP_REF_NAME),
+        Utils.formatL("Could not find {}", HELP_REF_NAME));
+      try (InputStream is = helpRef.openStream()) {
+        try (Reader reader = new InputStreamReader(is)) {
+          Properties helpRefs = new Properties();
+          helpRefs.load(reader);
+          HELP_REF_PROPERTIES = helpRefs;
+        }
+      }
+    } catch (Exception ex) {
+      String msg = "Error loading " + HELP_REF_NAME;
+      throw new RuntimeException(msg, ex);
+    }
   }
+
+  public HelpResource() {  }
 
   @GET
   @ApiOperation(value = "Returns HELP Reference", response = Map.class, authorizations = @Authorization(value = "basic"))
   @Produces(MediaType.APPLICATION_JSON)
   @PermitAll
   public Response getHelpRefs() throws IOException {
-    File helpRefFile = new File(runtimeInfo.getConfigDir(), "helpref.properties");
-    try (Reader reader = new FileReader(helpRefFile)) {
-      Properties helpRefs = new Properties();
-      helpRefs.load(reader);
-      return Response.ok().type(MediaType.APPLICATION_JSON).entity(helpRefs).build();
-    }
+    return Response.ok().type(MediaType.APPLICATION_JSON).entity(HELP_REF_PROPERTIES).build();
   }
-
-
 }
