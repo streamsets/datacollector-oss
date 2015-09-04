1e93f9deec3ead7024b722cb8f59e87ef36cbe09
Brock Noland <brockn@gmail.com>
2015-09-03 18:54:34 -0700

    SDC-1719 - Create 1.1.0-RC2
    Change-Id: I4eb2709dfe5d52af6ff0f4b93b4a0474699b267d
    Reviewed-on: https://review.streamsets.net/1256
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Brock Noland <brock@streamsets.com>

---
+++ dev-support/update-version.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+#
+set -e
+set -x
+version=$1
+if [[ -z "$version" ]]
+then
+  echo "Usage: $0 NEW-VERSION"
+  exit 1
+fi
+mvn versions:set -Drelease -DnewVersion=$version
+pushd rbgen-maven-plugin
+mvn versions:set -Drelease -DnewVersion=$version
+popd
+pushd e2e-tests
+mvn versions:set -Drelease -DnewVersion=$version
+popd
+perl -i -pe 's@^(\s+"version"\s*:\s*").*("\s*,\s*)$@${1}'"$version"'$2@' datacollector-ui/package.json
+find . -name pom.xml.versionsBackup -delete
