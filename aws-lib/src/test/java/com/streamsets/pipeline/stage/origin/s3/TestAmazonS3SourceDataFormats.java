f0566396b8ef1849f2f9bc51d7ff8219cec55db7
harikiran nayak <hari@streamsets.com>
2015-09-09 09:59:09 -0700

    SDC-1725. Unit test for all datatypes in AmazonS3 origin
    Change-Id: I4876d6b66e7f4de97e2d3adb031a6714bd9dde1c
    
    Fix rat failures
    
    Change-Id: If118592bb7454aff37dfdc2fcf11a796de4a2f60
    
    Fix pom file to revert unexpected change
    
    Change-Id: If79e9c1da354946f5c775ce8776706821d06fb33
    Reviewed-on: https://review.streamsets.net/1296
    Reviewed-by: Harikiran Nayak <hari@streamsets.com>

---
+++ aws-lib/src/test/java/com/streamsets/pipeline/stage/origin/s3/TestAmazonS3SourceDataFormats.java
@@ -0,0 +1,475 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.origin.s3;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.S3ClientOptions;
+import com.amazonaws.services.s3.iterable.S3Objects;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.google.common.io.Resources;
+import com.streamsets.pipeline.api.BatchMaker;
+import com.streamsets.pipeline.api.Record;
+import com.streamsets.pipeline.config.CsvHeader;
+import com.streamsets.pipeline.config.CsvMode;
+import com.streamsets.pipeline.config.CsvRecordType;
+import com.streamsets.pipeline.config.DataFormat;
+import com.streamsets.pipeline.config.JsonMode;
+import com.streamsets.pipeline.config.LogMode;
+import com.streamsets.pipeline.config.PostProcessingOptions;
+import com.streamsets.pipeline.sdk.SourceRunner;
+import com.streamsets.pipeline.sdk.StageRunner;
+import com.streamsets.pipeline.stage.origin.lib.BasicConfig;
+import com.streamsets.pipeline.stage.origin.lib.DataFormatConfig;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestAmazonS3SourceDataFormats {
+
+  private static String fakeS3Root;
+  private static ExecutorService executorService;
+  private static FakeS3 fakeS3;
+  private static AmazonS3Client s3client;
+  private static final String BUCKET_NAME = "testamazonssourcedataformats";
+  private static int port;
+
+  @BeforeClass
+  public static void setUpClass() throws IOException, InterruptedException {
+    File dir = new File(new File("target", UUID.randomUUID().toString()), "fakes3_root").getAbsoluteFile();
+    Assert.assertTrue(dir.mkdirs());
+    fakeS3Root = dir.getAbsolutePath();
+    port = getFreePort();
+    fakeS3 = new FakeS3(fakeS3Root, port);
+    Assume.assumeTrue("Please install fakes3 in your system", fakeS3.fakes3Installed());
+    //Start the fakes3 server
+    executorService = Executors.newSingleThreadExecutor();
+    executorService.submit(fakeS3);
+
+    populateFakes3();
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+    if(executorService != null) {
+      executorService.shutdownNow();
+    }
+    if(fakeS3 != null) {
+      fakeS3.shutdown();
+    }
+  }
+
+  private static void populateFakes3() throws IOException, InterruptedException {
+    BasicAWSCredentials credentials = new BasicAWSCredentials("foo", "bar");
+    s3client = new AmazonS3Client(credentials);
+    s3client.setEndpoint("http://localhost:" + port);
+    s3client.setS3ClientOptions(new S3ClientOptions().withPathStyleAccess(true));
+
+    createBucket(s3client, BUCKET_NAME);
+
+    //write files each under myBucket
+    //delimited
+    InputStream in = Resources.getResource("sample_csv.csv").openStream();
+    PutObjectRequest putObjectRequest = new PutObjectRequest(BUCKET_NAME, "sample_csv.csv", in, new ObjectMetadata());
+    s3client.putObject(putObjectRequest);
+    //sdc
+    in = Resources.getResource("sample_sdc.sdc").openStream();
+    putObjectRequest = new PutObjectRequest(BUCKET_NAME, "sample_sdc.sdc", in, new ObjectMetadata());
+    s3client.putObject(putObjectRequest);
+    //xml
+    in = Resources.getResource("sample_xml.xml").openStream();
+    putObjectRequest = new PutObjectRequest(BUCKET_NAME, "sample_xml.xml", in, new ObjectMetadata());
+    s3client.putObject(putObjectRequest);
+    //json
+    in = Resources.getResource("sample_json.json").openStream();
+    putObjectRequest = new PutObjectRequest(BUCKET_NAME, "sample_json.json", in, new ObjectMetadata());
+    s3client.putObject(putObjectRequest);
+    //log
+    in = Resources.getResource("sample_log.log").openStream();
+    putObjectRequest = new PutObjectRequest(BUCKET_NAME, "sample_log.log", in, new ObjectMetadata());
+    s3client.putObject(putObjectRequest);
+
+
+    int count = 0;
+    if(s3client.doesBucketExist(BUCKET_NAME)) {
+      for(S3ObjectSummary s : S3Objects.withPrefix(s3client, BUCKET_NAME, "")) {
+        System.out.println(s.getKey());
+        count++;
+      }
+    }
+    Assert.assertEquals(5, count);
+  }
+
+  private static void createBucket(AmazonS3Client s3client, String bucketName) {
+    if(s3client.doesBucketExist(bucketName)) {
+      for(S3ObjectSummary s : S3Objects.inBucket(s3client, bucketName)) {
+        s3client.deleteObject(bucketName, s.getKey());
+      }
+      s3client.deleteBucket(bucketName);
+    }
+    Assert.assertFalse(s3client.doesBucketExist(bucketName));
+    // Note that CreateBucketRequest does not specify region. So bucket is
+    // bucketName
+    s3client.createBucket(new CreateBucketRequest(bucketName));
+  }
+
+  @Test
+  public void testProduceLogFile() throws Exception {
+    AmazonS3Source source = createSourceLog();
+    SourceRunner runner = new SourceRunner.Builder(AmazonS3DSource.class, source).addOutputLane("lane").build();
+    runner.runInit();
+    try {
+      int initialCount = getObjectCount(s3client, BUCKET_NAME);
+
+      List<Record> allRecords = new ArrayList<>();
+      String offset = null;
+      for(int i = 0; i < 2; i++) {
+        BatchMaker batchMaker = SourceRunner.createTestBatchMaker("lane");
+        offset = source.produce(offset, 60000, batchMaker);
+        Assert.assertNotNull(offset);
+
+        StageRunner.Output output = SourceRunner.getOutput(batchMaker);
+        List<Record> records = output.getRecords().get("lane");
+        allRecords.addAll(records);
+      }
+
+      Assert.assertEquals(10, allRecords.size());
+      Assert.assertTrue(offset.contains("sample_log.log::-1"));
+      Assert.assertEquals(initialCount, getObjectCount(s3client, BUCKET_NAME));
+
+    } finally {
+      runner.runDestroy();
+    }
+  }
+
+  @Test
+  public void testProduceDelimitedFile() throws Exception {
+    AmazonS3Source source = createSourceDelimited();
+    SourceRunner runner = new SourceRunner.Builder(AmazonS3DSource.class, source).addOutputLane("lane").build();
+    runner.runInit();
+    try {
+      int initialCount = getObjectCount(s3client, BUCKET_NAME);
+
+      List<Record> allRecords = new ArrayList<>();
+      String offset = null;
+      for(int i = 0; i < 2; i++) {
+        BatchMaker batchMaker = SourceRunner.createTestBatchMaker("lane");
+        offset = source.produce(offset, 60000, batchMaker);
+        Assert.assertNotNull(offset);
+
+        StageRunner.Output output = SourceRunner.getOutput(batchMaker);
+        List<Record> records = output.getRecords().get("lane");
+        allRecords.addAll(records);
+      }
+
+      Assert.assertEquals(12, allRecords.size());
+      Assert.assertTrue(offset.contains("sample_csv.csv::-1"));
+      Assert.assertEquals(initialCount, getObjectCount(s3client, BUCKET_NAME));
+
+    } finally {
+      runner.runDestroy();
+    }
+  }
+
+  @Test
+  public void testProduceSdcFile() throws Exception {
+    AmazonS3Source source = createSourceSdc();
+    SourceRunner runner = new SourceRunner.Builder(AmazonS3DSource.class, source).addOutputLane("lane").build();
+    runner.runInit();
+    try {
+      int initialCount = getObjectCount(s3client, BUCKET_NAME);
+
+      List<Record> allRecords = new ArrayList<>();
+      String offset = null;
+      for(int i = 0; i < 2; i++) {
+        BatchMaker batchMaker = SourceRunner.createTestBatchMaker("lane");
+        offset = source.produce(offset, 60000, batchMaker);
+        Assert.assertNotNull(offset);
+
+        StageRunner.Output output = SourceRunner.getOutput(batchMaker);
+        List<Record> records = output.getRecords().get("lane");
+        allRecords.addAll(records);
+      }
+
+      Assert.assertEquals(1023, allRecords.size());
+      Assert.assertTrue(offset.contains("sample_sdc.sdc::-1"));
+      Assert.assertEquals(initialCount, getObjectCount(s3client, BUCKET_NAME));
+
+    } finally {
+      runner.runDestroy();
+    }
+  }
+
+  @Test
+  public void testProduceXmlFile() throws Exception {
+    AmazonS3Source source = createSourceXml();
+    SourceRunner runner = new SourceRunner.Builder(AmazonS3DSource.class, source).addOutputLane("lane").build();
+    runner.runInit();
+    try {
+      int initialCount = getObjectCount(s3client, BUCKET_NAME);
+
+      List<Record> allRecords = new ArrayList<>();
+      String offset = null;
+      for(int i = 0; i < 2; i++) {
+        BatchMaker batchMaker = SourceRunner.createTestBatchMaker("lane");
+        offset = source.produce(offset, 60000, batchMaker);
+        Assert.assertNotNull(offset);
+
+        StageRunner.Output output = SourceRunner.getOutput(batchMaker);
+        List<Record> records = output.getRecords().get("lane");
+        allRecords.addAll(records);
+      }
+
+      Assert.assertEquals(12, allRecords.size());
+      Assert.assertTrue(offset.contains("sample_xml.xml::-1"));
+      Assert.assertEquals(initialCount, getObjectCount(s3client, BUCKET_NAME));
+
+    } finally {
+      runner.runDestroy();
+    }
+  }
+
+  @Test
+  public void testProduceJsonFile() throws Exception {
+    AmazonS3Source source = createSourceJson();
+    SourceRunner runner = new SourceRunner.Builder(AmazonS3DSource.class, source).addOutputLane("lane").build();
+    runner.runInit();
+    try {
+      int initialCount = getObjectCount(s3client, BUCKET_NAME);
+
+      List<Record> allRecords = new ArrayList<>();
+      String offset = null;
+      for(int i = 0; i < 2; i++) {
+        BatchMaker batchMaker = SourceRunner.createTestBatchMaker("lane");
+        offset = source.produce(offset, 60000, batchMaker);
+        Assert.assertNotNull(offset);
+
+        StageRunner.Output output = SourceRunner.getOutput(batchMaker);
+        List<Record> records = output.getRecords().get("lane");
+        allRecords.addAll(records);
+      }
+
+      Assert.assertEquals(3, allRecords.size());
+      Assert.assertTrue(offset.contains("sample_json.json::-1"));
+      Assert.assertEquals(initialCount, getObjectCount(s3client, BUCKET_NAME));
+
+    } finally {
+      runner.runDestroy();
+    }
+  }
+
+  private AmazonS3Source createSourceLog() {
+
+    S3ConfigBean s3ConfigBean = new S3ConfigBean();
+    s3ConfigBean.basicConfig = new BasicConfig();
+    s3ConfigBean.basicConfig.maxWaitTime = 1000;
+    s3ConfigBean.basicConfig.maxBatchSize = 60000;
+
+    s3ConfigBean.dataFormatConfig = new DataFormatConfig();
+    s3ConfigBean.dataFormat = DataFormat.LOG;
+    s3ConfigBean.dataFormatConfig.logMode = LogMode.COMMON_LOG_FORMAT;
+    s3ConfigBean.dataFormatConfig.logMaxObjectLen = 1024;
+    s3ConfigBean.dataFormatConfig.charset = "UTF-8";
+
+    s3ConfigBean.errorConfig = new S3ErrorConfig();
+    s3ConfigBean.errorConfig.errorHandlingOption = PostProcessingOptions.NONE;
+
+    s3ConfigBean.postProcessingConfig = new S3PostProcessingConfig();
+    s3ConfigBean.postProcessingConfig.postProcessing = PostProcessingOptions.NONE;
+
+    s3ConfigBean.s3FileConfig = new S3FileConfig();
+    s3ConfigBean.s3FileConfig.overrunLimit = 65*1000;
+    s3ConfigBean.s3FileConfig.filePattern = "*.log";
+
+    s3ConfigBean.s3Config = new S3Config();
+    s3ConfigBean.s3Config.setEndPointForTest("http://localhost:" + port);
+    s3ConfigBean.s3Config.bucket = BUCKET_NAME;
+    s3ConfigBean.s3Config.accessKeyId = "foo";
+    s3ConfigBean.s3Config.secretAccessKey = "bar";
+    s3ConfigBean.s3Config.folder = "";
+    s3ConfigBean.s3Config.delimiter = "/";
+
+    return new AmazonS3Source(s3ConfigBean);
+  }
+
+  private AmazonS3Source createSourceDelimited() {
+
+    S3ConfigBean s3ConfigBean = new S3ConfigBean();
+    s3ConfigBean.basicConfig = new BasicConfig();
+    s3ConfigBean.basicConfig.maxWaitTime = 1000;
+    s3ConfigBean.basicConfig.maxBatchSize = 60000;
+
+    s3ConfigBean.dataFormatConfig = new DataFormatConfig();
+    s3ConfigBean.dataFormat = DataFormat.DELIMITED;
+    s3ConfigBean.dataFormatConfig.csvFileFormat = CsvMode.CSV;
+    s3ConfigBean.dataFormatConfig.csvMaxObjectLen = 1024;
+    s3ConfigBean.dataFormatConfig.csvHeader = CsvHeader.IGNORE_HEADER;
+    s3ConfigBean.dataFormatConfig.csvRecordType = CsvRecordType.LIST;
+    s3ConfigBean.dataFormatConfig.charset = "UTF-8";
+
+    s3ConfigBean.errorConfig = new S3ErrorConfig();
+    s3ConfigBean.errorConfig.errorHandlingOption = PostProcessingOptions.NONE;
+
+    s3ConfigBean.postProcessingConfig = new S3PostProcessingConfig();
+    s3ConfigBean.postProcessingConfig.postProcessing = PostProcessingOptions.NONE;
+
+    s3ConfigBean.s3FileConfig = new S3FileConfig();
+    s3ConfigBean.s3FileConfig.overrunLimit = 65*1000;
+    s3ConfigBean.s3FileConfig.filePattern = "*.csv";
+
+    s3ConfigBean.s3Config = new S3Config();
+    s3ConfigBean.s3Config.setEndPointForTest("http://localhost:" + port);
+    s3ConfigBean.s3Config.bucket = BUCKET_NAME;
+    s3ConfigBean.s3Config.accessKeyId = "foo";
+    s3ConfigBean.s3Config.secretAccessKey = "bar";
+    s3ConfigBean.s3Config.folder = "";
+    s3ConfigBean.s3Config.delimiter = "/";
+
+    return new AmazonS3Source(s3ConfigBean);
+  }
+
+  private AmazonS3Source createSourceSdc() {
+
+    S3ConfigBean s3ConfigBean = new S3ConfigBean();
+    s3ConfigBean.basicConfig = new BasicConfig();
+    s3ConfigBean.basicConfig.maxWaitTime = 1000;
+    s3ConfigBean.basicConfig.maxBatchSize = 60000;
+
+    s3ConfigBean.dataFormatConfig = new DataFormatConfig();
+    s3ConfigBean.dataFormat = DataFormat.SDC_JSON;
+    s3ConfigBean.dataFormatConfig.charset = "UTF-8";
+
+    s3ConfigBean.errorConfig = new S3ErrorConfig();
+    s3ConfigBean.errorConfig.errorHandlingOption = PostProcessingOptions.NONE;
+
+    s3ConfigBean.postProcessingConfig = new S3PostProcessingConfig();
+    s3ConfigBean.postProcessingConfig.postProcessing = PostProcessingOptions.NONE;
+
+    s3ConfigBean.s3FileConfig = new S3FileConfig();
+    s3ConfigBean.s3FileConfig.overrunLimit = 65*1000;
+    s3ConfigBean.s3FileConfig.filePattern = "*.sdc";
+
+    s3ConfigBean.s3Config = new S3Config();
+    s3ConfigBean.s3Config.setEndPointForTest("http://localhost:" + port);
+    s3ConfigBean.s3Config.bucket = BUCKET_NAME;
+    s3ConfigBean.s3Config.accessKeyId = "foo";
+    s3ConfigBean.s3Config.secretAccessKey = "bar";
+    s3ConfigBean.s3Config.folder = "";
+    s3ConfigBean.s3Config.delimiter = "/";
+
+    return new AmazonS3Source(s3ConfigBean);
+  }
+
+  private AmazonS3Source createSourceXml() {
+
+    S3ConfigBean s3ConfigBean = new S3ConfigBean();
+    s3ConfigBean.basicConfig = new BasicConfig();
+    s3ConfigBean.basicConfig.maxWaitTime = 1000;
+    s3ConfigBean.basicConfig.maxBatchSize = 60000;
+
+    s3ConfigBean.dataFormatConfig = new DataFormatConfig();
+    s3ConfigBean.dataFormat = DataFormat.XML;
+    s3ConfigBean.dataFormatConfig.charset = "UTF-8";
+    s3ConfigBean.dataFormatConfig.xmlMaxObjectLen = 1024;
+    s3ConfigBean.dataFormatConfig.xmlRecordElement = "book";
+
+    s3ConfigBean.errorConfig = new S3ErrorConfig();
+    s3ConfigBean.errorConfig.errorHandlingOption = PostProcessingOptions.NONE;
+
+    s3ConfigBean.postProcessingConfig = new S3PostProcessingConfig();
+    s3ConfigBean.postProcessingConfig.postProcessing = PostProcessingOptions.NONE;
+
+    s3ConfigBean.s3FileConfig = new S3FileConfig();
+    s3ConfigBean.s3FileConfig.overrunLimit = 65*1000;
+    s3ConfigBean.s3FileConfig.filePattern = "*.xml";
+
+    s3ConfigBean.s3Config = new S3Config();
+    s3ConfigBean.s3Config.setEndPointForTest("http://localhost:" + port);
+    s3ConfigBean.s3Config.bucket = BUCKET_NAME;
+    s3ConfigBean.s3Config.accessKeyId = "foo";
+    s3ConfigBean.s3Config.secretAccessKey = "bar";
+    s3ConfigBean.s3Config.folder = "";
+    s3ConfigBean.s3Config.delimiter = "/";
+
+    return new AmazonS3Source(s3ConfigBean);
+  }
+
+  private AmazonS3Source createSourceJson() {
+
+    S3ConfigBean s3ConfigBean = new S3ConfigBean();
+    s3ConfigBean.basicConfig = new BasicConfig();
+    s3ConfigBean.basicConfig.maxWaitTime = 1000;
+    s3ConfigBean.basicConfig.maxBatchSize = 60000;
+
+    s3ConfigBean.dataFormatConfig = new DataFormatConfig();
+    s3ConfigBean.dataFormat = DataFormat.JSON;
+    s3ConfigBean.dataFormatConfig.charset = "UTF-8";
+    s3ConfigBean.dataFormatConfig.jsonMaxObjectLen = 10000;
+    s3ConfigBean.dataFormatConfig.jsonContent = JsonMode.MULTIPLE_OBJECTS;
+
+    s3ConfigBean.errorConfig = new S3ErrorConfig();
+    s3ConfigBean.errorConfig.errorHandlingOption = PostProcessingOptions.NONE;
+
+    s3ConfigBean.postProcessingConfig = new S3PostProcessingConfig();
+    s3ConfigBean.postProcessingConfig.postProcessing = PostProcessingOptions.NONE;
+
+    s3ConfigBean.s3FileConfig = new S3FileConfig();
+    s3ConfigBean.s3FileConfig.overrunLimit = 65*1000;
+    s3ConfigBean.s3FileConfig.filePattern = "*.json";
+
+    s3ConfigBean.s3Config = new S3Config();
+    s3ConfigBean.s3Config.setEndPointForTest("http://localhost:" + port);
+    s3ConfigBean.s3Config.bucket = BUCKET_NAME;
+    s3ConfigBean.s3Config.accessKeyId = "foo";
+    s3ConfigBean.s3Config.secretAccessKey = "bar";
+    s3ConfigBean.s3Config.folder = "";
+    s3ConfigBean.s3Config.delimiter = "/";
+
+    return new AmazonS3Source(s3ConfigBean);
+  }
+
+  public static int getFreePort() throws IOException {
+    ServerSocket serverSocket = new ServerSocket(0);
+    int port = serverSocket.getLocalPort();
+    serverSocket.close();
+    return port;
+  }
+
+  private int getObjectCount(AmazonS3Client s3Client, String bucket) {
+    int count = 0;
+    for(S3ObjectSummary s : S3Objects.inBucket(s3Client, bucket)) {
+      count++;
+    }
+    return count;
+  }
+}
