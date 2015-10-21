d972f55f1c4ef56e0d0ef70956d17663801ab10d
harikiran nayak <hari@streamsets.com>
2015-10-21 10:41:08 -0700

    SDC-1924. Undo Boon Generator Support in JsonCharDatagenerator
    Change-Id: I578c6dfeff5c2139073dd78927880388f789bf15
    Reviewed-on: https://review.streamsets.net/1472
    Reviewed-by: Harikiran Nayak <hari@streamsets.com>

--- commonlib/src/main/java/com/streamsets/pipeline/lib/generator/json/JsonCharDataGenerator.java
+++ commonlib/src/main/java/com/streamsets/pipeline/lib/generator/json/JsonCharDataGenerator.java
@@ -19,6 +19,10 @@
  */
 package com.streamsets.pipeline.lib.generator.json;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.streamsets.pipeline.api.Field;
 import com.streamsets.pipeline.api.Record;
 import com.streamsets.pipeline.config.JsonMode;
 import com.streamsets.pipeline.lib.generator.DataGenerator;
@@ -26,27 +30,24 @@ import com.streamsets.pipeline.lib.generator.DataGeneratorException;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class JsonCharDataGenerator implements DataGenerator {
+  final static String EOL = System.getProperty("line.separator");
+  private static final JsonFactory JSON_FACTORY = new ObjectMapper().getFactory();
 
-  private static final String SDC_JSON_GENERATOR_BOON_ENABLED_KEY = "sdc.json.generator.boon.enabled";
-  private static final String SDC_JSON_GENERATOR_BOON_ENABLED_DEFAULT = "false";
+  private final boolean isArray;
+  private final JsonGenerator generator;
+  private boolean closed;
 
-  private DataGenerator dataGenerator;
-  private boolean isArray;
-
-  public JsonCharDataGenerator(Writer writer, JsonMode jsonMode)
-      throws IOException {
-    if(jsonMode == JsonMode.ARRAY_OBJECTS) {
-      isArray = true;
-      dataGenerator = new JacksonCharDataGenerator(writer, jsonMode);
-    } else {
-      String property = System.getProperty(SDC_JSON_GENERATOR_BOON_ENABLED_KEY, SDC_JSON_GENERATOR_BOON_ENABLED_DEFAULT);
-      if("true".equalsIgnoreCase(property)) {
-        dataGenerator = new BoonCharDataGenerator(writer, jsonMode);
-      } else {
-        dataGenerator = new JacksonCharDataGenerator(writer, jsonMode);
-      }
+  public JsonCharDataGenerator(Writer writer, JsonMode jsonMode) throws IOException {
+    isArray = jsonMode == JsonMode.ARRAY_OBJECTS;
+    generator = JSON_FACTORY.createGenerator(writer);
+    if (isArray) {
+      generator.writeStartArray();
     }
   }
 
@@ -57,17 +58,80 @@ public class JsonCharDataGenerator implements DataGenerator {
 
   @Override
   public void write(Record record) throws IOException, DataGeneratorException {
-    dataGenerator.write(record);
+    if (closed) {
+      throw new IOException("generator has been closed");
+    }
+    generator.writeObject(fieldToJsonObject(record, record.get()));
+    if (!isArray) {
+      generator.writeRaw(EOL);
+    }
   }
 
   @Override
   public void flush() throws IOException {
-    dataGenerator.flush();
+    if (closed) {
+      throw new IOException("generator has been closed");
+    }
+    generator.flush();
   }
 
   @Override
   public void close() throws IOException {
-    dataGenerator.close();
+    closed = true;
+    if (isArray) {
+      generator.writeEndArray();
+    }
+    generator.close();
   }
 
+  public static Object fieldToJsonObject(Record record, Field field) throws DataGeneratorException {
+    Object obj;
+    if (field == null || field.getValue() == null) {
+      obj = null;
+    } else if (field.getType() == Field.Type.BOOLEAN) {
+      obj = field.getValueAsBoolean();
+    } else if (field.getType() == Field.Type.BYTE) {
+      obj = field.getValueAsByte();
+    } else if (field.getType() == Field.Type.BYTE_ARRAY) {
+      obj = field.getValueAsByteArray();
+    } else if (field.getType() == Field.Type.CHAR) {
+      obj = field.getValueAsChar();
+    } else if (field.getType() == Field.Type.DATE) {
+      obj = field.getValueAsDate();
+    } else if (field.getType() == Field.Type.DATETIME) {
+      obj = field.getValueAsDatetime();
+    } else if (field.getType() == Field.Type.DECIMAL) {
+      obj = field.getValueAsDecimal();
+    } else if (field.getType() == Field.Type.DOUBLE) {
+      obj = field.getValueAsDouble();
+    } else if (field.getType() == Field.Type.FLOAT) {
+      obj = field.getValueAsFloat();
+    } else if (field.getType() == Field.Type.INTEGER) {
+      obj = field.getValueAsInteger();
+    } else if (field.getType() == Field.Type.LONG) {
+      obj = field.getValueAsLong();
+    } else if (field.getType() == Field.Type.SHORT) {
+      obj = field.getValueAsShort();
+    } else if (field.getType() == Field.Type.STRING) {
+      obj = field.getValueAsString();
+    } else if (field.getType() == Field.Type.LIST) {
+      List<Field> list = field.getValueAsList();
+      List<Object> toReturn = new ArrayList<>(list.size());
+      for (Field f : list) {
+        toReturn.add(fieldToJsonObject(record, f));
+      }
+      obj = toReturn;
+    } else if (field.getType() == Field.Type.MAP || field.getType() == Field.Type.LIST_MAP) {
+      Map<String, Field> map = field.getValueAsMap();
+      Map<String, Object> toReturn = new LinkedHashMap<>();
+      for (Map.Entry<String, Field> entry : map.entrySet()) {
+        toReturn.put(entry.getKey(), fieldToJsonObject(record, entry.getValue()));
+      }
+      obj = toReturn;
+    } else {
+      throw new DataGeneratorException(Errors.JSON_GENERATOR_00, field.getType(), field.getValue(),
+        record.getHeader().getSourceId());
+    }
+    return obj;
+  }
 }
