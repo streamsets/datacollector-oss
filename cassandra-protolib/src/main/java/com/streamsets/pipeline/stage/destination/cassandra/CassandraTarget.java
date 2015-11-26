3aaa0ac202fba535adc3d02a513dd46217dd35bb
Jurjen Vorhauer <jvorhauer@users.noreply.github.com>
2015-11-25 20:52:00 -0800

    SDC-2076. Cassandra Target credentials not passed to checkCassandraReachable
    Change-Id: If343330ca0b648227f6d19717abc422783c5d2ea
    Reviewed-on: https://review.streamsets.net/1631
    Reviewed-by: Adam Kunicki <adam@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- cassandra-protolib/src/main/java/com/streamsets/pipeline/stage/destination/cassandra/CassandraTarget.java
+++ cassandra-protolib/src/main/java/com/streamsets/pipeline/stage/destination/cassandra/CassandraTarget.java
@@ -358,6 +358,7 @@ public class CassandraTarget extends BaseTarget {
   private Cluster getCluster() {
     return Cluster.builder()
         .addContactPoints(contactPoints)
+        .withCredentials(username, password)
         .withPort(port)
         .build();
   }
