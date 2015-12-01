71de2da2c071d2c7ac6f46c36912a1c6ade26e4f
Brock Noland <brockn@gmail.com>
2015-12-01 12:09:47 -0600

    SDC-2097 - Add cloudera profile to build
    Change-Id: I24498c81b702f964860aa78cb1ba57271db8e9d9
    Reviewed-on: https://review.streamsets.net/1646
    Reviewed-by: Bryan Duxbury <bryan@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- dev-support/ci-run-all-libs.sh
+++ dev-support/ci-run-all-libs.sh
@@ -53,7 +53,7 @@ git show
 /opt/scripts/docker-delete-stopped-containers.sh || true
 /opt/scripts/docker-delete-local-images.sh || true
 # compile and install
-mvn clean install -U -Pdist,all-libs,ui,rpm,miniIT -Drelease -Dtest=DoesNotExist -DfailIfNoTests=false
+mvn clean install -U -Pdist,all-libs,ui,rpm,miniIT,cloudera -Drelease -Dtest=DoesNotExist -DfailIfNoTests=false
 # package and run tests (if appropiate)
 set +e
 export JAVA_HOME=${TEST_JVM}
