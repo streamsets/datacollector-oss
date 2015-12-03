7f73b074c3e141a97a2680a46e6d97adf2946e81
Madhukar Devaraju <madhu@streamsets.com>
2015-12-03 10:30:34 -0800

    SDC-1831 Auto generate hosted help link, part2
    Change-Id: I17d0fa92146a685d8ce426d088c22247087431d5
    Reviewed-on: https://review.streamsets.net/1663
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>

--- datacollector-ui/src/main/webapp/app/common/contextHelpService.js
+++ datacollector-ui/src/main/webapp/app/common/contextHelpService.js
@@ -59,7 +59,7 @@ angular.module('dataCollectorApp.common')
       this.configInitPromise.then(function() {
         var uiHelpBaseURL, helpURL;
         if ($rootScope.$storage.helpLocation === pipelineConstant.HOSTED_HELP) {
-          uiHelpBaseURL = configuration.getUIHostedHelpBaseURL();
+          uiHelpBaseURL = 'https://www.streamsets.com/documentation/datacollector/' + buildInfo.version + '/help';
         } else {
           uiHelpBaseURL = configuration.getUILocalHelpBaseURL();
         }
