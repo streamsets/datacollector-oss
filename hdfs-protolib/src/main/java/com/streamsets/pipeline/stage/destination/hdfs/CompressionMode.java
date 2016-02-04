32be37b2e9f3848fd7e849ba66d70b4868b2ca8a
Bryan Duxbury <bryan@streamsets.com>
2016-02-03 16:11:08 -0800

    SDC-2220. Hadoop FS and Local FS destinations dont list lz4 as compression option
    Change-Id: I3dc78579020cf5cb82b561befe2c6adc22ba479f
    Reviewed-on: https://review.streamsets.net/1926
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Alejandro Abdelnur <tucu@streamsets.com>

--- hdfs-protolib/src/main/java/com/streamsets/pipeline/stage/destination/hdfs/CompressionMode.java
+++ hdfs-protolib/src/main/java/com/streamsets/pipeline/stage/destination/hdfs/CompressionMode.java
@@ -24,6 +24,7 @@ import com.streamsets.pipeline.api.Label;
 import org.apache.hadoop.io.compress.BZip2Codec;
 import org.apache.hadoop.io.compress.CompressionCodec;
 import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.io.compress.Lz4Codec;
 import org.apache.hadoop.io.compress.SnappyCodec;
 
 @GenerateResourceBundle
@@ -32,6 +33,7 @@ public enum CompressionMode implements Label {
   GZIP("Gzip", GzipCodec.class),
   BZIP2("Bzip2", BZip2Codec.class),
   SNAPPY("Snappy", SnappyCodec.class),
+  LZ4("LZ4", Lz4Codec.class),
   OTHER("Other...", null),
 
   ;
