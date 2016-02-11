a03bb856eab947e9f4f633081047c7f020406357
harikiran nayak <hari@streamsets.com>
2016-02-10 19:10:04 -0800

    SDC-2452. Add MapR Streams Producer
    Change-Id: If66e6b9903bd07c01a337e100aed1cae43732114
    Reviewed-on: https://review.streamsets.net/2045
    Reviewed-by: Harikiran Nayak <hari@streamsets.com>
    Reviewed-on: https://review.streamsets.net/2160
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

---
+++ maprstreams-target-protolib/src/main/java/com/streamsets/pipeline/stage/destination/maprstreams/MapRStreamsTargetConfigBean.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2015 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.destination.maprstreams;
+
+import com.streamsets.pipeline.api.ConfigDef;
+import com.streamsets.pipeline.api.ConfigDefBean;
+import com.streamsets.pipeline.api.Stage;
+import com.streamsets.pipeline.api.ValueChooserModel;
+import com.streamsets.pipeline.config.DataFormat;
+import com.streamsets.pipeline.kafka.api.KafkaDestinationGroups;
+import com.streamsets.pipeline.stage.destination.lib.DataGeneratorFormatConfig;
+
+import java.util.List;
+
+public class MapRStreamsTargetConfigBean {
+
+  public static final String MAPR_STREAMS_CONFIG_BEAN_PREFIX = "maprStreamsTargetConfigBean.";
+  @ConfigDefBean(groups = {"MAPR_STREAMS"})
+  public MapRStreamsTargetConfig mapRStreamsTargetConfig;
+
+  @ConfigDef(
+    required = true,
+    type = ConfigDef.Type.MODEL,
+    defaultValue = "SDC_JSON",
+    label = "Data Format",
+    description = "",
+    displayPosition = 60,
+    group = "MAPR_STREAMS"
+  )
+  @ValueChooserModel(ProducerDataFormatChooserValues.class)
+  public DataFormat dataFormat;
+
+  @ConfigDefBean(groups = {"MapR Streams"})
+  public DataGeneratorFormatConfig dataGeneratorFormatConfig;
+
+  public void init(Stage.Context context, List<Stage.ConfigIssue> issues) {
+    dataGeneratorFormatConfig.init(
+        context,
+        dataFormat,
+        KafkaDestinationGroups.KAFKA.name(),
+        MAPR_STREAMS_CONFIG_BEAN_PREFIX + "dataGeneratorFormatConfig",
+        issues
+    );
+    mapRStreamsTargetConfig.init(context, dataFormat, issues);
+  }
+
+
+  public void destroy() {
+    mapRStreamsTargetConfig.destroy();
+  }
+}
--- sdc-kafka_0_9-common/src/main/java/com/streamsets/pipeline/kafka/impl/Kafka09Constants.java
+++ sdc-kafka_0_9-common/src/main/java/com/streamsets/pipeline/kafka/impl/Kafka09Constants.java
@@ -26,14 +26,7 @@ public class Kafka09Constants {
   // Producer related Constants
   public static final String BOOTSTRAP_SERVERS_KEY = "bootstrap.servers";
   public static final String KEY_SERIALIZER_KEY = "key.serializer";
-  public static final String KEY_SERIALIZER_DEFAULT = "org.apache.kafka.common.serialization.StringSerializer";
   public static final String VALUE_SERIALIZER_KEY = "value.serializer";
-  public static final String VALUE_SERIALIZER_DEFAULT = "org.apache.kafka.common.serialization.ByteArraySerializer";
-  public static final String REQUEST_REQUIRED_ACKS_KEY = "acks";
-  public static final String REQUEST_REQUIRED_ACKS_DEFAULT = "1";
 
-  public static final String PARTITIONER_CLASS_KEY = "partitioner.class";
-  public static final String RANDOM_PARTITIONER_CLASS = "com.streamsets.pipeline.kafka.impl.RandomPartitioner";
-  public static final String ROUND_ROBIN_PARTITIONER_CLASS = "com.streamsets.pipeline.kafka.impl.RoundRobinPartitioner";
-  public static final String EXPRESSION_PARTITIONER_CLASS = "com.streamsets.pipeline.kafka.impl.ExpressionPartitioner";
+  private Kafka09Constants() {}
 }
