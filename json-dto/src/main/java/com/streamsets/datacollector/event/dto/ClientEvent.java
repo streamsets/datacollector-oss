ba72313028478422333a7956d79f823be557eecb
Virag Kothari <virag@streamsets.com>
2016-03-25 12:15:42 -0700

    SDC-2658 Create ServerEvent and ClientEvent beans in json-dto module
    Change-Id: Id97622333a93a2e43b47ea4081f34e3b387d0e8b
    Reviewed-on: https://review.streamsets.net/2375
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>

---
+++ json-dto/src/main/java/com/streamsets/datacollector/event/dto/ClientEvent.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.datacollector.event.dto;
+
+import java.util.List;
+
+import com.streamsets.datacollector.event.dto.EventType;
+
+public class ClientEvent {
+  private String eventId;
+  private List<String> destinations;
+  private boolean requiresAck;
+  private boolean isAckEvent;
+  private EventType eventType;
+  private String payload;
+
+  public ClientEvent(String eventId,
+    List<String> destinations,
+    boolean requiresAck,
+    boolean isAckEvent,
+    EventType eventType,
+    String payload) {
+    this.eventId = eventId;
+    this.destinations = destinations;
+    this.requiresAck = requiresAck;
+    this.eventType = eventType;
+    this.payload = payload;
+    this.isAckEvent = isAckEvent;
+  }
+
+  public String getEventId() {
+    return eventId;
+  }
+
+  public void setEventId(String eventId) {
+    this.eventId = eventId;
+  }
+
+  public ClientEvent() {
+  }
+
+  public boolean isAckEvent() {
+    return isAckEvent;
+  }
+
+  public void setAckEvent(boolean isAckEvent) {
+    this.isAckEvent = isAckEvent;
+  }
+
+  public List<String> getDestinations() {
+    return destinations;
+  }
+
+  public void setDestinations(List<String> destinations) {
+    this.destinations = destinations;
+  }
+
+  public boolean isRequiresAck() {
+    return requiresAck;
+  }
+
+  public void setRequiresAck(boolean requiresAck) {
+    this.requiresAck = requiresAck;
+  }
+
+  public EventType getEventType() {
+    return eventType;
+  }
+
+  public void setEventType(EventType eventType) {
+    this.eventType = eventType;
+  }
+
+  public String getPayload() {
+    return payload;
+  }
+
+  public void setPayload(String payload) {
+    this.payload = payload;
+  }
+
+}
---
+++ json-dto/src/main/java/com/streamsets/datacollector/event/json/ServerEventJson.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.datacollector.event.json;
+
+import com.streamsets.datacollector.event.dto.EventType;
+
+public class ServerEventJson {
+  private String eventId;
+  private String from;
+  private boolean requiresAck;
+  private boolean isAckEvent;
+  private EventType eventType;
+  private String payload;
+  private long receivedTime;
+
+  public ServerEventJson() {
+  }
+
+  public String getEventId() {
+    return eventId;
+  }
+
+  public void setEventId(String eventId) {
+    this.eventId = eventId;
+  }
+
+  public String getFrom() {
+    return from;
+  }
+
+  public void setFrom(String from) {
+    this.from = from;
+  }
+
+  public boolean isRequiresAck() {
+    return requiresAck;
+  }
+
+  public void setRequiresAck(boolean requiresAck) {
+    this.requiresAck = requiresAck;
+  }
+
+  public boolean isAckEvent() {
+    return isAckEvent;
+  }
+
+  public void setAckEvent(boolean isAckEvent) {
+    this.isAckEvent = isAckEvent;
+  }
+
+  public EventType getEventType() {
+    return eventType;
+  }
+
+  public void setEventType(EventType eventType) {
+    this.eventType = eventType;
+  }
+
+  public String getPayload() {
+    return payload;
+  }
+
+  public void setPayload(String payload) {
+    this.payload = payload;
+  }
+
+  public long getReceivedTime() {
+    return receivedTime;
+  }
+
+  public void setReceivedTime(long receivedTime) {
+    this.receivedTime = receivedTime;
+  }
+}
