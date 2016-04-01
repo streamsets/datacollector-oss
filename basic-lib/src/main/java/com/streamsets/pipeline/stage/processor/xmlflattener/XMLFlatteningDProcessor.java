ba2bea154e6ecf296a7fd89758bfd96eb72988a5
Hari Shreedharan <hshreedharan@streamsets.com>
2016-03-31 21:49:17 -0700

    SDC-2572. Processor for flattening XML objects.
    Change-Id: Ide1096191d2d5baa24696fc1ebc11c75e263e421
    Reviewed-on: https://review.streamsets.net/2452
    Reviewed-by: Bryan Duxbury <bryan@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

---
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/processor/xmlflattener/XMLFlatteningDProcessor.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2015 StreamSets Inc.
+ * <p>
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.processor.xmlflattener;
+
+import com.streamsets.pipeline.api.ConfigDef;
+import com.streamsets.pipeline.api.ConfigGroups;
+import com.streamsets.pipeline.api.FieldSelectorModel;
+import com.streamsets.pipeline.api.GenerateResourceBundle;
+import com.streamsets.pipeline.api.Processor;
+import com.streamsets.pipeline.api.StageDef;
+import com.streamsets.pipeline.configurablestage.DProcessor;
+
+
+@StageDef(
+    version = 1,
+    label = "XML Flattener",
+    description = "Flatten XML data into fields of a record",
+    icon = "xmlparser.png",
+    onlineHelpRefUrl = "index.html#Processors/XMLFlattener.html#task_pmb_l55_sv"
+)
+@ConfigGroups(value = Groups.class)
+@GenerateResourceBundle
+public class XMLFlatteningDProcessor extends DProcessor {
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.MODEL,
+      defaultValue="",
+      label = "XML Field",
+      description = "The field containing XML to flatten",
+      displayPosition = 10,
+      group = "XML"
+  )
+  @FieldSelectorModel(singleValued = true)
+  public String fromField;
+
+  @ConfigDef(
+      required = false,
+      type = ConfigDef.Type.STRING,
+      defaultValue="",
+      label = "Record Delimiter",
+      description = "The tag used to delimit records. If this is not specified, only a single record is generated",
+      displayPosition = 30,
+      group = "XML"
+  )
+  public String recordDelimiter;
+
+  @ConfigDef(
+      required = false,
+      type = ConfigDef.Type.BOOLEAN,
+      defaultValue = "false",
+      label = "Ignore Attributes",
+      description = "Whether attributes of tags should be ignored",
+      displayPosition = 40,
+      group = "XML"
+  )
+  public boolean ignoreAttributes;
+
+  @ConfigDef(
+      required = false,
+      type = ConfigDef.Type.BOOLEAN,
+      defaultValue = "false",
+      label = "Ignore Namespace URI",
+      description = "Whether namespace URIs should be ignored",
+      displayPosition = 50,
+      group = "XML"
+  )
+  public boolean ignoreNamespace;
+
+  @Override
+  protected Processor createProcessor() {
+    return new XMLFlatteningProcessor(fromField, recordDelimiter, ignoreAttributes, ignoreNamespace);
+  }
+}
