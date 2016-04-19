5765162fab1576dbfb39b6e44afde93f16222883
harikiran nayak <hari@streamsets.com>
2016-04-19 13:40:39 -0700

    SDC-2792. Support Kinesis stats aggregator stage
    Change-Id: I3eb0d83fb3fb874038c4890d75a91a1f52e15657
    Reviewed-on: https://review.streamsets.net/2659
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>

---
+++ aws-lib/src/main/java/com/streamsets/pipeline/stage/destination/kinesis/StatsKinesisDTarget.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2015 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.destination.kinesis;
+
+import com.streamsets.pipeline.api.ConfigDefBean;
+import com.streamsets.pipeline.api.ConfigGroups;
+import com.streamsets.pipeline.api.GenerateResourceBundle;
+import com.streamsets.pipeline.api.HideConfigs;
+import com.streamsets.pipeline.api.StageDef;
+import com.streamsets.pipeline.api.StatsAggregatorStage;
+import com.streamsets.pipeline.api.Target;
+import com.streamsets.pipeline.config.DataFormat;
+import com.streamsets.pipeline.configurablestage.DTarget;
+
+@StageDef(
+    version = 3,
+    label = "Kinesis Producer",
+    description = "Writes Pipeline Statistic records to Kinesis",
+    icon = "",
+    upgrader = KinesisTargetUpgrader.class,
+    onlineHelpRefUrl = ""
+)
+@ConfigGroups(value = Groups.class)
+@StatsAggregatorStage
+@HideConfigs(
+  preconditions = true,
+  onErrorRecord = true,
+  value = {"kinesisConfig.dataFormat", "kinesisConfig.preserveOrdering"}
+)
+@GenerateResourceBundle
+public class StatsKinesisDTarget extends DTarget {
+
+  @ConfigDefBean(groups = "KINESIS")
+  public KinesisProducerConfigBean kinesisConfig;
+
+  @Override
+  protected Target createTarget() {
+    kinesisConfig.dataFormat = DataFormat.SDC_JSON;
+    kinesisConfig.preserveOrdering = true;
+    return new KinesisTarget(kinesisConfig);
+  }
+}
