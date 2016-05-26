3c616d29fe42458e8158366eee4fdc72f3d2bf77
Adam Kunicki <adam@streamsets.com>
2016-05-27 00:12:37 +0200

    SDC-3046. SyslogParser does not put mandatory [ ] brackets around IP for IPv6 addresses.
    Change-Id: I5277f5420f2619dc053b18eb84b81e54e63fcad4
    Reviewed-on: https://review.streamsets.net/3006
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

--- basic-lib/src/test/java/com/streamsets/pipeline/lib/parser/syslog/TestSyslogParser.java
+++ basic-lib/src/test/java/com/streamsets/pipeline/lib/parser/syslog/TestSyslogParser.java
@@ -19,15 +19,7 @@
  */
 package com.streamsets.pipeline.lib.parser.syslog;
 
-import com.google.common.base.Charsets;
 import com.google.common.collect.Lists;
-
-import java.net.InetSocketAddress;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import java.util.List;
-
 import com.streamsets.pipeline.api.OnRecordError;
 import com.streamsets.pipeline.api.Record;
 import com.streamsets.pipeline.api.Stage;
@@ -39,6 +31,11 @@ import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 
 public class TestSyslogParser {
 
@@ -92,7 +89,6 @@ public class TestSyslogParser {
   @Test
   public void testMessageParsing() throws Exception {
     SyslogParser parser = new SyslogParser(getContext(), StandardCharsets.UTF_8);
-    Charset charset = Charsets.UTF_8;
     List<String> messages = Lists.newArrayList();
 
     // supported examples from RFC 3164
@@ -133,6 +129,55 @@ public class TestSyslogParser {
         "localhost:50000", records.get(0).get("/senderAddr").getValueAsString());
       Assert.assertNotNull("Failure to parse known-good syslog message",
         records.get(0).get("/host").getValueAsString());
+    }
+  }
+
+  @Test
+  public void testMessageParsingIPv6() throws Exception {
+    SyslogParser parser = new SyslogParser(getContext(), StandardCharsets.UTF_8);
+    List<String> messages = Lists.newArrayList();
+
+    // supported examples from RFC 3164
+    messages.add("<34>Oct 11 22:14:15 mymachine su: 'su root' failed for " +
+        "lonvick on /dev/pts/8");
+    messages.add("<13>Feb  5 17:32:18 10.0.0.99 Use the BFG!");
+    messages.add("<165>Aug 24 05:34:00 CST 1987 mymachine myproc[10]: %% " +
+        "It's time to make the do-nuts.  %%  Ingredients: Mix=OK, Jelly=OK # " +
+        "Devices: Mixer=OK, Jelly_Injector=OK, Frier=OK # Transport: " +
+        "Conveyer1=OK, Conveyer2=OK # %%");
+    messages.add("<0>Oct 22 10:52:12 scapegoat 1990 Oct 22 10:52:01 TZ-6 " +
+        "scapegoat.dmz.example.org 10.1.2.3 sched[0]: That's All Folks!");
+
+    // supported examples from RFC 5424
+    messages.add("<34>1 2003-10-11T22:14:15.003Z mymachine.example.com su - " +
+        "ID47 - BOM'su root' failed for lonvick on /dev/pts/8");
+    messages.add("<165>1 2003-08-24T05:14:15.000003-07:00 192.0.2.1 myproc " +
+        "8710 - - %% It's time to make the do-nuts.");
+
+    // non-standard (but common) messages (RFC3339 dates, no version digit)
+    messages.add("<13>2003-08-24T05:14:15Z localhost snarf?");
+    messages.add("<13>2012-08-16T14:34:03-08:00 127.0.0.1 test shnap!");
+
+    UnpooledByteBufAllocator allocator = new UnpooledByteBufAllocator(false);
+    // test with default keepFields = false
+    for (String msg : messages) {
+      byte[] bytes = msg.getBytes(StandardCharsets.UTF_8);
+      ByteBuf buffer = allocator.buffer(bytes.length);
+      buffer.writeBytes(bytes);
+      List<Record> records = parser.parse(
+          buffer,
+          InetSocketAddress.createUnresolved("::1", 5000),
+          InetSocketAddress.createUnresolved("2001:db8::ff00:42:8329", 50000)
+      );
+      Assert.assertEquals(1, records.size());
+      Assert.assertEquals("Failure to parse known-good syslog message",
+          msg, records.get(0).get("/raw").getValueAsString());
+      Assert.assertEquals("Failure to parse known-good syslog message",
+          "[::1]:5000", records.get(0).get("/receiverAddr").getValueAsString());
+      Assert.assertEquals("Failure to parse known-good syslog message",
+          "[2001:db8::ff00:42:8329]:50000", records.get(0).get("/senderAddr").getValueAsString());
+      Assert.assertNotNull("Failure to parse known-good syslog message",
+          records.get(0).get("/host").getValueAsString());
     }
   }
 }
