7dc000a100d06aef7c7b926ee1e5092a8c65963d
Madhukar Devaraju <madhu@streamsets.com>
2016-05-26 22:38:14 -0700

    SDC-3058. UI: Add UI support for displaying Custom Stage Meters/Histograms/Timers
    Change-Id: I2533f43aa40d823825b6750f7605f913ae0f9a3a
    Reviewed-on: https://review.streamsets.net/3013
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>
    (cherry picked from commit bd8dd532de47acad3a794527b4b785c453b82242)
    Reviewed-on: https://review.streamsets.net/3020

---
+++ datacollector-ui/src/main/webapp/app/home/detail/summary/charts/customHistogram.js
@@ -0,0 +1,106 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Controller for Custom Stage Histogram Chart.
+ */
+
+angular
+  .module('dataCollectorApp.home')
+  .controller('CustomHistogramController', function($scope, $rootScope, pipelineConstant) {
+    angular.extend($scope, {
+      chartOptions: {
+        chart: {
+          type: 'multiBarHorizontalChart',
+          stacked: true,
+          height: 220,
+          showLabels: true,
+          duration: 0,
+          x: function(d) {
+            return d[0];
+          },
+          y: function(d) {
+            return d[1];
+          },
+          showLegend: false,
+          staggerLabels: true,
+          showValues: false,
+          yAxis: {
+            tickValues: 0
+          },
+          margin: {
+            left: 55,
+            top: 20,
+            bottom: 20,
+            right: 20
+          },
+          reduceXTicks: false,
+          showControls: false,
+          tooltip: {
+            valueFormatter: function(d) {
+              return d.toFixed(4);
+            }
+          }
+        }
+      },
+      chartData: [{
+        key: undefined,
+        values: []
+      }],
+      count: 0
+    });
+
+    function updateChartData() {
+      var customStageHistogram = $scope.customStageHistogram,
+        pipelineMetrics = $rootScope.common.pipelineMetrics;
+
+      if(pipelineMetrics && pipelineMetrics.histograms) {
+        var histogramData = pipelineMetrics.histograms[customStageHistogram.histogramKey];
+
+        if(histogramData) {
+          $scope.count = histogramData.count;
+
+          $scope.chartData[0].key = customStageHistogram.label;
+          $scope.chartData[0].values = [
+            ["Mean" , histogramData.mean ],
+            ["Std Dev" , histogramData.stddev ],
+            ["99.9%" , histogramData.p999 ],
+            ["99%" , histogramData.p99 ],
+            ["98%" , histogramData.p98 ],
+            ["95%" , histogramData.p95 ],
+            ["75%" , histogramData.p75 ],
+            ["50%" , histogramData.p50 ]
+          ];
+        }
+
+      }
+    }
+
+    $rootScope.$watch('common.pipelineMetrics', function() {
+      if($scope.isPipelineRunning &&
+        $rootScope.common.pipelineMetrics &&
+        $scope.selectedType === pipelineConstant.STAGE_INSTANCE &&
+        !$scope.monitoringPaused) {
+        updateChartData();
+      }
+    });
+
+    updateChartData();
+  });
---
+++ datacollector-ui/src/main/webapp/app/home/detail/summary/charts/customMeter.js
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Controller for Custom Stage Meter Bar Chart.
+ */
+
+angular
+  .module('dataCollectorApp.home')
+  .controller('CustomMeterController', function($scope, $rootScope, pipelineConstant) {
+
+    var yAxisLabel = '( events / sec )';
+
+    angular.extend($scope, {
+      chartOptions: {
+        chart: {
+          type: 'discreteBarChart',
+          height: 220,
+          showLabels: true,
+          duration: 0,
+          x: function(d) {
+            return d[0];
+          },
+          y: function(d) {
+            return d[1];
+          },
+          //showLegend: true,
+          staggerLabels: false,
+          showValues: true,
+          yAxis: {
+            tickValues: 0,
+            axisLabel: yAxisLabel,
+            axisLabelDistance: -10
+          },
+          valueFormat: function(d) {
+            return d3.format(',d')(d);
+          },
+          margin: {
+            left: 55,
+            top: 5,
+            bottom: 25,
+            right: 20
+          },
+          tooltip: {
+            valueFormatter: function(d) {
+              return d.toFixed(4);
+            }
+          }
+        }
+      },
+      chartData: [{
+        key: undefined,
+        values: []
+      }],
+      count: 0
+    });
+
+    function updateChartData() {
+      var customStageMeter = $scope.customStageMeter,
+        pipelineMetrics = $rootScope.common.pipelineMetrics;
+
+      if(pipelineMetrics && pipelineMetrics.meters) {
+        var meterData = pipelineMetrics.meters[customStageMeter.meterKey];
+
+        if(meterData) {
+          $scope.count = meterData.count;
+
+          $scope.chartData[0].key = customStageMeter.label;
+          $scope.chartData[0].values = [
+            ["1m" , meterData.m1_rate ],
+            ["5m" , meterData.m5_rate ],
+            ["15m" , meterData.m15_rate ],
+            ["30m" , meterData.m30_rate ],
+            ["1h" , meterData.h1_rate ],
+            ["6h" , meterData.h6_rate ],
+            ["12h" , meterData.h12_rate ],
+            ["1d" , meterData.h24_rate ],
+            ["Mean" , meterData.mean_rate ]
+          ];
+        }
+
+      }
+    }
+
+    $rootScope.$watch('common.pipelineMetrics', function() {
+      if($scope.isPipelineRunning &&
+        $rootScope.common.pipelineMetrics &&
+        $scope.selectedType === pipelineConstant.STAGE_INSTANCE &&
+        !$scope.monitoringPaused) {
+        updateChartData();
+      }
+    });
+
+    updateChartData();
+  });
