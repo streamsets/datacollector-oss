ee9743aef30d006a1d0b97d465f97b06702301ce
Madhukar Devaraju <madhu@streamsets.com>
2016-07-01 10:17:15 -0700

    SDC-3392. Add CLI support for enabling DPM
    Added new CLI command for enabling DPM.
    
    bin/streamsets cli -U http://localhost:18630 system enableDPM --dpmUrl
    http://localhost:18631 --dpmUser demo@demo --dpmPassword demo@demo
    --labels cli1,cli2
    
    ➜  streamsets-datacollector-1.6.0.0-SNAPSHOT git:(master) ✗
    bin/streamsets cli help system enableDPM
    NAME
            cli system enableDPM - Enable DPM
    
    SYNOPSIS
            cli [(-a <sdcAuthType> | --auth-type <sdcAuthType>)]
                    [(-p <sdcPassword> | --password <sdcPassword>)]
                    [(-u <sdcUser> | --user <sdcUser>)] (-U <sdcURL> |
    --url <sdcURL>)
                    system enableDPM --dpmPassword <dpmUserPassword>
    --dpmUrl <dpmBaseURL>
                    --dpmUser <dpmUserID> [--labels <labels>] [--stack]
    
    OPTIONS
            -a <sdcAuthType>, --auth-type <sdcAuthType>
                Data Collector Authentication Type
    
            --dpmPassword <dpmUserPassword>
                DPM password
    
            --dpmUrl <dpmBaseURL>
                DPM URL
    
            --dpmUser <dpmUserID>
                DPM User ID
    
            --labels <labels>
                Labels to report the remote control system
    
            -p <sdcPassword>, --password <sdcPassword>
                Data Collector password
    
            --stack
                Print a stack trace when exiting with a warning or fatal
    error.
    
            -u <sdcUser>, --user <sdcUser>
                Data Collector User
    
            -U <sdcURL>, --url <sdcURL>
                Data Collector URL
    
    Change-Id: I3c15c14bda84436a813fd3dcd7a11cbf2015fd7d
    Reviewed-on: https://review.streamsets.net/3417
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>
    (cherry picked from commit b19ae9d5c241f86f6e982ff774fa03581dd8350e)
    Reviewed-on: https://review.streamsets.net/3579

---
+++ cli/src/main/java/com/streamsets/datacollector/client/cli/command/system/EnableDPMCommand.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.datacollector.client.cli.command.system;
+
+import com.streamsets.datacollector.client.api.SystemApi;
+import com.streamsets.datacollector.client.cli.command.BaseCommand;
+import com.streamsets.datacollector.client.model.DPMInfoJson;
+import io.airlift.airline.Command;
+import io.airlift.airline.Option;
+
+import java.util.Arrays;
+
+@Command(name = "enableDPM", description = "Enable DPM")
+public class EnableDPMCommand extends BaseCommand {
+
+  @Option(
+      name = {"--dpmUrl"},
+      description = "DPM URL",
+      required = true
+  )
+  public String dpmBaseURL;
+
+  @Option(
+      name = {"--dpmUser"},
+      description = "DPM User ID",
+      required = true
+  )
+  public String dpmUserID;
+
+  @Option(
+      name = {"--dpmPassword"},
+      description = "DPM password",
+      required = true
+  )
+  public String dpmUserPassword;
+
+  @Option(
+      name = {"--labels"},
+      description = "Labels to report the remote control system",
+      required = false
+  )
+  public String labels;
+
+  @Override
+  public void run() {
+    SystemApi systemApi = new SystemApi(getApiClient());
+    try {
+      String organization = getOrganizationId();
+      if (organization == null) {
+        System.out.println("DPM User ID must be <ID>@<Organization ID>");
+        return;
+      }
+
+      DPMInfoJson dpmInfo = new DPMInfoJson();
+      dpmInfo.setBaseURL(dpmBaseURL);
+      dpmInfo.setUserID(dpmUserID);
+      dpmInfo.setUserPassword(dpmUserPassword);
+      dpmInfo.setOrganization(organization);
+      if (labels != null && labels.trim().length() > 0) {
+        dpmInfo.setLabels(Arrays.asList(labels.split(",")));
+      }
+
+      systemApi.enableDPM(dpmInfo);
+      System.out.println("Successfully generated application token and updated the file 'etc/application-token.txt' " +
+          "and 'etc/dpm.properties'. Restart the Data Collector to register Application Token.");
+    } catch (Exception ex) {
+      if(printStackTrace) {
+        ex.printStackTrace();
+      } else {
+        System.out.println(ex.getMessage());
+      }
+    }
+  }
+
+  private String getOrganizationId() {
+    if (dpmUserID != null) {
+      String[] parts = dpmUserID.split("@");
+      if( parts.length == 2) {
+        return parts[1];
+      }
+    }
+    return null;
+  }
+}
