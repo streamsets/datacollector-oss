3e3a3ba1ba1ced1aac036c27da740335163ad802
Madhukar Devaraju <madhu@streamsets.com>
2016-07-02 08:51:52 -0700

    SDC-3395. Rename "Kinesis Producer" statistics aggregator to "Write to Kinesis"
    Change-Id: I7ce17c65fdc615474b8b3fd72740b5900565333a
    Reviewed-on: https://review.streamsets.net/3432
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>
    (cherry picked from commit c1499ade9309ef8d4777359048ba831fcfd57404)
    Reviewed-on: https://review.streamsets.net/3581

--- aws-lib/src/main/java/com/streamsets/pipeline/stage/destination/kinesis/StatsKinesisDTarget.java
+++ aws-lib/src/main/java/com/streamsets/pipeline/stage/destination/kinesis/StatsKinesisDTarget.java
@@ -31,7 +31,7 @@ import com.streamsets.pipeline.configurablestage.DTarget;
 
 @StageDef(
     version = 3,
-    label = "Kinesis Producer",
+    label = "Write to Kinesis",
     description = "Writes Pipeline Statistic records to Kinesis",
     icon = "",
     upgrader = KinesisTargetUpgrader.class,
