29b677175fa7ba98deeb2c49e2ddb8fe2a8c6d4b
Madhukar Devaraju <madhu@streamsets.com>
2016-07-06 16:10:00 -0700

    SDC-2948. Show "connection lost" instead of losing the whole page when frontend loses connection to backend
    Change-Id: I8bdef7dd398d8a7100ed9d7f2fe0377ed6178632
    Reviewed-on: https://review.streamsets.net/3460
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>

---
+++ common-ui/src/main/webapp/common/administration/connectionLost/connectionLost.js
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Controller for Connection to server lost Modal Dialog.
+ */
+
+angular
+  .module('dataCollectorApp')
+  .controller('ConnectionLostModalInstanceController', function (
+    $scope,
+    $rootScope,
+    $modalInstance,
+    $modalStack,
+    $timeout,
+    api
+  ) {
+    angular.extend($scope, {
+      nextRetryInSeconds: 0,
+      retryCountDown: 0,
+      common: {
+        errors: []
+      },
+      isRetryingInProgress: false,
+
+      retryNow: function() {
+        retryToConnect();
+      },
+
+      refreshBrowser: function() {
+        window.location.reload();
+      }
+    });
+
+    var nextRetryInSeconds = 1;
+    var maxAttempts = 5;
+    var attempts = 0;
+    var retryCountDownTimer;
+    var updateRetryCountdown = function() {
+      $scope.retryCountDown = Math.min(nextRetryInSeconds, 16);
+
+      if(retryCountDownTimer) {
+        $timeout.cancel(retryCountDownTimer);
+      }
+
+      var retryCountDownCallback = function(){
+        $scope.retryCountDown--;
+        if($scope.retryCountDown > 0) {
+          retryCountDownTimer = $timeout(retryCountDownCallback,1000);
+        } else {
+          $scope.retryCountDown = 0;
+          retryToConnect();
+        }
+      };
+      retryCountDownTimer = $timeout(retryCountDownCallback, 1000);
+    };
+
+    var retryToConnect = function() {
+      if (attempts > maxAttempts) {
+        window.location.reload();
+      }
+      attempts++;
+      nextRetryInSeconds *= 2;
+
+      $scope.isRetryingInProgress = true;
+      api.admin.getUserInfo()
+        .success(function(res) {
+          window.location.reload();
+        })
+        .error(function(res) {
+          $scope.isRetryingInProgress = false;
+          updateRetryCountdown();
+        });
+    };
+
+    retryToConnect();
+  });
