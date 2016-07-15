3f0dab5dbdeed1c8f2c16ccca1095a24dd3f83da
harikiran nayak <hari@streamsets.com>
2016-07-14 23:12:12 -0700

    SDC-3454. Move Snappy reader and writer interceptors to container-common module
    	- Add SnappyReaderInterceptor and SnappyWriterInterceptor in container-common module
    	- Add unit tests
    	- Make HttpTarget use this SnappyWriterInterceptor
    
    Change-Id: I864bdf9e8d08f1cd2b91a1e1b6827371db109927
    Reviewed-on: https://review.streamsets.net/3568
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Harikiran Nayak <hari@streamsets.com>

---
+++ container-common/src/test/java/com/streamsets/datacollector/http/TestSnappyReaderInterceptor.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.datacollector.http;
+
+import org.iq80.snappy.SnappyFramedInputStream;
+import org.iq80.snappy.SnappyFramedOutputStream;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.ReaderInterceptorContext;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class TestSnappyReaderInterceptor {
+
+  @Test
+  public void testSnappyReaderInterceptor() throws IOException {
+
+    SnappyReaderInterceptor readerInterceptor = new SnappyReaderInterceptor();
+
+    MultivaluedMap<String, String> headers = new MultivaluedHashMap<>();
+    headers.put(SnappyReaderInterceptor.CONTENT_ENCODING, Arrays.asList(SnappyReaderInterceptor.SNAPPY));
+
+    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    SnappyFramedOutputStream snappyFramedOutputStream = new SnappyFramedOutputStream(byteArrayOutputStream);
+    snappyFramedOutputStream.write("Hello".getBytes());
+    ByteArrayInputStream inputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+
+    ReaderInterceptorContext mockInterceptorContext = Mockito.mock(ReaderInterceptorContext.class);
+    Mockito.when(mockInterceptorContext.getHeaders()).thenReturn(headers);
+    Mockito.when(mockInterceptorContext.getInputStream()).thenReturn(inputStream);
+    Mockito.doNothing().when(mockInterceptorContext).setInputStream(Mockito.any(InputStream.class));
+
+    // call aroundReadFrom on mock
+    readerInterceptor.aroundReadFrom(mockInterceptorContext);
+
+    // verify that setInputStream method was called once with argument which is an instance of SnappyFramedInputStream
+    Mockito.verify(mockInterceptorContext, Mockito.times(1))
+      .setInputStream(Mockito.any(SnappyFramedInputStream.class));
+
+  }
+}
---
+++ container-common/src/test/java/com/streamsets/datacollector/http/TestSnappyWriterInterceptor.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.datacollector.http;
+
+import org.iq80.snappy.SnappyFramedOutputStream;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.WriterInterceptorContext;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class TestSnappyWriterInterceptor {
+
+  @Test
+  public void testSnappyWriterInterceptor() throws IOException {
+
+    SnappyWriterInterceptor writerInterceptor = new SnappyWriterInterceptor();
+
+    MultivaluedMap<String, Object> headers = new MultivaluedHashMap<>();
+    WriterInterceptorContext mockInterceptorContext = Mockito.mock(WriterInterceptorContext.class);
+    Mockito.when(mockInterceptorContext.getHeaders()).thenReturn(headers);
+    Mockito.when(mockInterceptorContext.getOutputStream()).thenReturn(new ByteArrayOutputStream());
+    Mockito.doNothing().when(mockInterceptorContext).setOutputStream(Mockito.any(OutputStream.class));
+
+    // call aroundWriteTo on mock
+    writerInterceptor.aroundWriteTo(mockInterceptorContext);
+
+    // verify that setOutputStream method was called once with argument which is an instance of SnappyFramedOutputStream
+    Mockito.verify(mockInterceptorContext, Mockito.times(1))
+      .setOutputStream(Mockito.any(SnappyFramedOutputStream.class));
+
+  }
+}
