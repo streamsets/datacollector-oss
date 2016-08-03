f99e405d4c477404982e7b38f86229c7af7adfd4
Jarek Jarcec Cecho <jarcec@streamsets.com>
2016-08-03 16:04:11 -0700

    SDC-3617: DPM in CM: Create base code to register SDC in DPM
    I've added several new DPM-related properties to our descriptor file for
    Cloudera Manager. As part of those properties user needs to specify if
    DPM should be enabled and if so, it's URL and credentials to a role that
    have ability to register SDC. Then on next restart of the service, we
    use DPM REST calls to pre-generate the app token and store it outside of
    CM space (so that it can be reused on service restart) and configure SDC
    to use this token.
    
    The whole process is thus driven from CM and user don't have to do any
    manual intervention outside of filling the CM configs and restarting the
    service. This patch doesn't contain ability to re-generate the token, that will
    be a follow up work.
    
    Change-Id: I83007ca8e69dedd7f46638b1bf5a87eeb6af602d
    Reviewed-on: https://review.streamsets.net/3819
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Adam Kunicki <adam@streamsets.com>
    Reviewed-by: Virag Kothari <virag@streamsets.com>

--- cloudera-integration/csd/src/main/scripts/control.sh
+++ cloudera-integration/csd/src/main/scripts/control.sh
@@ -31,6 +31,30 @@ function log {
   echo "$timestamp: $1" 1>&2; #stderr
 }
 
+# Dump environmental variables
+log "CONF_DIR: $CONF_DIR"
+log "SDC_LOG: $SDC_LOG"
+log "SDC_DATA: $SDC_DATA"
+log "SDC_RESOURCES: $SDC_RESOURCES"
+log "CONFIGURED_USERS: $CONFIGURED_USERS"
+log "AUTH_TYPE: $AUTH_TYPE"
+log "FILE_AUTH_TYPE: $FILE_AUTH_TYPE"
+log "LOGIN_MODULE: $LOGIN_MODULE"
+log "DPM_TOKEN_FILE: $DPM_TOKEN_FILE"
+log "DPM_BASE_URL: $DPM_BASE_URL"
+log "DPM_USER: $DPM_USER"
+log "DPM_PASSWORD: (omitted)"
+log "SDC_JAVA_OPTS: $SDC_JAVA_OPTS"
+log "SDC_CURL_OPTS: $SDC_CURL_OPTS"
+log "DPM_TOKEN_REGENERATE: $DPM_TOKEN_REGENERATE"
+log "DEBUG: $DEBUG"
+
+# If we're in debug mode, enable printing each executed command
+if [[ $DEBUG = "true" ]]; then
+  set -x
+  CURL_DEBUG="-v"
+fi
+
 function update_users {
   IFS=';' read -r -a array <<< "$CONFIGURED_USERS"
   for element in "${array[@]}"; do
@@ -70,37 +94,193 @@ function create_config_symlinks {
   fi
 }
 
+# Start SDC (exec into it)
+function start {
+  log "Starting StreamSets Data Collector"
+
+  # If we have DPM enabled, make sure that this SDC is registered
+  if [[ $DPM_ENABLED = "true" ]]; then
+    dpm
+  fi
+
+  if [[ "$LOGIN_MODULE" = "file" ]]; then
+    update_users
+  else
+    generate_ldap_configs
+  fi
+
+  create_config_symlinks
+
+  source "$CONF_DIR"/sdc-env.sh
+  exec $SDC_DIST/bin/streamsets dc -verbose -skipenvsourcing -exec
+}
+
+# Execute CURL and die the script if the CURL execution failed
+function run_curl {
+  method=$1
+  url=$2
+  payload=$3
+  extraArgs=$4
+
+  log "Executing curl $method to $url"
+  output=`curl $CURL_DEBUG $SDC_CURL_OPTS -S -X "$method" -d "$payload" "$url" -H 'Content-Type:application/json' -H 'X-Requested-By:SDC' $extraArgs`
+  if [ $? -ne 0 ]; then
+    log "Failed $method to $url"
+    exit 1
+  fi
+}
+
+# Print out DPM's login URL
+function dpm_url_login {
+  echo ${DPM_BASE_URL}/security/public-rest/v1/authentication/login
+}
+
+# Print out DPM's token generation URL
+function dpm_url_token_gen {
+  echo ${DPM_BASE_URL}/security/rest/v1/organization/$dpmOrg/components
+}
+
+# Print out DPM's logout URL
+function dpm_url_logout {
+  echo ${DPM_BASE_URL}/security/_logout
+}
+
+# Create DPM session and sets in in shared variable dpmSession
+function dpm_login {
+  log "Creating DPM session"
+  run_curl "POST" "$(dpm_url_login)" "{\"userName\":\"$DPM_USER\", \"password\": \"$DPM_PASSWORD\"}" "-D -"
+  # Session is stored in SS_SSO_LOGIN header and needs to be extracted
+  dpmSession=$(echo $output | grep SS-SSO-LOGIN | sed -e 's/[^=]*=//' -e 's/;.*//')
+
+  if [ -z "$dpmSession" ]; then
+    log "Can't open DPM session: $output"
+    exit 1
+  fi
+
+  log "Opened DPM session: $dpmSession"
+}
+
+# Log out DPM session
+function dpm_logout {
+  log "Logging out session: $dpmSession"
+  run_curl "GET" "$(dpm_url_logout)" "" "--header X-SS-User-Auth-Token:$dpmSession"
+}
+
+# Generate new DPM application token and fill it into token variable
+function dpm_generate_token {
+  log "Generating new token in session $dpmSession and in org $dpmOrg"
+  run_curl "PUT" "$(dpm_url_token_gen)" "{\"organization\": \"$dpmOrg\", \"componentType\" : \"dc\", \"numberOfComponents\" : 1, \"active\" : true}" "--header X-SS-REST-CALL:true --header X-SS-User-Auth-Token:$dpmSession"
+  token=$(echo $output | sed -e 's/.*"fullAuthToken":"//' -e 's/".*//')
+
+  if [ -z "$token" ]; then
+    log "Can't generate new token"
+    exit 1
+  fi
+}
+
+# Validate that we have all variables that are required for DPM (to register and such)
+function dpm_verify_config {
+  die="false"
+  log "Validating DPM configuration"
+
+  if [ -z "$DPM_BASE_URL" ]; then
+    log "Configuration 'dpm.base.url' is not properly set."
+    die="true"
+  fi
+  if [ -z "$DPM_TOKEN_FILE" ]; then
+    log "Configuration 'dpm.token.path' is not properly set."
+    die="true"
+  fi
+  if [ -z "$DPM_USER" ]; then
+    log "Configuration 'dpm.user' is not properly set."
+    die="true"
+  fi
+
+  # Calculate DPM organization
+  dpmOrg=`echo $DPM_USER | cut -f2 -d@`
+  if [ -z "$dpmOrg" ]; then
+    log "Configuration 'dpm.user' doesn't properly contain organization."
+    die="true"
+  fi
+  log "DPM organization is $dpmOrg"
+
+  if [ $die = "true" ]; then
+    log "Invalid configuration for DPM"
+    exit 1
+  fi
+}
+
+# Register or regenerate auth token for this SDC instance in DPM
+function dpm {
+  # TODO We should enable forced update
+  if [[ -f $DPM_TOKEN_FILE ]]; then
+    echo "DPM token already exists, skipping for now."
+    return
+  fi
+
+  dpm_verify_config
+  dpm_login
+
+  # Based on whether the token file already exists
+  if [ -f $DPM_TOKEN_FILE ]; then
+    # TODO Prepared for forced update
+    echo "DPM token already exists, skipping for now."
+  else
+    # Application token doesn't exists yet, we need to generate it
+    dpm_generate_token
+    mkdir -p "$(dirname $DPM_TOKEN_FILE)"
+    echo $token > $DPM_TOKEN_FILE
+  fi
+
+  dpm_logout
+}
+
 export SDC_CONF=$CONF_DIR
 
-# Propagate system white and black lists
 SDC_PROPERTIES=$SDC_CONF/sdc.properties
-if ! grep -q "system.stagelibs.*list" $SDC_PROPERTIES; then
-  echo "System white nor black list found in configuration"
-  if [ -f $SDC_DIST/etc/sdc.properties ]; then
-    echo "Propagating default white and black list from parcel"
-    grep "system.stagelibs.*list" $SDC_DIST/etc/sdc.properties >> $SDC_PROPERTIES
-  else
-    echo "Parcel doesn't contain default configuration file, skipping white/black list propagation"
+if [ -f $SDC_PROPERTIES ]; then
+  # Propagate system white and black lists
+  if ! grep -q "system.stagelibs.*list" $SDC_PROPERTIES; then
+    echo "System white nor black list found in configuration"
+    if [ -f $SDC_DIST/etc/sdc.properties ]; then
+      echo "Propagating default white and black list from parcel"
+      grep "system.stagelibs.*list" $SDC_DIST/etc/sdc.properties >> $SDC_PROPERTIES
+    else
+      echo "Parcel doesn't contain default configuration file, skipping white/black list propagation"
+    fi
   fi
+
+  # Detect if this is a DPM enabled deployment
+  if grep -q "dpm.enabled=true" $SDC_PROPERTIES; then
+    log "Detected DPM environment"
+    DPM_ENABLED="true"
+  else
+    log "Running in non-DPM environment"
+  fi
+
+  # CM exposes DPM token config as path to file, so we need to convert it to
+  # the actual value that is expected by SDC. We will append it to the config
+  # only if we're actually running with DPM enabled, otherwise SDC can fail
+  # to start if given file doesn't exists.
+  if [[ $DPM_ENABLED = "true" ]]; then
+    echo "dpm.appAuthToken=@$DPM_TOKEN_FILE@" >> $SDC_PROPERTIES
+  fi
+
 fi
 
+log "Executing command '$CMD'"
 case $CMD in
+  start)
+    start
+    ;;
 
-  (start)
-    log "Starting StreamSets Data Collector"
-    if [[ "$LOGIN_MODULE" = "file" ]]; then
-      update_users
-    else
-      generate_ldap_configs
-    fi
-
-    create_config_symlinks
-
-    source "$CONF_DIR"/sdc-env.sh
-    exec $SDC_DIST/bin/streamsets dc -verbose -skipenvsourcing -exec
-
-  (update_users)
+  update_users)
     update_users
     exit 0
+    ;;
 
+  dpm)
+    dpm
+    exit 0
+    ;;
 esac
