08bbbe6372a2243754967cd56feb303c0d667e4f
Alejandro Abdelnur <tucu@streamsets.com>
2016-08-22 23:05:03 -0500

    SDC-3745. Lightweight REST client for RemoteSSOService
      A minimal dependencies RestClient
    
    Change-Id: Ib6bd42cb013489632d33f885905a617e74ccfebd
    Reviewed-on: https://review.streamsets.net/4065
    Reviewed-by: Alejandro Abdelnur <tucu@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

---
+++ sso/src/main/java/com/streamsets/lib/security/http/RestClient.java
@@ -0,0 +1,345 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.lib.security.http;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.annotations.VisibleForTesting;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A REST Client with minimal dependencies (JDK & JACKSON -for JSON- )
+ */
+public class RestClient {
+
+  @VisibleForTesting
+  static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+  static final String CONTENT_TYPE = "content-type";
+  static final String ACCEPT = "accept";
+  static final String APPLICATION_JSON = "application/json";
+
+  public static class Builder {
+
+    @VisibleForTesting
+    String name;
+    final String baseUrl;
+    String path;
+    boolean csrf;
+    boolean json;
+    String componentId;
+    String appAuthToken;
+    Map<String, List<String>> headers;
+    Map<String, List<String>> queryParams;
+    int timeoutMillis;
+    ObjectMapper jsonMapper;
+
+    private Builder(String baseUrl) {
+      name = "RestClient";
+      this.baseUrl = (baseUrl.endsWith("/")) ? baseUrl : baseUrl + "/";
+      path = null;
+      csrf = true;
+      json = true;
+      headers = new HashMap<>();
+      queryParams = new HashMap<>();
+      timeoutMillis = 30 * 1000;
+      jsonMapper = OBJECT_MAPPER;
+    }
+
+    public Builder name(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public Builder path(String path) {
+      this.path = path;
+      return this;
+    }
+
+    public Builder csrf(boolean csrf) {
+      this.csrf = csrf;
+      return this;
+    }
+
+    public Builder jsonMapper(ObjectMapper mapper) {
+      this.jsonMapper = mapper;
+      return this;
+    }
+
+    public Builder componentId(String componentId) {
+      this.componentId = componentId;
+      return this;
+    }
+
+    public Builder appAuthToken(String appAuthToken) {
+      this.appAuthToken = appAuthToken;
+      return this;
+    }
+
+    public Builder json(boolean json) {
+      this.json = json;
+      return this;
+    }
+
+    Builder map(Map<String, List<String>> map, String name, String value) {
+      if (value == null) {
+        map.remove(name);
+      } else {
+        List<String> values = map.get(name);
+        if (values == null) {
+          values = new ArrayList<>();
+          map.put(name, values);
+        }
+        values.add(value);
+      }
+      return this;
+    }
+
+    public Builder header(String name, String value) {
+      return map(headers, name.toLowerCase(), value);
+    }
+
+    public Builder queryParam(String name, String value) {
+      return map(queryParams, name, value);
+    }
+
+    public Builder timeout(int timeoutMillis) {
+      this.timeoutMillis = timeoutMillis;
+      return this;
+    }
+
+    public RestClient build() throws IOException {
+      return new RestClient(name,
+          baseUrl,
+          path,
+          componentId,
+          appAuthToken,
+          csrf,
+          json,
+          headers,
+          queryParams,
+          timeoutMillis,
+          jsonMapper
+      );
+    }
+
+  }
+
+  public static class Response {
+
+    @VisibleForTesting
+    final ObjectMapper jsonMapper;
+    final HttpURLConnection conn;
+
+    Response(ObjectMapper jsonMapper, HttpURLConnection conn) {
+      this.jsonMapper = jsonMapper;
+      this.conn = conn;
+    }
+
+    public HttpURLConnection getConnection() {
+      return conn;
+    }
+
+    public int getStatus() throws IOException {
+      return conn.getResponseCode();
+    }
+
+    public String getContentType() {
+      return conn.getContentType();
+    }
+
+    public boolean isJson() {
+      String contentType = conn.getContentType();
+      return contentType != null & contentType.toLowerCase().trim().startsWith(APPLICATION_JSON);
+    }
+
+    public String getHeader(String headerName) {
+      return conn.getHeaderField(headerName);
+    }
+
+    public Map<String, List<String>> getHeaders() {
+      return conn.getHeaderFields();
+    }
+
+    public InputStream getInputStream() throws IOException {
+      return conn.getInputStream();
+    }
+
+    public <T> T getData(Class<T> klass) throws IOException {
+      if (isJson()) {
+        return jsonMapper.readValue(getInputStream(), klass);
+      } else {
+        throw new IllegalStateException("Response is not application/json, is " + conn.getContentType());
+      }
+    }
+
+    public Map getError() throws IOException {
+      Map map;
+      if (isJson()) {
+        map = jsonMapper.readValue(conn.getErrorStream(), Map.class);
+      } else {
+        map = new HashMap();
+        map.put("message", conn.getResponseMessage());
+      }
+      return map;
+    }
+
+  }
+
+  public static Builder builder(String baseUrl) {
+    return new Builder(baseUrl);
+  }
+
+  @VisibleForTesting
+  final String baseUrl;
+  final Map<String, List<String>> headers;
+  final Map<String, List<String>> queryParams;
+  final String path;
+  final boolean json;
+  final int timeoutMillis;
+  final ObjectMapper jsonMapper;
+  HttpURLConnection conn;
+
+  Map<String, List<String>> deepCopy(Map<String, List<String>> map) {
+    Map<String, List<String>> copy = new HashMap();
+    for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+      copy.put(entry.getKey(), new ArrayList<String>(entry.getValue()));
+    }
+    return copy;
+  }
+
+  private RestClient(
+      String name,
+      String baseUrl,
+      String path,
+      String componentId,
+      String appAuthToken,
+      boolean csrf,
+      boolean json,
+      Map<String, List<String>> headers,
+      Map<String, List<String>> queryParams,
+      int timeoutMillis,
+      ObjectMapper jsonMapper
+  ) throws IOException {
+    this.baseUrl = baseUrl;
+    this.path = path;
+    this.headers = deepCopy(headers);
+    if (csrf && !this.headers.containsKey(SSOConstants.X_REST_CALL.toLowerCase())) {
+      this.headers.put(SSOConstants.X_REST_CALL.toLowerCase(), Collections.singletonList(name));
+    }
+    this.json = json;
+    if (json && !this.headers.containsKey(CONTENT_TYPE)) {
+      this.headers.put(CONTENT_TYPE.toLowerCase(), Collections.singletonList(APPLICATION_JSON));
+      this.headers.put(ACCEPT, Collections.singletonList(APPLICATION_JSON));
+    }
+    if (componentId != null && appAuthToken != null) {
+      this.headers.put(SSOConstants.X_APP_COMPONENT_ID.toLowerCase(), Collections.singletonList(componentId));
+      this.headers.put(SSOConstants.X_APP_AUTH_TOKEN.toLowerCase(), Collections.singletonList(appAuthToken));
+    }
+    this.queryParams = deepCopy(queryParams);
+    this.timeoutMillis = timeoutMillis;
+    this.jsonMapper = jsonMapper;
+    reset();
+  }
+
+  HttpURLConnection createConnection() throws IOException {
+    URL url = new URL(baseUrl);
+    if (path != null) {
+      url = new URL(url, path);
+    }
+    if (!queryParams.isEmpty()) {
+      StringBuilder sb = new StringBuilder();
+      String separator = "?";
+      for (Map.Entry<String, List<String>> param : queryParams.entrySet()) {
+        for (String value : param.getValue()) {
+          sb
+              .append(separator)
+              .append(URLEncoder.encode(param.getKey(), "UTF-8"))
+              .append("=")
+              .append(URLEncoder.encode(value, "UTF-8"));
+          separator = "&";
+        }
+      }
+      url = new URL(sb.insert(0, url.toExternalForm()).toString());
+    }
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    for (Map.Entry<String, List<String>> header : headers.entrySet()) {
+      for (String value : header.getValue()) {
+        conn.addRequestProperty(header.getKey(), value);
+      }
+    }
+    conn.setConnectTimeout(timeoutMillis);
+    conn.setReadTimeout(timeoutMillis);
+    conn.setDefaultUseCaches(false);
+    return conn;
+  }
+
+  public void reset() throws IOException {
+    conn = createConnection();
+  }
+
+  public HttpURLConnection getConnection() {
+    return conn;
+  }
+
+  Response doHttp(HttpURLConnection conn, String method, Object upload) throws IOException {
+    conn.setRequestMethod(method);
+    if (upload != null) {
+      if (json) {
+        jsonMapper.writeValue(conn.getOutputStream(), upload);
+      } else {
+        throw new IllegalStateException("Content type is not JSON, cannot upload data bean");
+      }
+    }
+    return new Response(jsonMapper, conn);
+  }
+
+  public Response get() throws IOException {
+    getConnection().setDoOutput(true);
+    return doHttp(getConnection(), "GET", null);
+  }
+
+  public <T> Response post(T data) throws IOException {
+    getConnection().setDoOutput(true);
+    getConnection().setDoInput(true);
+    return doHttp(getConnection(), "POST", data);
+  }
+
+  public <T> Response put(T data) throws IOException {
+    getConnection().setDoOutput(true);
+    getConnection().setDoInput(true);
+    return doHttp(getConnection(), "PUT", data);
+  }
+
+  public Response delete() throws IOException {
+    getConnection().setDoOutput(true);
+    return doHttp(getConnection(), "DELETE", null);
+  }
+
+}
---
+++ sso/src/test/java/com/streamsets/lib/security/http/TestRestClient.java
@@ -0,0 +1,276 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.lib.security.http;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TestRestClient {
+
+  @Test
+  public void testBuilderAndClientCreation() throws Exception {
+    RestClient.Builder builder = RestClient.builder("http://foo/bar");
+
+    Assert.assertEquals("RestClient", builder.name);
+    builder.name("name");
+    Assert.assertEquals("name", builder.name);
+
+    Assert.assertEquals("http://foo/bar/", builder.baseUrl);
+
+    Assert.assertNull(builder.path);
+    builder.path("path");
+    Assert.assertEquals("path", builder.path);
+
+    Assert.assertTrue(builder.csrf);
+    builder.csrf(false);
+    Assert.assertFalse(builder.csrf);
+
+    Assert.assertTrue(builder.json);
+    builder.json(false);
+    Assert.assertFalse(builder.json);
+
+    Assert.assertNull(builder.componentId);
+    builder.componentId("componentId");
+    Assert.assertEquals("componentId", builder.componentId);
+
+    Assert.assertNull(builder.appAuthToken);
+    builder.appAuthToken("appAuthToken");
+    Assert.assertEquals("appAuthToken", builder.appAuthToken);
+
+    Assert.assertTrue(builder.headers.isEmpty());
+    builder.header("a", "A");
+    Assert.assertEquals(1, builder.headers.size());
+    Assert.assertTrue(builder.headers.containsKey("a"));
+    Assert.assertEquals(ImmutableList.of("A"), builder.headers.get("a"));
+
+    Assert.assertTrue(builder.queryParams.isEmpty());
+    builder.queryParam("b", "B");
+    Assert.assertEquals(1, builder.queryParams.size());
+    Assert.assertTrue(builder.queryParams.containsKey("b"));
+    Assert.assertEquals(ImmutableList.of("B"), builder.queryParams.get("b"));
+
+    Assert.assertEquals(30 * 1000, builder.timeoutMillis);
+    builder.timeout(10);
+    Assert.assertEquals(10, builder.timeoutMillis);
+
+    Assert.assertSame(RestClient.OBJECT_MAPPER, builder.jsonMapper);
+    ObjectMapper mapper = new ObjectMapper();
+    builder.jsonMapper(mapper);
+    Assert.assertSame(mapper, builder.jsonMapper);
+
+    RestClient client = builder.build();
+
+    Assert.assertNotNull(client);
+    Assert.assertEquals("http://foo/bar/", client.baseUrl);
+    Assert.assertEquals("path", client.path);
+
+    Map expectedHeaders = ImmutableMap.of(
+        "a", ImmutableList.of("A"),
+        SSOConstants.X_APP_COMPONENT_ID.toLowerCase(), ImmutableList.of("componentId"),
+        SSOConstants.X_APP_AUTH_TOKEN.toLowerCase(), ImmutableList.of("appAuthToken")
+    );
+
+    Assert.assertEquals(3, client.headers.size());
+    Assert.assertTrue(client.headers.containsKey("a"));
+    Assert.assertEquals(expectedHeaders, client.headers);
+    Assert.assertNotSame(builder.headers, client.headers);
+
+    Assert.assertFalse(client.json);
+
+    Assert.assertEquals(1, client.queryParams.size());
+    Assert.assertTrue(client.queryParams.containsKey("b"));
+    Assert.assertEquals(ImmutableList.of("B"), client.queryParams.get("b"));
+    Assert.assertNotSame(builder.queryParams, client.queryParams);
+
+    Assert.assertEquals(10, client.timeoutMillis);
+    Assert.assertSame(mapper, client.jsonMapper);
+
+    Assert.assertNotNull(client.conn);
+    Assert.assertEquals(new URL("http://foo/bar/path?b=B"), client.conn.getURL());
+    Assert.assertEquals(expectedHeaders, client.conn.getRequestProperties());
+    Assert.assertEquals(10, client.conn.getConnectTimeout());
+    Assert.assertEquals(10, client.conn.getReadTimeout());
+    Assert.assertFalse(client.conn.getDefaultUseCaches());
+
+    builder.path(null).queryParam("b", null).json(true).csrf(true);
+
+    expectedHeaders =
+        ImmutableMap.builder()
+            .putAll(expectedHeaders)
+            .put(SSOConstants.X_REST_CALL.toLowerCase(), ImmutableList.of("name"))
+            .put(RestClient.CONTENT_TYPE, ImmutableList.of(RestClient.APPLICATION_JSON))
+            .put(RestClient.ACCEPT, ImmutableList.of(RestClient.APPLICATION_JSON))
+            .build();
+
+    client = builder.build();
+    Assert.assertEquals(new URL("http://foo/bar/"), client.conn.getURL());
+    Assert.assertEquals(expectedHeaders, client.conn.getRequestProperties());
+  }
+
+  @Test
+  public void testClientGet() throws Exception {
+    RestClient client = Mockito.spy(RestClient.builder("http://foo").build());
+
+    RestClient.Response response = Mockito.mock(RestClient.Response.class);
+
+    HttpURLConnection conn = Mockito.mock(HttpURLConnection.class);
+    Mockito.doReturn(conn).when(client).getConnection();
+    Mockito
+        .doReturn(response)
+        .when(client).doHttp(Mockito.eq(conn), Mockito.eq("GET"), Mockito.any());
+    Assert.assertEquals(response, client.get());
+    Mockito.verify(conn, Mockito.times(1)).setDoOutput(Mockito.eq(true));
+  }
+
+  @Test
+  public void testClientPost() throws Exception {
+    RestClient client = Mockito.spy(RestClient.builder("http://foo").build());
+
+    RestClient.Response response = Mockito.mock(RestClient.Response.class);
+
+    HttpURLConnection conn = Mockito.mock(HttpURLConnection.class);
+    Mockito.doReturn(conn).when(client).getConnection();
+    Object data = new Object();
+    Mockito
+        .doReturn(response)
+        .when(client).doHttp(Mockito.eq(conn), Mockito.eq("POST"), Mockito.eq(data));
+    Assert.assertEquals(response, client.post(data));
+    Mockito.verify(conn, Mockito.times(1)).setDoOutput(Mockito.eq(true));
+    Mockito.verify(conn, Mockito.times(1)).setDoInput(Mockito.eq(true));
+  }
+
+  @Test
+  public void testClientPut() throws Exception {
+    RestClient client = Mockito.spy(RestClient.builder("http://foo").build());
+
+    RestClient.Response response = Mockito.mock(RestClient.Response.class);
+
+    HttpURLConnection conn = Mockito.mock(HttpURLConnection.class);
+    Mockito.doReturn(conn).when(client).getConnection();
+    Object data = new Object();
+    Mockito
+        .doReturn(response)
+        .when(client).doHttp(Mockito.eq(conn), Mockito.eq("PUT"), Mockito.eq(data));
+    Assert.assertEquals(response, client.put(data));
+    Mockito.verify(conn, Mockito.times(1)).setDoOutput(Mockito.eq(true));
+    Mockito.verify(conn, Mockito.times(1)).setDoInput(Mockito.eq(true));
+  }
+
+  @Test
+  public void testClientDelete() throws Exception {
+    RestClient client = Mockito.spy(RestClient.builder("http://foo").build());
+
+    RestClient.Response response = Mockito.mock(RestClient.Response.class);
+
+    HttpURLConnection conn = Mockito.mock(HttpURLConnection.class);
+    Mockito.doReturn(conn).when(client).getConnection();
+    Mockito
+        .doReturn(response)
+        .when(client).doHttp(Mockito.eq(conn), Mockito.eq("DELETE"), Mockito.any());
+    Assert.assertEquals(response, client.delete());
+    Mockito.verify(conn, Mockito.times(1)).setDoOutput(Mockito.eq(true));
+  }
+
+  @Test
+  public void testDoHttp() throws Exception {
+    RestClient client = Mockito.spy(RestClient.builder("http://foo").build());
+
+    HttpURLConnection conn = Mockito.mock(HttpURLConnection.class);
+
+    RestClient.Response response = client.doHttp(conn, "METHOD", null);
+    Assert.assertEquals(client.jsonMapper, response.jsonMapper);
+    Assert.assertEquals(conn, response.conn);
+    Mockito.verify(conn, Mockito.times(1)).setRequestMethod(Mockito.eq("METHOD"));
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    Mockito.when(conn.getOutputStream()).thenReturn(baos);
+    client.doHttp(conn, "METHOD", new HashMap());
+    Assert.assertEquals("{}", new String(baos.toByteArray()));
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void testDoHttpNotJson() throws Exception {
+    RestClient client = Mockito.spy(RestClient.builder("http://foo").json(false).build());
+
+    HttpURLConnection conn = Mockito.mock(HttpURLConnection.class);
+    Mockito.doReturn(conn).when(client).getConnection();
+    Object data = new Object();
+    client.post(data);
+  }
+
+  @Test
+  public void testResponse() throws Exception {
+    HttpURLConnection conn = Mockito.mock(HttpURLConnection.class);
+    Mockito.when(conn.getResponseCode()).thenReturn(100);
+    Mockito.when(conn.getContentType()).thenReturn("application/json");
+    Mockito.when(conn.getHeaderField(Mockito.eq("a"))).thenReturn("A");
+    Map<String, List<String>> headers = new HashMap<>();
+    Mockito.when(conn.getHeaderFields()).thenReturn(headers);
+    InputStream inputStream = Mockito.mock(InputStream.class);
+    Mockito.when(conn.getInputStream()).thenReturn(inputStream);
+
+
+    RestClient.Response response = new RestClient.Response(new ObjectMapper(), conn);
+    Assert.assertEquals(conn, response.getConnection());
+    Assert.assertEquals(100, response.getStatus());
+    Assert.assertEquals("application/json", response.getContentType());
+    Assert.assertTrue(response.isJson());
+    Assert.assertEquals("A", response.getHeader("a"));
+    Assert.assertEquals(headers, response.getHeaders());
+    Assert.assertEquals(inputStream, response.getInputStream());
+
+    inputStream = new ByteArrayInputStream("[\"a\"]".getBytes());
+    Mockito.when(conn.getInputStream()).thenReturn(inputStream);
+    response = new RestClient.Response(new ObjectMapper(), conn);
+    List list = response.getData(List.class);
+    Assert.assertNotNull(list);
+    Assert.assertEquals(ImmutableList.of("a"), list);
+
+    inputStream = new ByteArrayInputStream("{\"message\" : \"hello\"}".getBytes());
+    Mockito.when(conn.getErrorStream()).thenReturn(inputStream);
+    response = new RestClient.Response(new ObjectMapper(), conn);
+    Assert.assertEquals(ImmutableMap.of("message", "hello"), response.getError());
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void testResponseNoJson() throws Exception {
+    HttpURLConnection conn = Mockito.mock(HttpURLConnection.class);
+    Mockito.when(conn.getResponseCode()).thenReturn(100);
+    Mockito.when(conn.getContentType()).thenReturn("foo");
+
+
+    RestClient.Response response = new RestClient.Response(new ObjectMapper(), conn);
+    response.getData(Object.class);
+  }
+
+}
