5f3661056262b40b00d45d5517e523ce224e98ab
Jarek Jarcec Cecho <jarcec@streamsets.com>
2016-09-12 10:38:34 -0700

    SDC-3891: Provide ExecutorRunner in our sdk
    Change-Id: Ib1d196a344e351f69c1ad6d3f5eee464384199fd
    Reviewed-on: https://review.streamsets.net/4307
    Reviewed-by: Santhosh Kumar <santhosh@streamsets.com>

---
+++ sdk/src/main/java/com/streamsets/pipeline/sdk/ExecutorRunner.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.sdk;
+
+import com.streamsets.datacollector.config.StageType;
+import com.streamsets.datacollector.runner.BatchImpl;
+import com.streamsets.pipeline.api.ExecutionMode;
+import com.streamsets.pipeline.api.Executor;
+import com.streamsets.pipeline.api.OnRecordError;
+import com.streamsets.pipeline.api.Record;
+import com.streamsets.pipeline.api.StageException;
+import com.streamsets.pipeline.api.impl.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class ExecutorRunner extends StageRunner<Executor> {
+  private static final Logger LOG = LoggerFactory.getLogger(TargetRunner.class);
+
+  @SuppressWarnings("unchecked")
+  public ExecutorRunner(Class<Executor> executorClass,
+                        Executor executor,
+                        Map<String, Object> configuration,
+                        boolean isPreview,
+                        OnRecordError onRecordError,
+                        Map<String, Object> constants,
+                        ExecutionMode executionMode,
+                        String resourcesDir) {
+    super(executorClass,
+      executor,
+      StageType.EXECUTOR,
+      configuration,
+      Collections.EMPTY_LIST,
+      isPreview,
+      onRecordError,
+      constants,
+      executionMode,
+      resourcesDir
+    );
+  }
+
+  @SuppressWarnings("unchecked")
+  public ExecutorRunner(Class<Executor> executorClass,
+                        Map<String, Object> configuration,
+                        boolean isPreview,
+                        OnRecordError onRecordError,
+                        Map<String, Object> constants,
+                        ExecutionMode executionMode,
+                        String resourcesDir) {
+    super(executorClass,
+      StageType.EXECUTOR,
+      configuration,
+      Collections.EMPTY_LIST,
+      isPreview,
+      onRecordError,
+      constants,
+      executionMode,
+      resourcesDir
+    );
+  }
+
+  public void runWrite(List<Record> inputRecords) throws StageException {
+    LOG.debug("Stage '{}' write starts", getInfo().getInstanceName());
+    ensureStatus(Status.INITIALIZED);
+    BatchImpl batch = new BatchImpl(getInfo().getInstanceName(), "sdk:sourceOffset", inputRecords);
+    getStage().write(batch);
+    LOG.debug("Stage '{}' write ends", getInfo().getInstanceName());
+  }
+
+  public static class Builder extends StageRunner.Builder<Executor, ExecutorRunner, Builder> {
+
+    public Builder(Class<? extends Executor> executorClass, Executor executor) {
+      super((Class<Executor>)executorClass, executor);
+    }
+
+    @SuppressWarnings("unchecked")
+    public Builder(Class<? extends Executor> executorClass) {
+      super((Class<Executor>) executorClass);
+    }
+
+    @Override
+    public ExecutorRunner build() {
+      Utils.checkState(outputLanes.isEmpty(), "An Executor cannot have output streams");
+      return (stage != null) ?
+        new ExecutorRunner(stageClass, stage, configs, isPreview, onRecordError, constants, executionMode, resourcesDir)
+        : new ExecutorRunner(stageClass, configs, isPreview, onRecordError, constants, executionMode, resourcesDir);
+    }
+  }
+
+}
---
+++ sdk/src/test/java/com/streamsets/pipeline/sdk/TestExecutorRunner.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.sdk;
+
+import com.google.common.collect.ImmutableList;
+import com.streamsets.pipeline.api.Batch;
+import com.streamsets.pipeline.api.Record;
+import com.streamsets.pipeline.api.StageException;
+import com.streamsets.pipeline.api.base.BaseExecutor;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Iterator;
+
+public class TestExecutorRunner {
+
+  public static class DummyExecutorEmptyBatch extends BaseExecutor {
+    public boolean write;
+
+    @Override
+    public void write(Batch batch) throws StageException {
+      Assert.assertNotNull(batch.getSourceOffset());
+      Iterator<Record> it = batch.getRecords();
+      Assert.assertFalse(it.hasNext());
+      write = true;
+    }
+
+  }
+
+  public static class DummyExecutor extends BaseExecutor {
+    public boolean write;
+
+    @Override
+    public void write(Batch batch) throws StageException {
+      Assert.assertNotNull(batch.getSourceOffset());
+      Iterator<Record> it = batch.getRecords();
+      Assert.assertTrue(it.hasNext());
+      Record record = it.next();
+      Assert.assertNotNull(record);
+      Assert.assertFalse(it.hasNext());
+      write = true;
+    }
+
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testBuilderOutputLane() throws Exception {
+    DummyExecutor stage = new DummyExecutor();
+    ExecutorRunner.Builder builder = new ExecutorRunner.Builder(DummyExecutor.class, stage).addOutputLane("a");
+    builder.build();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void testInitProcessDestroy() throws Exception {
+    DummyExecutorEmptyBatch stage = new DummyExecutorEmptyBatch();
+    ExecutorRunner.Builder builder = new ExecutorRunner.Builder(DummyExecutor.class, stage);
+    ExecutorRunner runner = builder.build();
+    try {
+      runner.runInit();
+      runner.runWrite(Collections.EMPTY_LIST);
+    } finally {
+      runner.runDestroy();
+    }
+  }
+
+  @Test(expected = RuntimeException.class)
+  @SuppressWarnings("unchecked")
+  public void testInvalidProcess1() throws Exception {
+    DummyExecutor stage = new DummyExecutor();
+    ExecutorRunner.Builder builder = new ExecutorRunner.Builder(DummyExecutor.class, stage);
+    ExecutorRunner runner = builder.build();
+    try {
+      runner.runWrite(Collections.EMPTY_LIST);
+    } finally {
+      runner.runDestroy();
+    }
+  }
+
+  @Test(expected = RuntimeException.class)
+  @SuppressWarnings("unchecked")
+  public void testInvalidProcess2() throws Exception {
+    DummyExecutor stage = new DummyExecutor();
+    ExecutorRunner.Builder builder = new ExecutorRunner.Builder(DummyExecutor.class, stage);
+    ExecutorRunner runner = builder.build();
+    runner.runInit();
+    runner.runDestroy();
+    runner.runWrite(Collections.EMPTY_LIST);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void testProcessEmptyBatch() throws Exception {
+    DummyExecutorEmptyBatch stage = new DummyExecutorEmptyBatch();
+    ExecutorRunner.Builder builder = new ExecutorRunner.Builder(DummyExecutor.class, stage);
+    ExecutorRunner runner = builder.build();
+    try {
+      runner.runInit();
+      runner.runWrite(Collections.EMPTY_LIST);
+      Assert.assertTrue(stage.write);
+    } finally {
+      runner.runDestroy();
+    }
+  }
+
+  @Test
+  public void testProcessNonEmptyBatch() throws Exception {
+    DummyExecutor stage = new DummyExecutor();
+    ExecutorRunner.Builder builder = new ExecutorRunner.Builder(DummyExecutor.class, stage);
+    ExecutorRunner runner = builder.build();
+    try {
+      runner.runInit();
+      runner.runWrite(ImmutableList.of(RecordCreator.create()));
+      Assert.assertTrue(stage.write);
+    } finally {
+      runner.runDestroy();
+    }
+  }
+
+}
