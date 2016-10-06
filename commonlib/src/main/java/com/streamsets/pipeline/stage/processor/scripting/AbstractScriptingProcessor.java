b8684bdc04827930901f5dac308765d047532971
Hari Shreedharan <hshreedharan@streamsets.com>
2016-10-06 10:36:02 -0700

    SDC-4100. Provide function to create list maps in scripting processors.
    - Adds a new function to sdcFunctions: createMap(boolean listMap) which
      returns a map or a list map based on the parameter passed. This can be
      used as a field in any record.
    
    Change-Id: I025bf90b6ffb6a8e8a939e8b8a313fca23b5beeb
    Reviewed-on: https://review.streamsets.net/4677
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>
    (cherry picked from commit 5d439ac25d947c070c5817c046cabe9bda04f0ed)
    Reviewed-on: https://review.streamsets.net/4681
    Reviewed-by: Santhosh Kumar <santhosh@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- commonlib/src/main/java/com/streamsets/pipeline/stage/processor/scripting/AbstractScriptingProcessor.java
+++ commonlib/src/main/java/com/streamsets/pipeline/stage/processor/scripting/AbstractScriptingProcessor.java
@@ -75,6 +75,10 @@ public abstract class AbstractScriptingProcessor extends SingleLaneProcessor {
       return getScriptObjectFactory().createScriptRecord(getContext().createRecord(recordSourceId));
     }
 
+    public Object createMap(boolean listMap) {
+      return getScriptObjectFactory().createMap(listMap);
+    }
+
   }
 
   private final String scriptingEngineName;
