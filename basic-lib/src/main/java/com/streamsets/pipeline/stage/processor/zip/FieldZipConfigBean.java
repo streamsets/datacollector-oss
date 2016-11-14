6fdd0def3ff8ee7b3642a15d27680fa5134f9b2f
Jon Natkins <natty@streamsets.com>
2016-11-14 11:17:40 -0800

    SDC-4359. Zip Processor
    Change-Id: I63186bd308e2f28623c2ae0257029f5db0da1f62
    Reviewed-on: https://review.streamsets.net/5224
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>
    (cherry picked from commit c9a1b2bae618c6d78c8413ed4340e672c0c60bbe)
    Reviewed-on: https://review.streamsets.net/5579
    Reviewed-by: Jonathan Natkins <natty@streamsets.com>

---
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/processor/zip/FieldZipConfigBean.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.processor.zip;
+
+import com.streamsets.pipeline.api.ConfigDef;
+import com.streamsets.pipeline.api.ListBeanModel;
+import com.streamsets.pipeline.api.ValueChooserModel;
+import com.streamsets.pipeline.config.OnStagePreConditionFailure;
+import com.streamsets.pipeline.config.OnStagePreConditionFailureChooserValues;
+
+import java.util.List;
+
+public class FieldZipConfigBean {
+
+  @ConfigDef(
+      required = false,
+      type = ConfigDef.Type.MODEL,
+      label = "Fields to Zip",
+      description="Zips the specified fields into a single list.",
+      displayPosition = 10,
+      group = "ZIP"
+  )
+  @ListBeanModel
+  public List<FieldZipConfig> fieldZipConfigs;
+
+
+  @ConfigDef(
+      required = true,
+      group = "ZIP",
+      type = ConfigDef.Type.BOOLEAN,
+      defaultValue = "false",
+      label = "Zip Values Only",
+      description = "If checked, the output will be a list of two-element lists. If unchecked, the output " +
+          "will be a list of maps with each field value identified by the originating field name.",
+      displayPosition = 20
+  )
+  public boolean valuesOnly;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.MODEL,
+      defaultValue = "TO_ERROR",
+      label = "Field Does Not Exist",
+      description="Action for data that does not contain the specified fields",
+      displayPosition = 50,
+      group = "ZIP"
+  )
+  @ValueChooserModel(OnStagePreConditionFailureChooserValues.class)
+  public OnStagePreConditionFailure onStagePreConditionFailure;
+}
---
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/processor/zip/FieldZipProcessor.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.processor.zip;
+
+import com.google.api.client.util.Lists;
+import com.google.common.collect.ImmutableMap;
+import com.streamsets.pipeline.api.Field;
+import com.streamsets.pipeline.api.Record;
+import com.streamsets.pipeline.api.StageException;
+import com.streamsets.pipeline.api.base.OnRecordErrorException;
+import com.streamsets.pipeline.api.base.SingleLaneRecordProcessor;
+import com.streamsets.pipeline.config.OnStagePreConditionFailure;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class FieldZipProcessor extends SingleLaneRecordProcessor {
+
+  private final List<FieldZipConfig> fieldZipConfigs;
+  private final boolean valuesOnly;
+  private final OnStagePreConditionFailure onStagePreConditionFailure;
+
+  public FieldZipProcessor(
+      FieldZipConfigBean configBean
+  ) {
+    this.fieldZipConfigs = configBean.fieldZipConfigs;
+    this.valuesOnly = configBean.valuesOnly;
+    this.onStagePreConditionFailure = configBean.onStagePreConditionFailure;
+  }
+
+  private class ZipEntry {
+    String name;
+    Field value;
+
+    ZipEntry(String name, Field value) {
+      this.name = name;
+      this.value = value;
+    }
+  }
+
+  @Override
+  protected void process(Record record, SingleLaneBatchMaker batchMaker) throws StageException {
+    checkConfigs(record);
+
+    for (FieldZipConfig zipConfig : fieldZipConfigs) {
+      String firstFieldPath = zipConfig.firstField;
+      String secondFieldPath = zipConfig.secondField;
+      String zippedPath = zipConfig.zippedFieldPath;
+
+      List<ZipEntry> firstEntries = getZipEntries(firstFieldPath, record.get(firstFieldPath));
+      List<ZipEntry> secondEntries = getZipEntries(secondFieldPath, record.get(secondFieldPath));
+      List<Field> zipped = Lists.newArrayList();
+      if (valuesOnly) {
+        for (int i = 0; i < firstEntries.size() && i < secondEntries.size(); i++) {
+          zipped.add(Field.create(Arrays.asList(firstEntries.get(i).value, secondEntries.get(i).value)));
+        }
+      } else {
+        for (int i = 0; i < firstEntries.size() && i < secondEntries.size(); i++) {
+          zipped.add(Field.create(ImmutableMap.of(firstEntries.get(i).name, firstEntries.get(i).value,
+              secondEntries.get(i).name, secondEntries.get(i).value)));
+        }
+      }
+
+      record.set(zippedPath, Field.create(zipped));
+    }
+
+    batchMaker.addRecord(record);
+  }
+
+  private List<ZipEntry> getZipEntries(String fieldPath, Field field) {
+    List<ZipEntry> zipEntries = Lists.newArrayList();
+    if (field.getType() == Field.Type.LIST) {
+      String fieldName = fieldPath.substring(fieldPath.lastIndexOf("/") + 1);
+      for (Field entry : field.getValueAsList()) {
+        zipEntries.add(new ZipEntry(fieldName, entry));
+      }
+    } else if (field.getType().isOneOf(Field.Type.LIST_MAP, Field.Type.MAP)) {
+      for (Map.Entry<String, Field> entry : field.getValueAsMap().entrySet()) {
+        zipEntries.add(new ZipEntry(entry.getKey(), entry.getValue()));
+      }
+    }
+    return zipEntries;
+  }
+
+  private void checkConfigs(Record record) throws OnRecordErrorException {
+    for (FieldZipConfig zipConfig : fieldZipConfigs) {
+      List<String> missingFields = Lists.newArrayList();
+      List<String> nonListFields = Lists.newArrayList();
+
+      if (!record.has(zipConfig.firstField)) {
+        missingFields.add(zipConfig.firstField);
+      } else if (!record.get(zipConfig.firstField).getType().isOneOf(Field.Type.LIST, Field.Type.LIST_MAP)) {
+        nonListFields.add(zipConfig.firstField);
+      }
+      if (!record.has(zipConfig.secondField)) {
+        missingFields.add(zipConfig.secondField);
+      } else if (!record.get(zipConfig.secondField).getType().isOneOf(Field.Type.LIST, Field.Type.LIST_MAP)) {
+        nonListFields.add(zipConfig.secondField);
+      }
+
+      switch (onStagePreConditionFailure) {
+        case TO_ERROR:
+          if (!missingFields.isEmpty()) {
+            throw new OnRecordErrorException(Errors.ZIP_01, record.getHeader().getSourceId(), missingFields);
+          } else if (!nonListFields.isEmpty()) {
+            throw new OnRecordErrorException(Errors.ZIP_00, record.getHeader().getSourceId(), nonListFields);
+          }
+        case CONTINUE:
+          return;
+        default:
+          throw new IllegalStateException("Invalid value for on stage pre-condition failure");
+      }
+    }
+  }
+}
