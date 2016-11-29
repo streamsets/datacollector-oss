5a1cbdd017492f35406fcea5e59abdd309099c89
Alejandro Abdelnur <tucu@streamsets.com>
2016-11-29 15:41:41 +0100

    SDC-4668. HTTP multithreaded source to Kafka
      A new HTTP multithreaded orgiin that receives arbitrary
      payloads and writes every request as a single Kafka message.
      It is based on the code introduced by SDC-4666.
    
    Change-Id: Id5e12d97c1382c96a03b42090bca44a5bab63a39
    (cherry picked from commit 801d078c10b97a4178dd08df4649634e9c737d63)
    Reviewed-on: https://review.streamsets.net/5543
    Reviewed-by: Alejandro Abdelnur <tucu@streamsets.com>

---
+++ kafka_target-protolib/src/main/java/com/streamsets/pipeline/stage/origin/httptokafka/HttpToKafkaDSource.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.origin.httptokafka;
+
+import com.streamsets.pipeline.api.ConfigDef;
+import com.streamsets.pipeline.api.ConfigDefBean;
+import com.streamsets.pipeline.api.ConfigGroups;
+import com.streamsets.pipeline.api.ExecutionMode;
+import com.streamsets.pipeline.api.GenerateResourceBundle;
+import com.streamsets.pipeline.api.HideConfigs;
+import com.streamsets.pipeline.api.Source;
+import com.streamsets.pipeline.api.StageDef;
+import com.streamsets.pipeline.configurablestage.DSourceOffsetCommitter;
+import com.streamsets.pipeline.stage.destination.kafka.KafkaTargetConfig;
+
+@StageDef(
+  version = 1,
+  label = "HTTP to Kafka",
+  execution = ExecutionMode.STANDALONE,
+  description = "Receives data via HTTP and writes every HTTP request payload to Kafka",
+  icon="httptokafka.png",
+  onlineHelpRefUrl = "index.html#Origins/HTTPtoKafka.html#task_vgx_nqd_dy"
+)
+@ConfigGroups(Groups.class)
+@GenerateResourceBundle
+@HideConfigs(
+    value = {
+        "conf.dataGeneratorFormatConfig.csvFileFormat",
+        "conf.dataGeneratorFormatConfig.csvHeader",
+        "conf.dataGeneratorFormatConfig.csvReplaceNewLines",
+        "conf.dataGeneratorFormatConfig.csvReplaceNewLinesString",
+        "conf.dataGeneratorFormatConfig.csvCustomDelimiter",
+        "conf.dataGeneratorFormatConfig.csvCustomEscape",
+        "conf.dataGeneratorFormatConfig.csvCustomQuote",
+        "conf.dataGeneratorFormatConfig.jsonMode",
+        "conf.dataGeneratorFormatConfig.textFieldPath",
+        "conf.dataGeneratorFormatConfig.textEmptyLineIfNull",
+        "conf.dataGeneratorFormatConfig.textRecordSeparator",
+        "conf.dataGeneratorFormatConfig.avroSchemaSource",
+        "conf.dataGeneratorFormatConfig.avroSchema",
+        "conf.dataGeneratorFormatConfig.schemaRegistryUrls",
+        "conf.dataGeneratorFormatConfig.schemaLookupMode",
+        "conf.dataGeneratorFormatConfig.subject",
+        "conf.dataGeneratorFormatConfig.subjectToRegister",
+        "conf.dataGeneratorFormatConfig.schemaRegistryUrlsForRegistration",
+        "conf.dataGeneratorFormatConfig.registerSchema",
+        "conf.dataGeneratorFormatConfig.schemaId",
+        "conf.dataGeneratorFormatConfig.includeSchema",
+        "conf.dataGeneratorFormatConfig.avroCompression",
+        "conf.dataGeneratorFormatConfig.binaryFieldPath",
+        "conf.dataGeneratorFormatConfig.protoDescriptorFile",
+        "conf.dataGeneratorFormatConfig.messageType",
+        "conf.dataGeneratorFormatConfig.fileNameEL",
+        "conf.dataGeneratorFormatConfig.wholeFileExistsAction",
+        "conf.dataGeneratorFormatConfig.includeChecksumInTheEvents",
+        "conf.dataGeneratorFormatConfig.checksumAlgorithm",
+        "conf.dataFormat",
+        "conf.runtimeTopicResolution",
+        "conf.partitionStrategy",
+        "conf.partition",
+        "conf.singleMessagePerBatch",
+        "conf.topicExpression",
+        "conf.topicWhiteList"
+    }
+)
+public class HttpToKafkaDSource extends DSourceOffsetCommitter {
+
+  @ConfigDefBean
+  public RawHttpConfigs configs;
+
+  @ConfigDefBean()
+  public KafkaTargetConfig conf;
+
+  @ConfigDef(
+    required = true,
+    type = ConfigDef.Type.NUMBER,
+    defaultValue = "900",
+    label = "Max Message Size (KB)",
+    description = "Maximum size of the message written to Kafka. Configure in relationship to Max Batch Request " +
+        "Size and the maximum message size configured in Kafka. ",
+    displayPosition = 30,
+    group = "KAFKA",
+    min = 1,
+    max = 10000
+  )
+  public int kafkaMaxMessageSizeKB;
+
+  // The "topic" config from KafkaConfigBean depends on "runtimeTopicResolution" config which is hidden because
+  // it does not make sense in this origin. As a result "topic" config will also not be shown to the user.
+  // The only caveat is that the UI will not be able to pin point the topic configuration if it fails validation.
+  @ConfigDef(
+    required = true,
+    type = ConfigDef.Type.STRING,
+    defaultValue = "topicName",
+    label = "Topic",
+    description = "",
+    displayPosition = 25,
+    group = "KAFKA"
+  )
+  public String topic;
+
+  @Override
+  protected Source createSource() {
+    conf.topic = topic;
+    configs.setMaxHttpRequestSizeKB(kafkaMaxMessageSizeKB);
+    return new HttpToKafkaSource(configs, conf, kafkaMaxMessageSizeKB);
+  }
+
+}
---
+++ kafka_target-protolib/src/main/java/com/streamsets/pipeline/stage/origin/httptokafka/RawHttpConfigs.java
@@ -0,0 +1,147 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.origin.httptokafka;
+
+import com.streamsets.pipeline.api.ConfigDef;
+import com.streamsets.pipeline.lib.el.VaultEL;
+import com.streamsets.pipeline.lib.http.HttpConfigs;
+
+public class RawHttpConfigs extends HttpConfigs {
+
+  public RawHttpConfigs() {
+    super(Groups.HTTP.name(), "config.");
+  }
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.NUMBER,
+      defaultValue = "8000",
+      label = "HTTP Listening Port",
+      description = "HTTP endpoint to listen for data.",
+      displayPosition = 10,
+      group = "HTTP",
+      min = 1,
+      max = 65535
+  )
+  public int port;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.NUMBER,
+      defaultValue = "10",
+      label = "Max Concurrent Requests",
+      description = "Maximum number of concurrent requests allowed by the origin.",
+      displayPosition = 15,
+      group = "HTTP",
+      min = 1,
+      max = 200
+  )
+  public int maxConcurrentRequests;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.STRING,
+      label = "Application ID",
+      description = "Only HTTP requests presenting this token will be accepted.",
+      displayPosition = 20,
+      elDefs = VaultEL.class,
+      group = "HTTP"
+  )
+  public String appId;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.BOOLEAN,
+      defaultValue = "false",
+      label = "Use HTTPS",
+      displayPosition = 40,
+      group = "HTTP"
+  )
+  public boolean sslEnabled;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.STRING,
+      defaultValue = "",
+      label = "Keystore File",
+      description = "The keystore file is expected in the Data Collector resources directory",
+      displayPosition = 50,
+      group = "HTTP",
+      dependsOn = "sslEnabled",
+      triggeredByValue = "true"
+  )
+  public String keyStoreFile;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.STRING,
+      defaultValue = "",
+      label = "Keystore Password",
+      displayPosition = 60,
+      elDefs = VaultEL.class,
+      group = "HTTP",
+      dependsOn = "sslEnabled",
+      triggeredByValue = "true"
+  )
+  public String keyStorePassword;
+
+  @Override
+  public int getPort() {
+    return port;
+  }
+
+  @Override
+  public int getMaxConcurrentRequests() {
+    return maxConcurrentRequests;
+  }
+
+  @Override
+  public String getAppId() {
+    return appId;
+  }
+
+  private int maxHttpRequestSizeKB = -1;
+
+  // in MBs
+  public void setMaxHttpRequestSizeKB(int size) {
+    maxHttpRequestSizeKB = size;
+  }
+
+  @Override
+  public int getMaxHttpRequestSizeKB() {
+    return maxHttpRequestSizeKB;
+  }
+
+  @Override
+  public boolean isSslEnabled() {
+    return sslEnabled;
+  }
+
+  @Override
+  public String getKeyStorePassword() {
+    return keyStorePassword;
+  }
+
+  @Override
+  public String getKeyStoreFile() {
+    return keyStoreFile;
+  }
+
+}
