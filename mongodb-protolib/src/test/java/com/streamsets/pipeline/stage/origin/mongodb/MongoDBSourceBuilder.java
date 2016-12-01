5612a1dc8da4265df120d760796b1f10cf5660f7
Ji Sun <jisun@streamsets.com>
2016-11-30 16:03:50 -0800

    SDC-4640. Mongo Origin does not handle BSON timestamp data type
    - map BSON timestamp into SDC record map as "timestamp" and "ordinal"
    
    Change-Id: I4cbd12e6dead8eb18ed9435d728583bfc85b8ac3
    Reviewed-on: https://review.streamsets.net/5533
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Hari Shreedharan <hshreedharan@streamsets.com>
    Reviewed-by: Harikiran Nayak <hari@streamsets.com>
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

---
+++ mongodb-protolib/src/test/java/com/streamsets/pipeline/stage/origin/mongodb/MongoDBSourceBuilder.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.streamsets.pipeline.stage.origin.mongodb;
+
+import com.streamsets.pipeline.stage.common.mongodb.AuthenticationType;
+import com.streamsets.pipeline.stage.common.mongodb.MongoDBConfig;
+
+public class MongoDBSourceBuilder {
+  private MongoSourceConfigBean config;
+
+  public MongoDBSourceBuilder() {
+    config = new MongoSourceConfigBean();
+    config.mongoConfig = new MongoDBConfig();
+    config.mongoConfig.connectionString = "mongodb://";
+    config.mongoConfig.database = "database";
+    config.mongoConfig.collection = "collection";
+    config.mongoConfig.authenticationType = AuthenticationType.NONE;
+    config.mongoConfig.username = null;
+    config.mongoConfig.password = null;
+    config.isCapped = true;
+    config.offsetField = "_id";
+    config.initialOffset = "0";
+    config.batchSize = 100;
+    config.maxBatchWaitTime = 1;
+    config.readPreference = ReadPreferenceLabel.NEAREST;
+  }
+
+  public MongoDBSourceBuilder connectionString(String connectionString) {
+    config.mongoConfig.connectionString = connectionString;
+    return this;
+  }
+
+  public MongoDBSourceBuilder database(String database) {
+    config.mongoConfig.database = database;
+    return this;
+  }
+
+  public MongoDBSourceBuilder collection(String collection) {
+    config.mongoConfig.collection = collection;
+    return this;
+  }
+
+  public MongoDBSourceBuilder isCapped(boolean isCapped) {
+    config.isCapped = isCapped;
+    return this;
+  }
+
+  public MongoDBSourceBuilder initialOffset(String initialOffset) {
+    config.initialOffset = initialOffset;
+    return this;
+  }
+
+  public MongoDBSourceBuilder maxBatchWaitTime(long maxBatchWaitTime) {
+    config.maxBatchWaitTime = maxBatchWaitTime;
+    return this;
+  }
+
+  public MongoDBSource build() {
+    return new MongoDBSource(config);
+  }
+}
