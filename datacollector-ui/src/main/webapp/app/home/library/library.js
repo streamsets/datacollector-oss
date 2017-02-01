2ebe2601e2e18ea30fc288d342614724e1359ff0
Wagner Camarao <wagner@streamsets.com>
2017-02-01 11:47:43 -0800

    SDC-4836. Nested labels, created like "path/to/label" and displayed like a tree on the side bar
    Custom labels are now a TreeNode and rendered with nested ul elements.
    Also changed library template structure very slightly, and updated
    package manager accordingly. May need the same for users and groups
    in the future if we add hover and click behavior there.
    
    Change-Id: Ia8e8e1d83901baf1df8989ae9d8086f90dc5fad2
    Reviewed-on: https://review.streamsets.net/6400
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- datacollector-ui/src/main/webapp/app/home/library/library.js
+++ datacollector-ui/src/main/webapp/app/home/library/library.js
@@ -21,7 +21,6 @@
 /**
  * Controller for Library Pane.
  */
-
 angular
   .module('dataCollectorApp.home')
   .controller('LibraryController', function ($scope, api, $q) {
@@ -30,22 +29,89 @@ angular
       pipelineLabels: [],
 
       /**
-       * Callback function for selecting pipeline label
-       *
+       * Determine whether the view has user-defined labels
+       * @returns {boolean}
+       */
+      hasPipelineLabels: function() {
+        return !_.isEmpty($scope.pipelineLabels);
+      },
+
+      /**
+       * Return obj keys sorted alphabetically
+       * @param obj
+       * @returns {*}
+       */
+      sortedKeys: function(obj) {
+        return !obj ? null : Object.keys(obj).sort();
+      },
+
+      /**
+       * Select a label string from the flat list of system labels
        * @param label
        */
       onSelectLabel : function(label) {
         $scope.$storage.pipelineListState.selectedLabel = label;
         $scope.selectPipelineLabel(label);
+      },
+
+      /**
+       * Select a label node from the tree of user-defined labels
+       * @param node
+       */
+      onSelectNode: function(node) {
+        if (this.hasChildren(node)) {
+          node.isExpanded = !node.isExpanded;
+        }
+        if ($scope.rawPipelineLabels.indexOf(node.raw) !== -1) {
+          this.onSelectLabel(node.raw);
+        }
+      },
+
+      /**
+       * Determine whether a node has children
+       *
+       * @param node
+       * @returns {boolean}
+       */
+      hasChildren: function(node) {
+        return !_.isEmpty(node.children);
       }
     });
 
     /**
+     * Build a TreeNode of Labels
+     *
+     * @param labels
+     * @returns {{}}
+     */
+    var buildLabelTreeNode = function(labels, selectedLabel) {
+      var tree = {};
+
+      labels.forEach(function(raw) {
+        var parts = (raw || '').replace(/\/+/g, '/').replace(/^\/|\/$/g, '').split('/');
+        var context = tree;
+
+        parts.forEach(function(part, j) {
+          var index = raw.indexOf('/' + parts[j + 1]);
+          var vPath = (index === -1) ? raw : raw.substr(0, index);
+          var node = context[part] = context[part] || {raw: vPath, isExpanded: selectedLabel.indexOf(vPath) === 0};
+          node.children = node.children || {};
+          context = node.children;
+        });
+      });
+
+      return tree;
+    };
+
+    /**
      * Labels are loaded only once in home.js so we get notified with them here
      */
-    $scope.onLabelsLoaded(function(systemPipelineLabels, pipelineLabels) {
+    $scope.onLabelsLoaded(function(systemPipelineLabels, rawPipelineLabels) {
       $scope.systemPipelineLabels = systemPipelineLabels;
-      $scope.pipelineLabels = pipelineLabels.sort();
-    });
+      $scope.rawPipelineLabels = rawPipelineLabels.sort();
 
+      $scope.pipelineLabels = {
+        children: buildLabelTreeNode($scope.rawPipelineLabels, $scope.$storage.pipelineListState.selectedLabel)
+      };
+    });
   });
