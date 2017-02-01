15a4c0090934dbc452c175522fa859490256e304
Cheolsoo Park <cheolsoo@streamsets.com>
2017-02-01 09:02:27 -0800

    SDC-5205. Reorder Elasticsearch destination configs: Additional HTTP Params and Detect Additional Nodes in Cluster
    Change-Id: I7de9b36404b60ff9e98b2f821dae7d73eb958f85
    Reviewed-on: https://review.streamsets.net/6385
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Cheolsoo Park <cheolsoo@streamsets.com>

--- elasticsearch-protolib/src/main/java/com/streamsets/pipeline/stage/destination/elasticsearch/ElasticSearchConfigBean.java
+++ elasticsearch-protolib/src/main/java/com/streamsets/pipeline/stage/destination/elasticsearch/ElasticSearchConfigBean.java
@@ -52,18 +52,6 @@ public class ElasticSearchConfigBean {
   public List<String> httpUris;
 
   @ConfigDef(
-      required = true,
-      type = ConfigDef.Type.BOOLEAN,
-      label = "Detect Additional Nodes in Cluster",
-      defaultValue = "false",
-      description = "Select to automatically discover additional Elasticsearch nodes in the cluster. " +
-          "Do not use if the Data Collector is on a different network from the cluster.",
-      displayPosition = 15,
-      group = "ELASTIC_SEARCH"
-  )
-  public boolean clientSniff;
-
-  @ConfigDef(
       required = false,
       type = ConfigDef.Type.MAP,
       defaultValue = "",
@@ -73,6 +61,18 @@ public class ElasticSearchConfigBean {
       group = "ELASTIC_SEARCH"
   )
   public Map<String, String> params;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.BOOLEAN,
+      label = "Detect Additional Nodes in Cluster",
+      defaultValue = "false",
+      description = "Select to automatically discover additional Elasticsearch nodes in the cluster. " +
+          "Do not use if the Data Collector is on a different network from the cluster.",
+      displayPosition = 25,
+      group = "ELASTIC_SEARCH"
+  )
+  public boolean clientSniff;
 
   @ConfigDef(
       required = true,
