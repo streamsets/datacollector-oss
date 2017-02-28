ce5122526833c00c56fba3acf534fb5e38a074b2
Adam Kunicki <adam@streamsets.com>
2017-02-27 21:20:21 -0800

    SDC-5468. Remove Java 7 JVM flags from cluster mode defaults
    Change-Id: I7564bee4759ed850d71e9b23ec942b62636da002
    Reviewed-on: https://review.streamsets.net/6937
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>
    Reviewed-by: Virag Kothari <virag@streamsets.com>

--- cluster-bootstrap-api/src/test/java/com/streamsets/pipeline/hadoop/TestHadoopMapReduceBinding.java
+++ cluster-bootstrap-api/src/test/java/com/streamsets/pipeline/hadoop/TestHadoopMapReduceBinding.java
@@ -19,11 +19,11 @@
  */
 package com.streamsets.pipeline.hadoop;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
 import org.apache.hadoop.conf.Configuration;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class TestHadoopMapReduceBinding {
 
@@ -32,25 +32,25 @@ public class TestHadoopMapReduceBinding {
     Configuration conf = new Configuration();
     conf.set(HadoopMapReduceBinding.MAPREDUCE_MAP_MEMORY_MB, "1024");
     // Xmx with m option
-    String javaOpts = "-Xmx1024m -XX:PermSize=128M -XX:MaxPermSize=256M -Dlog4j.debug";
+    String javaOpts = "-Xmx1024m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -Dlog4j.debug";
     assertEquals(Integer.valueOf(1280), HadoopMapReduceBinding.getMapMemoryMb(javaOpts, conf));
     // Xmx with g option
-    javaOpts = "-Xmx1g -XX:PermSize=128M -XX:MaxPermSize=256M -Dlog4j.debug";
+    javaOpts = "-Xmx1g -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -Dlog4j.debug";
     assertEquals(Integer.valueOf(1280), HadoopMapReduceBinding.getMapMemoryMb(javaOpts, conf));
     // Xmx with k option
-    javaOpts = "-Xmx1048576K -XX:PermSize=128M -XX:MaxPermSize=256M -Dlog4j.debug";
+    javaOpts = "-Xmx1048576K -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -Dlog4j.debug";
     assertEquals(Integer.valueOf(1280), HadoopMapReduceBinding.getMapMemoryMb(javaOpts, conf));
     // Xmx only bytes
-    javaOpts = "-Xmx1073741824 -XX:PermSize=128M -XX:MaxPermSize=256M -Dlog4j.debug";
+    javaOpts = "-Xmx1073741824 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -Dlog4j.debug";
     assertEquals(Integer.valueOf(1280), HadoopMapReduceBinding.getMapMemoryMb(javaOpts, conf));
     // last -Xmx wins
-    javaOpts = "-Xmx512m -Xmx1024m -XX:PermSize=128M -XX:MaxPermSize=256M -Dlog4j.debug";
+    javaOpts = "-Xmx512m -Xmx1024m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -Dlog4j.debug";
     assertEquals(Integer.valueOf(1280), HadoopMapReduceBinding.getMapMemoryMb(javaOpts, conf));
     conf.set(HadoopMapReduceBinding.MAPREDUCE_MAP_MEMORY_MB, "2048");
     // default config > passed java_opts
     assertEquals(Integer.valueOf(2048), HadoopMapReduceBinding.getMapMemoryMb(javaOpts, conf));
     // no Xmx option
-    javaOpts = "-XX:PermSize=128M -XX:MaxPermSize=256M -Dlog4j.debug";
+    javaOpts = "-XX:+UseConcMarkSweepGC -XX:+UseParNewGC -Dlog4j.debug";
     assertNull(HadoopMapReduceBinding.getMapMemoryMb(javaOpts, conf));
 
   }
