0ac1e5d2616d13c51bbe0496b8260bdf729fbc5e
Jarek Jarcec Cecho <jarcec@streamsets.com>
2017-04-20 09:21:33 -0700

    SDC-5921: Bundles: Annotation infrastructure for bundle generators
    Change-Id: I3575367cbfb00420df895046ef3109d5aa5499b8
    Reviewed-on: https://review.streamsets.net/7618
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

---
+++ container-common/src/main/java/com/streamsets/datacollector/bundles/BundleContentGeneratorDef.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.datacollector.bundles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation describing Support Bundle Content generator and it's metadata.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface BundleContentGeneratorDef {
+
+  /**
+   * User visible name of the bundle.
+   */
+  String name();
+
+  /**
+   * User visible description (few sentences) about what this generator does - what kind of data it collects.
+   */
+  String description();
+
+  /**
+   * Version of the content generator.
+   */
+  int version();
+
+  /**
+   * Whether this content generator should be used by default when user is not specifying what generators should be used.
+   */
+  boolean enabledByDefault() default false;
+
+}
---
+++ container/src/test/java/com/streamsets/datacollector/bundles/TestSupportBundleContentGeneratorProcessor.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.datacollector.bundles;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class TestSupportBundleContentGeneratorProcessor {
+  private static final Logger LOG = LoggerFactory.getLogger(TestSupportBundleContentGeneratorProcessor.class);
+
+  @BundleContentGeneratorDef(
+    name = "Test",
+    description = "Still testing",
+    version = 666
+  )
+  private static class CustomGenerator {
+  }
+
+  @Test
+  public void testSerializingCustomGenerator() throws Exception {
+    InputStream generatorResource = Thread.currentThread().getContextClassLoader().getResourceAsStream(SupportBundleContentGeneratorProcessor.RESOURCE_NAME);
+    assertNotNull(generatorResource);
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(generatorResource));
+    String className;
+    boolean found = false;
+    while((className = reader.readLine()) != null) {
+      LOG.debug("Found class: " + className);
+      if(className.equals("com.streamsets.datacollector.bundles.TestSupportBundleContentGeneratorProcessor.CustomGenerator")) {
+        found = true;
+        break;
+      }
+    }
+
+    assertTrue(found);
+  }
+}
