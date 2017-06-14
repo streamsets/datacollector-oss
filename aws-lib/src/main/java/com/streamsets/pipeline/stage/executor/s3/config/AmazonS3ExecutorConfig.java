e718899f35fb794e74f08e8e9bf60603396b7eac
Jarek Jarcec Cecho <jarcec@streamsets.com>
2017-06-13 19:17:11 -0700

    SDC-6431: Create S3 executor
    Simple S3 executor that currently can only tag given object on S3.
    Additional functionality will be provided via separate JIRAs in the
    future.
    
    Change-Id: Ic57a00851e5854073dcb3849afb9abd770c4f2f6
    Reviewed-on: https://review.streamsets.net/8831
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Jeff Evans <jeff@streamsets.com>
    Reviewed-on: https://review.streamsets.net/9276
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

---
+++ aws-lib/src/main/java/com/streamsets/pipeline/stage/executor/s3/config/AmazonS3ExecutorConfig.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.executor.s3.config;
+
+import com.streamsets.pipeline.api.ConfigDefBean;
+import com.streamsets.pipeline.api.Stage;
+import com.streamsets.pipeline.stage.destination.s3.S3ConnectionTargetConfig;
+import com.streamsets.pipeline.stage.lib.aws.ProxyConfig;
+
+import java.util.List;
+
+public class AmazonS3ExecutorConfig {
+
+  public static final String BEAN_PREFIX = "config.";
+  public static final String S3_CONFIG_PREFIX = BEAN_PREFIX + "s3Config.";
+
+  @ConfigDefBean(groups = "S3")
+  public S3ConnectionTargetConfig s3Config = new S3ConnectionTargetConfig();
+
+  @ConfigDefBean(groups = "ADVANCED")
+  public ProxyConfig proxyConfig = new ProxyConfig();
+
+  @ConfigDefBean(groups = "TASKS")
+  public S3TaskConfig taskConfig = new S3TaskConfig();
+
+  public List<Stage.ConfigIssue> init(Stage.Context context, List<Stage.ConfigIssue> issues) {
+    s3Config.init(context, S3_CONFIG_PREFIX, proxyConfig, issues, -1);
+
+    return issues;
+  }
+
+  public void destroy() {
+    s3Config.destroy();
+  }
+
+}
