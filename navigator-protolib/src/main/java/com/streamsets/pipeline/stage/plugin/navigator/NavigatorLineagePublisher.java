d0776701a81a89dc6f9c50567c9979147f3bb0f6
Jarek Jarcec Cecho <jarcec@streamsets.com>
2017-06-17 11:39:59 -0700

    SDC-6495: Lineage: Create scaffolding for Navigator plugin
    Change-Id: Ia9b86332ecc5669a9ead01d3e2b72ab36d62038e
    Reviewed-on: https://review.streamsets.net/8890
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

---
+++ navigator-protolib/src/main/java/com/streamsets/pipeline/stage/plugin/navigator/NavigatorLineagePublisher.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.plugin.navigator;
+
+import com.streamsets.pipeline.api.lineage.LineageEvent;
+import com.streamsets.pipeline.api.lineage.LineagePublisher;
+import com.streamsets.pipeline.api.lineage.LineagePublisherDef;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.List;
+
+@LineagePublisherDef(
+  label = "Navigator",
+  description = "Publishes lineage data to Cloudera Navigator.",
+  configurationPrefix = "navigator"
+)
+public class NavigatorLineagePublisher implements LineagePublisher {
+
+  private static final Logger LOG = LoggerFactory.getLogger(NavigatorLineagePublisher.class);
+
+  @Override
+  public List<ConfigIssue> init(Context context) {
+    LOG.info("Navigator Lineage Plugin initializing");
+    return Collections.emptyList();
+  }
+
+  @Override
+  public void run(List<LineageEvent> events) {
+
+  }
+
+  @Override
+  public void destroy() {
+    LOG.info("Navigator Lineage Plugin destroying");
+  }
+}
