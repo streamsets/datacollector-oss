f4f2d6f4e4492f9b2c82e98d97deac316fa8cf08
Madhukar Devaraju <madhu@streamsets.com>
2017-06-29 16:21:34 -0700

    SDC-6583. UI: Fix bug in revert changes modal dialog
    Change-Id: I5a9f6210a13e515a1bb6a2f974583abca01f7b92
    Reviewed-on: https://review.streamsets.net/9055
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>

--- datacollector-ui/src/main/webapp/app/home/library/revert_changes/revertChanges.js
+++ datacollector-ui/src/main/webapp/app/home/library/revert_changes/revertChanges.js
@@ -40,57 +40,60 @@ angular
         ).then(
           function(res) {
             var commits = res.data;
+            var remotePipeline;
             if (commits && commits.length) {
-              var remotePipeline = _.find(commits, function(commit) {
+              remotePipeline = _.find(commits, function(commit) {
                 return commit.version === $scope.dpmPipelineVersion;
               });
+            }
 
-              if (remotePipeline) {
-                api.remote.getPipeline(authService.getRemoteBaseUrl(), authService.getSSOToken(), remotePipeline)
-                  .then(
-                    function(res) {
-                      var orgRemotePipeline = res.data;
-                      var pipelineEnvelope = {
-                        pipelineConfig: JSON.parse(orgRemotePipeline.pipelineDefinition),
-                        pipelineRules: JSON.parse(orgRemotePipeline.currentRules.rulesDefinition)
-                      };
+            if (remotePipeline) {
+              api.remote.getPipeline(authService.getRemoteBaseUrl(), authService.getSSOToken(), remotePipeline)
+                .then(
+                  function(res) {
+                    var orgRemotePipeline = res.data;
+                    var pipelineEnvelope = {
+                      pipelineConfig: JSON.parse(orgRemotePipeline.pipelineDefinition),
+                      pipelineRules: JSON.parse(orgRemotePipeline.currentRules.rulesDefinition)
+                    };
 
-                      api.pipelineAgent.importPipelineConfig(pipelineInfo.pipelineId, pipelineEnvelope, true)
-                        .then(
-                          function(res) {
-                            $scope.updatedPipelineConfig = res.data.pipelineConfig;
-                            $scope.pipelineVersion = orgRemotePipeline.version;
-                            var newMetadata = res.data.pipelineConfig.metadata;
-                            newMetadata['lastConfigId'] = res.data.pipelineConfig.uuid;
-                            newMetadata['lastRulesId'] = res.data.pipelineRules.uuid;
-                            api.pipelineAgent.savePipelineMetadata(pipelineInfo.pipelineId, newMetadata)
-                              .then(
-                                function(res) {
-                                  $modalInstance.close($scope.updatedPipelineConfig);
-                                },
-                                function(res) {
-                                  $scope.common.errors = [res.data];
-                                  $scope.operationInProgress = false;
-                                }
-                              );
-                          },
-                          function(res) {
-                            $scope.common.errors = [res.data];
-                            $scope.operationInProgress = false;
-                          }
-                        );
-                    },
-                    function(res) {
-                      $scope.common.errors = [res.data];
-                    }
-                  );
-              } else {
-                $scope.common.errors = ['No pipeline found in DPM Pipeline Repository'];
-                $scope.operationInProgress = false;
-              }
+                    api.pipelineAgent.importPipelineConfig(pipelineInfo.pipelineId, pipelineEnvelope, true)
+                      .then(
+                        function(res) {
+                          $scope.updatedPipelineConfig = res.data.pipelineConfig;
+                          $scope.pipelineVersion = orgRemotePipeline.version;
+                          var newMetadata = res.data.pipelineConfig.metadata;
+                          newMetadata['lastConfigId'] = res.data.pipelineConfig.uuid;
+                          newMetadata['lastRulesId'] = res.data.pipelineRules.uuid;
+                          api.pipelineAgent.savePipelineMetadata(pipelineInfo.pipelineId, newMetadata)
+                            .then(
+                              function(res) {
+                                $modalInstance.close($scope.updatedPipelineConfig);
+                              },
+                              function(res) {
+                                $scope.common.errors = [res.data];
+                                $scope.operationInProgress = false;
+                              }
+                            );
+                        },
+                        function(res) {
+                          $scope.common.errors = [res.data];
+                          $scope.operationInProgress = false;
+                        }
+                      );
+                  },
+                  function(res) {
+                    $scope.common.errors = [res.data];
+                  }
+                );
+            } else {
+              $scope.common.errors = ['No pipeline found in DPM Pipeline Repository with pipeline ID: ' +
+              $scope.dpmPipelineId];
+              $scope.operationInProgress = false;
             }
           },
           function(res) {
+            $scope.operationInProgress = false;
             $scope.common.errors = [res.data];
           }
         );
