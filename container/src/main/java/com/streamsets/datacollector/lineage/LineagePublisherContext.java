f4b764a491b243b44427f9aac614c7b26690e841
Jarek Jarcec Cecho <jarcec@streamsets.com>
2017-07-03 13:00:44 -0700

    SDC-6642: Lineage: Provide implementation of LineagePlugin.Context
    Change-Id: Id196e1c33969130c80f8f53cf3b9b4c32eb18b16
    Reviewed-on: https://review.streamsets.net/9083
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Bob Plotts <bob@streamsets.com>

---
+++ container/src/main/java/com/streamsets/datacollector/lineage/LineagePublisherContext.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.datacollector.lineage;
+
+import com.streamsets.datacollector.util.Configuration;
+import com.streamsets.pipeline.api.ErrorCode;
+import com.streamsets.pipeline.api.impl.ErrorMessage;
+import com.streamsets.pipeline.api.lineage.LineagePublisher;
+
+public class LineagePublisherContext implements LineagePublisher.Context {
+
+  private static final String CONF_PREFIX = "lineage_publisher.";
+
+  private final String id;
+  private final Configuration configuration;
+  private final String confPrefix;
+
+  public LineagePublisherContext(
+    String id,
+    Configuration configuration
+  ) {
+    this.id = id;
+    this.configuration = configuration;
+    this.confPrefix = CONF_PREFIX + id + ".";
+  }
+
+  @Override
+  public String getId() {
+    return id;
+  }
+
+  public static class ConfigIssueImpl implements LineagePublisher.ConfigIssue {
+    private final ErrorMessage errorMessage;
+
+    public ConfigIssueImpl(ErrorCode errorCode, Object... args) {
+      this.errorMessage = new ErrorMessage(errorCode, args);
+    }
+  }
+
+  @Override
+  public LineagePublisher.ConfigIssue createConfigIssue(ErrorCode errorCode, Object... args) {
+    return new ConfigIssueImpl(errorCode, args);
+  }
+
+  @Override
+  public String getConfig(String configName) {
+    return configuration.get(confPrefix + configName, null);
+  }
+}
---
+++ container/src/test/java/com/streamsets/datacollector/lineage/TestLineagePublisherContext.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.datacollector.lineage;
+
+import com.streamsets.datacollector.util.Configuration;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLineagePublisherContext {
+
+  @Test
+  public void testId() {
+    LineagePublisherContext context = new LineagePublisherContext("random-id", getConfiguration());
+    Assert.assertEquals("random-id", context.getId());
+  }
+
+  @Test
+  public void testGetConfig() {
+    LineagePublisherContext context = new LineagePublisherContext("random-id", getConfiguration());
+
+    Assert.assertEquals("StreamSets", context.getConfig("company"));
+    Assert.assertNull(context.getConfig("this-does-not-exists"));
+  }
+
+  private Configuration getConfiguration() {
+    Configuration configuration = new Configuration();
+    configuration.set("lineage_publisher.random-id.company", "StreamSets");
+    return configuration;
+  }
+}
