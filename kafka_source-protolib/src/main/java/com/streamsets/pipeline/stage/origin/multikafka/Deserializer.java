a1668067ccf86939b49a3ce86706fae6deb477a0
Tony McKeehan <anthony@streamsets.com>
2017-08-08 15:06:04 -0700

    SDC-551. KafkaSource should have option to consume data from multiple topics
    Change-Id: I143542d3345ae0c4b5c10e72404bffe9604fe16f
    Reviewed-on: https://review.streamsets.net/9807
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

---
+++ kafka_source-protolib/src/main/java/com/streamsets/pipeline/stage/origin/multikafka/Deserializer.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.origin.multikafka;
+
+import com.streamsets.pipeline.api.Label;
+
+public enum Deserializer implements Label {
+  STRING("String", "org.apache.kafka.common.serialization.StringDeserializer", "org.apache.kafka.common.serialization.StringDeserializer"),
+  DEFAULT("Default", "org.apache.kafka.common.serialization.ByteArrayDeserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer"),
+  CONFLUENT("Confluent", "io.confluent.kafka.serializers.KafkaAvroDeserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+
+  private final String label;
+  private final String keyClass;
+  private final String valueClass;
+
+  Deserializer(String label, String keyClass, String valueClass) {
+    this.label = label;
+    this.keyClass = keyClass;
+    this.valueClass = valueClass;
+  }
+
+  @Override
+  public String getLabel() {
+    return label;
+  }
+
+  public String getKeyClass() {
+    return keyClass;
+  }
+
+  public String getValueClass() {
+    return valueClass;
+  }
+}
