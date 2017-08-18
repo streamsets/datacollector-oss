ea0895bce2f13ac9ca1924d9274d855b2608d085
Santhosh Kumar Manavasi Lakshminarayanan <santhosh@streamsets.com>
2017-08-17 23:36:07 -0700

    SDC-7118. Create Windows Event Log Source.
    Change-Id: Ic34481762d05ba0ade79cfc2cb03068370c997be
    Reviewed-on: https://review.streamsets.net/9987
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

---
+++ windows-lib/src/main/java/com/streamsets/pipeline/stage/origin/windows/WindowsEventLogDSource.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.origin.windows;
+
+import com.streamsets.pipeline.api.ConfigDef;
+import com.streamsets.pipeline.api.ConfigGroups;
+import com.streamsets.pipeline.api.ExecutionMode;
+import com.streamsets.pipeline.api.GenerateResourceBundle;
+import com.streamsets.pipeline.api.Source;
+import com.streamsets.pipeline.api.StageDef;
+import com.streamsets.pipeline.api.ValueChooserModel;
+import com.streamsets.pipeline.configurablestage.DSource;
+
+@GenerateResourceBundle
+@StageDef(
+    version = 1,
+    label = "Windows Event Log",
+    description = "Reads data from Windows Event Log",
+    execution = {ExecutionMode.EDGE},
+    //TODO SDC-7119
+    icon = "",
+    onlineHelpRefUrl = ""
+)
+
+@ConfigGroups(Groups.class)
+public class WindowsEventLogDSource extends DSource {
+
+  @ConfigDef(required = true,
+      type = ConfigDef.Type.MODEL,
+      defaultValue = "APPLICATION",
+      label = "Log Name",
+      description = "Log Name",
+      displayPosition = 10,
+      group = "WINDOWS"
+  )
+  @ValueChooserModel(LogNameChooserValues.class)
+  public LogName logName = LogName.APPLICATION;
+
+  @ConfigDef(required = true,
+      type = ConfigDef.Type.MODEL,
+      defaultValue = "ALL",
+      label = "Read Mode",
+      description = "Read Mode",
+      displayPosition = 20,
+      group = "WINDOWS"
+  )
+  @ValueChooserModel(ReadModeChooserValues.class)
+  public ReadMode readMode = ReadMode.ALL;
+
+  @Override
+  protected Source createSource() {
+    return new WindowsEventLogSource(logName, readMode);
+  }
+}
---
+++ windows-lib/src/main/java/com/streamsets/pipeline/stage/origin/windows/WindowsEventLogSource.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.origin.windows;
+
+import com.streamsets.pipeline.api.BatchMaker;
+import com.streamsets.pipeline.api.ExecutionMode;
+import com.streamsets.pipeline.api.StageException;
+import com.streamsets.pipeline.api.base.BaseSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class WindowsEventLogSource extends BaseSource {
+  private static final Logger LOG = LoggerFactory.getLogger(WindowsEventLogSource.class);
+
+  private final LogName logName;
+  private final ReadMode readMode;
+
+  WindowsEventLogSource(LogName logName, ReadMode readMode) {
+    this.logName = logName;
+    this.readMode = readMode;
+  }
+
+  @Override
+  public List<ConfigIssue> init() {
+      LOG.debug("Windows Event Log Source configured with Log Name - '{}', Read Mode -'{}'", logName, readMode);
+      return super.init();
+  }
+
+  @Override
+  public String produce(String lastSourceOffset, int maxBatchSize, BatchMaker batchMaker) throws StageException {
+    throw new UnsupportedOperationException("Windows Event Log Source Is supported only in " + ExecutionMode.EDGE.name());
+  }
+
+}
