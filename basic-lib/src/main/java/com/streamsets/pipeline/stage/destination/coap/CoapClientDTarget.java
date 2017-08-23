aebc0cf2a33b00726a1ba21389c17db78cbe72d2
Madhukar Devaraju <madhu@streamsets.com>
2017-08-23 07:45:18 -0700

    SDC-7158. Fix execution mode for CoAP Client Destination
    Change-Id: I26fa79c9a0a7a5f9066f5b9353e27737441f6cc1
    Reviewed-on: https://review.streamsets.net/10058
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Santhosh Kumar <santhosh@streamsets.com>

--- basic-lib/src/main/java/com/streamsets/pipeline/stage/destination/coap/CoapClientDTarget.java
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/destination/coap/CoapClientDTarget.java
@@ -17,6 +17,7 @@ package com.streamsets.pipeline.stage.destination.coap;
 
 import com.streamsets.pipeline.api.ConfigDefBean;
 import com.streamsets.pipeline.api.ConfigGroups;
+import com.streamsets.pipeline.api.ExecutionMode;
 import com.streamsets.pipeline.api.GenerateResourceBundle;
 import com.streamsets.pipeline.api.StageDef;
 import com.streamsets.pipeline.api.Target;
@@ -28,6 +29,13 @@ import com.streamsets.pipeline.lib.coap.Groups;
     label = "CoAP Client",
     description = "Uses a CoAP client to write data",
     icon = "coap.png",
+    execution = {
+        ExecutionMode.STANDALONE,
+        ExecutionMode.CLUSTER_BATCH,
+        ExecutionMode.CLUSTER_YARN_STREAMING,
+        ExecutionMode.CLUSTER_MESOS_STREAMING,
+        ExecutionMode.EDGE
+    },
     recordsByRef = true,
     onlineHelpRefUrl = "index.html#Destinations/CoAPClient.html#task_d2p_w3n_sz"
 )
