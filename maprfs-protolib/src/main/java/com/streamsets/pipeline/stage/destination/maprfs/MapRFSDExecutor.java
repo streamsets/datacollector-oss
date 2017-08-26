bbe24aad0cdfa67898080bcdf66e0e6e7006d3d9
Jarek Jarcec Cecho <jarcec@streamsets.com>
2017-08-26 09:02:39 -0700

    SDC-7171: Replicate the HDFS Executor for MapR FS Executor
    Change-Id: I7d0e777a3b4ce099971504a52929b40fb0a6e6ee
    Reviewed-on: https://review.streamsets.net/10125
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Ji Sun Kim <jisun@streamsets.com>

---
+++ maprfs-protolib/src/main/java/com/streamsets/pipeline/stage/destination/maprfs/MapRFSDExecutor.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.destination.maprfs;
+
+import com.streamsets.pipeline.api.Executor;
+import com.streamsets.pipeline.api.GenerateResourceBundle;
+import com.streamsets.pipeline.api.PipelineLifecycleStage;
+import com.streamsets.pipeline.api.StageDef;
+import com.streamsets.pipeline.stage.destination.hdfs.metadataexecutor.HdfsMetadataDExecutor;
+
+@StageDef(
+    version = 1,
+    label = "MapR FS File Metadata",
+    description = "Changes MapR FS file metadata such as renaming files or changing permissions.",
+    icon = "mapr.png",
+    producesEvents = true,
+    privateClassLoader = false,
+    onlineHelpRefUrl = "index.html#Executors/MapRFSFileMeta.html#task_v3c_bvh_z1b"
+)
+@GenerateResourceBundle
+@PipelineLifecycleStage
+public class MapRFSDExecutor extends HdfsMetadataDExecutor {
+
+  @Override
+  protected Executor createExecutor() {
+    // Since we're inheriting the configuration from usual HDFS executor, we don't have a way to override the default value in the
+    // annotation and hence the default value is kind of "hidden" and supplied here.
+    if(connection.hdfsUri == null || connection.hdfsUri.isEmpty()) {
+      connection.hdfsUri = "maprfs:///";
+    }
+
+    return super.createExecutor();
+  }
+
+}
