f9528472cdaffe80e94ed9ef75f917fc926a007f
Madhukar Devaraju <madhu@streamsets.com>
2017-08-28 08:02:30 -0700

    SDC-7189. Implement destination to write data to Azure IoT Hub
    Change-Id: Icb32ce4e63eb0b8f91c7f2532882f90c58014538
    Reviewed-on: https://review.streamsets.net/10145
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>
    (cherry picked from commit 89c686543be3b00332164d2719ef4021c63a7183)
    Reviewed-on: https://review.streamsets.net/10151

---
+++ azure-lib/src/main/java/com/streamsets/pipeline/stage/destination/iothub/IotHubProducerConfigBean.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.destination.iothub;
+
+import com.streamsets.pipeline.api.ConfigDef;
+import com.streamsets.pipeline.api.ConfigDefBean;
+import com.streamsets.pipeline.api.ValueChooserModel;
+import com.streamsets.pipeline.config.DataFormat;
+import com.streamsets.pipeline.lib.eventhubs.DataFormatChooserValues;
+import com.streamsets.pipeline.stage.destination.lib.DataGeneratorFormatConfig;
+
+public class IotHubProducerConfigBean {
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.MODEL,
+      defaultValue = "JSON",
+      label = "Data Format",
+      description = "Event Hub payload data format",
+      displayPosition = 1,
+      group = "DATA_FORMAT"
+  )
+  @ValueChooserModel(DataFormatChooserValues.class)
+  public DataFormat dataFormat = DataFormat.JSON;
+
+  @ConfigDefBean(groups = {"IOT_HUB"})
+  public DataGeneratorFormatConfig dataGeneratorFormatConfig;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.STRING,
+      label = "IoT Hub Name",
+      defaultValue = "",
+      description = "",
+      displayPosition = 10,
+      group = "IOT_HUB"
+  )
+  public String iotHubName = "";
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.STRING,
+      label = "Device ID",
+      defaultValue = "",
+      description = "",
+      displayPosition = 20,
+      group = "IOT_HUB"
+  )
+  public String deviceId = "";
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.STRING,
+      label = "Shared Access Signature Key",
+      defaultValue = "",
+      description = "",
+      displayPosition = 30,
+      group = "IOT_HUB"
+  )
+  public String sasKey = "";
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.NUMBER,
+      label = "Maximum Request Time (sec)",
+      defaultValue = "300",
+      description = "Maximum time to wait for each batch request completion.",
+      displayPosition = 40,
+      group = "IOT_HUB"
+  )
+  public long maxRequestCompletionSecs = 300;
+
+}
---
+++ azure-lib/src/test/java/com/streamsets/pipeline/stage/destination/iothub/IotHubProducerTargetBuilder.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.destination.iothub;
+
+import com.streamsets.pipeline.config.DataFormat;
+import com.streamsets.pipeline.stage.destination.lib.DataGeneratorFormatConfig;
+
+class IotHubProducerTargetBuilder {
+  private IotHubProducerConfigBean producerConf;
+
+  IotHubProducerTargetBuilder() {
+    producerConf = new IotHubProducerConfigBean();
+    producerConf.dataFormat = DataFormat.TEXT;
+    producerConf.dataGeneratorFormatConfig = new DataGeneratorFormatConfig();
+    producerConf.dataGeneratorFormatConfig.textFieldPath = "/text";
+  }
+
+  IotHubProducerTargetBuilder iotHubName(String iotHubName) {
+    producerConf.iotHubName = iotHubName;
+    return this;
+  }
+
+  IotHubProducerTargetBuilder deviceId(String deviceId) {
+    producerConf.deviceId = deviceId;
+    return this;
+  }
+
+  IotHubProducerTargetBuilder sasKey(String sasKey) {
+    producerConf.sasKey = sasKey;
+    return this;
+  }
+
+  IotHubProducerTarget build() {
+    return new IotHubProducerTarget(producerConf);
+  }
+
+}
