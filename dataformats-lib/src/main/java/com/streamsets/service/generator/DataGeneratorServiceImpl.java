c731a159715560d86aea40f6bd55035f54926603
Jarek Jarcec Cecho <jarcec@streamsets.com>
2017-09-21 10:39:09 -0700

    SDC-7383: Service: Add scaffolding of example DataGeneratorService
    This patch adds non-working example of DataFormat service. It should
    still be loadable by the framework and hence it will enable progress on
    the framework-loading portion of the project.
    
    Change-Id: I53e2bb05c2402be3f83592d5ca1ae92b65dc1bb6
    Reviewed-on: https://review.streamsets.net/10535
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Ji Sun Kim <jisun@streamsets.com>

---
+++ dataformats-lib/src/main/java/com/streamsets/service/generator/DataGeneratorServiceImpl.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2017 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.service.generator;
+
+import com.streamsets.pipeline.api.ConfigDef;
+import com.streamsets.pipeline.api.ConfigDefBean;
+import com.streamsets.pipeline.api.ValueChooserModel;
+import com.streamsets.pipeline.api.service.Service;
+import com.streamsets.pipeline.api.service.ServiceDef;
+import com.streamsets.pipeline.api.service.dataformats.DataFormatGeneratorService;
+import com.streamsets.pipeline.api.service.dataformats.DataGenerator;
+import com.streamsets.pipeline.config.DataFormat;
+import com.streamsets.pipeline.stage.destination.lib.DataGeneratorFormatConfig;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.List;
+
+@ServiceDef(
+  provides = DataFormatGeneratorService.class,
+  version = 1,
+  label = "DataFormat Generator"
+)
+public class DataGeneratorServiceImpl implements Service, DataFormatGeneratorService {
+
+  @ConfigDef(
+    required = true,
+    type = ConfigDef.Type.MODEL,
+    label = "Data Format",
+    description = "Data Format",
+    displayPosition = 1,
+    group = "DATA_FORMAT"
+  )
+  @ValueChooserModel(DataFormatChooserValues.class)
+  public DataFormat dataFormat;
+
+  @ConfigDefBean()
+  public DataGeneratorFormatConfig dataGeneratorFormatConfig;
+
+  @Override
+  public List<ConfigIssue> init(Context context) {
+    /* TODO: We need to first abstract some of the common interfaces up
+    return dataGeneratorFormatConfig.init(
+      context,
+      dataFormat,
+      "DATA_FORMAT",
+      "dataGeneratorFormatConfig.",
+      new LinkedList<>()
+    );
+    */
+
+    return Collections.emptyList();
+  }
+
+  @Override
+  public void destroy() {
+  }
+
+  @Override
+  public DataGenerator getGenerator(OutputStream os) throws IOException {
+    return null;
+  }
+}
