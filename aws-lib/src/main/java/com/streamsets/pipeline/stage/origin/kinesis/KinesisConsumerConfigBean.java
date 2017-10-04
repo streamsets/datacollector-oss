ed6754423ef8da38552ede374b422c7753a09239
Jarek Jarcec Cecho <jarcec@streamsets.com>
2017-10-04 11:56:29 -0700

    SDC-7462: Kinesis Consumer Initial Timestamp default value throwing validation error
    Change-Id: Ia10841bbb792aa02fde5a131309b6a7ac58d8df9
    Reviewed-on: https://review.streamsets.net/10734
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Hari Shreedharan <hshreedharan@streamsets.com>

--- aws-lib/src/main/java/com/streamsets/pipeline/stage/origin/kinesis/KinesisConsumerConfigBean.java
+++ aws-lib/src/main/java/com/streamsets/pipeline/stage/origin/kinesis/KinesisConsumerConfigBean.java
@@ -20,6 +20,7 @@ import com.streamsets.pipeline.api.ConfigDef;
 import com.streamsets.pipeline.api.ConfigDefBean;
 import com.streamsets.pipeline.api.ValueChooserModel;
 import com.streamsets.pipeline.config.DataFormat;
+import com.streamsets.pipeline.lib.el.TimeNowEL;
 import com.streamsets.pipeline.stage.lib.aws.ProxyConfig;
 import com.streamsets.pipeline.stage.lib.kinesis.KinesisConfigBean;
 import com.streamsets.pipeline.stage.origin.lib.DataParserFormatConfig;
@@ -64,6 +65,7 @@ public class KinesisConsumerConfigBean extends KinesisConfigBean {
       dependsOn = "initialPositionInStream",
       triggeredByValue = "AT_TIMESTAMP",
       displayPosition = 45,
+      elDefs = TimeNowEL.class,
       group = "#0"
   )
   public long initialTimestamp;
