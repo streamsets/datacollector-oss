3110cb61d54bb2e70cd0e378a15580b094abce6e
Virag Kothari <virag@streamsets.com>
2017-11-11 18:59:25 -0800

    SDC-7779. Don't allow choice of 'Direct to DPM' statistics in Cluster streaming pipelines
    Change-Id: Id2df4f6e4abd4dc9d7e0cafac898f346df9f2804
    Reviewed-on: https://review.streamsets.net/11398
    Reviewed-by: Santhosh Kumar <santhosh@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- container/src/main/java/com/streamsets/datacollector/validation/ValidationError.java
+++ container/src/main/java/com/streamsets/datacollector/validation/ValidationError.java
@@ -95,7 +95,6 @@ public enum ValidationError implements ErrorCode {
   VALIDATION_0071("Stage '{}' from '{}' library does not support '{}' execution mode"),
   VALIDATION_0072("Data collector is in standalone mode, cannot run pipeline cluster mode"),
   VALIDATION_0073("Data collector is in cluster mode, cannot run pipeline standalone mode"),
-  VALIDATION_0074("Error Stage '{}' from '{}' library does not support '{}' execution mode"),
   VALIDATION_0080("Precondition '{}' must begin with '${' and end with '}'"),
   VALIDATION_0081("Invalid precondition '{}': {}"),
   VALIDATION_0082("Cannot create runner with execution mode '{}', another runner with execution mode '{}'"
