605a94206a35a3991de5967579cb457e366af12f
Jarek Jarcec Cecho <jarcec@streamsets.com>
2018-01-03 14:41:15 -0800

    SDC-8183: Service: Limit file formats required by Generator and Parser Processors
    Now when both DataFormat services - parser and generator - supports
    filtering of what formats should be available for each stage, this patch
    changes Data Generator and Data Parser processor to use that
    functionality and filter out only those file formats that does make
    sense for given stage.
    
    Change-Id: I8e57c172c8d0697a6e53b453a85f6e4cd0f9494b
    Reviewed-on: https://review.streamsets.net/12293
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- basic-lib/src/main/java/com/streamsets/pipeline/stage/processor/generator/DataGeneratorDProcessor.java
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/processor/generator/DataGeneratorDProcessor.java
@@ -20,6 +20,7 @@ import com.streamsets.pipeline.api.ConfigGroups;
 import com.streamsets.pipeline.api.GenerateResourceBundle;
 import com.streamsets.pipeline.api.Processor;
 import com.streamsets.pipeline.api.StageDef;
+import com.streamsets.pipeline.api.service.ServiceConfiguration;
 import com.streamsets.pipeline.api.service.ServiceDependency;
 import com.streamsets.pipeline.api.service.dataformats.DataFormatGeneratorService;
 import com.streamsets.pipeline.configurablestage.DProcessor;
@@ -29,7 +30,12 @@ import com.streamsets.pipeline.configurablestage.DProcessor;
   label="Data Generator",
   description = "Serializes records to various different data formats.",
   icon="coding.png",
-  services = @ServiceDependency(service = DataFormatGeneratorService.class),
+  services = @ServiceDependency(
+    service = DataFormatGeneratorService.class,
+    configuration = {
+      @ServiceConfiguration(name = "displayFormats", value = "AVRO,BINARY,DELIMITED,JSON,PROTOBUF,TEXT,SDC_JSON,XML")
+    }
+  ),
   onlineHelpRefUrl = "" // TODO(Add proper link)
 )
 @ConfigGroups(Groups.class)
