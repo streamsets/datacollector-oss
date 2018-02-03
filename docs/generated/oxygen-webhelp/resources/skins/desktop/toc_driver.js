2f7a7b90ec507afd89c9527dee014157795e137c
Loretta <loretta@streamsets.com>
2018-02-02 16:33:20 -0800

    [doc] generated help
    Change-Id: Ice289d793eebde80c0942e072989eb350fed0f0b
    Reviewed-on: https://review.streamsets.net/12755
    Reviewed-by: Alison Taylor <alison@streamsets.com>

--- docs/generated/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ docs/generated/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180126122040", "oxygen-webhelp/search/index-1.js?uniqueId=20180126122040", "oxygen-webhelp/search/index-2.js?uniqueId=20180126122040", "oxygen-webhelp/search/index-3.js?uniqueId=20180126122040"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180202042636", "oxygen-webhelp/search/index-1.js?uniqueId=20180202042636", "oxygen-webhelp/search/index-2.js?uniqueId=20180202042636", "oxygen-webhelp/search/index-3.js?uniqueId=20180202042636"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
