afdbb4a51451e4a78cc603e9064fedf419fa0271
Alison Taylor <alison@streamsets.com>
2018-02-08 09:11:33 -0800

    [doc] updated 3.1 help
    Change-Id: I66809e0cdc966bb75ff2710342859476ab6c90c2
    Reviewed-on: https://review.streamsets.net/12811
    Reviewed-by: Loretta Chen <loretta@streamsets.com>
    (cherry picked from commit 39447b7fd43ee34dd55b830a8404f8c32911ae18)
    Reviewed-on: https://review.streamsets.net/12812
    Reviewed-by: Alison Taylor <alison@streamsets.com>

--- docs/generated/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ docs/generated/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180206041913", "oxygen-webhelp/search/index-1.js?uniqueId=20180206041913", "oxygen-webhelp/search/index-2.js?uniqueId=20180206041913", "oxygen-webhelp/search/index-3.js?uniqueId=20180206041913"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180208090620", "oxygen-webhelp/search/index-1.js?uniqueId=20180208090620", "oxygen-webhelp/search/index-2.js?uniqueId=20180208090620", "oxygen-webhelp/search/index-3.js?uniqueId=20180208090620"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
