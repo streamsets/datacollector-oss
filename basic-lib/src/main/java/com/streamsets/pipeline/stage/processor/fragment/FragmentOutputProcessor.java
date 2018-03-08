059fd10504da4c6efe8a6bbcfa3d4ace95fdcad7
Hari Shreedharan <hshreedharan@streamsets.com>
2018-03-08 15:43:52 -0800

    SDC-8565. Add FragmentInput and FragmentOutput processors
    Change-Id: I412435587300b267e4d4f0d2f0c50dfbcc6f692e
    Reviewed-on: https://review.streamsets.net/13210
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>

---
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/processor/fragment/FragmentOutputProcessor.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2018 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.processor.fragment;
+
+import com.streamsets.pipeline.api.ExecutionMode;
+import com.streamsets.pipeline.api.GenerateResourceBundle;
+import com.streamsets.pipeline.api.StageDef;
+
+@GenerateResourceBundle
+@StageDef(
+    version = 1,
+    label = "Fragment Output Processor",
+    icon="dev.png",
+    execution = {
+        ExecutionMode.STANDALONE,
+        ExecutionMode.CLUSTER_BATCH,
+        ExecutionMode.CLUSTER_YARN_STREAMING,
+        ExecutionMode.CLUSTER_MESOS_STREAMING,
+        ExecutionMode.EDGE
+    },
+    onlineHelpRefUrl =""
+)
+// This is hidden by the UI, so the help, icon, description, label etc don't matter at all. Only execution mode matters.
+// The UI will not allow the output lane for this processor to change.
+public class FragmentOutputProcessor extends BaseFragmentIdentityProcessor {
+
+}
