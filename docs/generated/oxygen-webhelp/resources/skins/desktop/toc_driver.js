9c0f2e84017a93a604a065574ad9b9173c3ae523
Loretta <loretta@streamsets.com>
2018-03-19 20:04:22 -0700

    [doc] 3120 help for build
    Change-Id: I72ef88f507f422619a0b632d0178065112f4f001
    Reviewed-on: https://review.streamsets.net/13442
    Reviewed-by: Santhosh Kumar <santhosh@streamsets.com>
    (cherry picked from commit b35fc4145d33e403793ecdddd025b113b079814f)
    Reviewed-on: https://review.streamsets.net/13443
    Reviewed-by: Loretta Chen <loretta@streamsets.com>

--- docs/generated/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ docs/generated/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180305115719", "oxygen-webhelp/search/index-1.js?uniqueId=20180305115719", "oxygen-webhelp/search/index-2.js?uniqueId=20180305115719", "oxygen-webhelp/search/index-3.js?uniqueId=20180305115719"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180319075605", "oxygen-webhelp/search/index-1.js?uniqueId=20180319075605", "oxygen-webhelp/search/index-2.js?uniqueId=20180319075605", "oxygen-webhelp/search/index-3.js?uniqueId=20180319075605"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
