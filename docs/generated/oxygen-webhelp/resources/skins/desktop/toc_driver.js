fc8a39cd22fde62df8d2c368e4fbc220b0aa3808
Loretta <loretta@streamsets.com>
2018-04-16 16:55:22 -0700

    [doc] 3.2.0.0 help for build
    Change-Id: I4bb820a34b69e7d716e34b620d3df9e1537f347b
    Reviewed-on: https://review.streamsets.net/13969
    Reviewed-by: Gary Beichler <gary@streamsets.com>

--- docs/generated/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ docs/generated/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180405021547", "oxygen-webhelp/search/index-1.js?uniqueId=20180405021547", "oxygen-webhelp/search/index-2.js?uniqueId=20180405021547", "oxygen-webhelp/search/index-3.js?uniqueId=20180405021547"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180416044738", "oxygen-webhelp/search/index-1.js?uniqueId=20180416044738", "oxygen-webhelp/search/index-2.js?uniqueId=20180416044738", "oxygen-webhelp/search/index-3.js?uniqueId=20180416044738"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
