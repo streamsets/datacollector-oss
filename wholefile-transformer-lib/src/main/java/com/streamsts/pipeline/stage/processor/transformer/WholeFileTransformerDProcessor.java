05b34c94493da3708471dffa5823017cc50b740e
Ji Sun <jisun@streamsets.com>
2018-05-21 11:01:26 -0700

    SDC-9042. Create Empty Whole File Transformer Package
    * wholefile-transformer-lib is the new stage lib that transforms avro file to parquet file without the need for additinal execution engine such as YARN
    * wholefile-converter-protolib is the new protolib that contains the common avro-to-parquet code from mapreduce-protolib
    * hence, wholefile-transformer-lib and mapreduce-protolib will depend on wholefile-converter-protolib
    * this patch, wholefile transformer processor will pass along incoming records to libs
    * this is the empty package, the follow-up tickets will be updated under SDC-9044
    
    Change-Id: I9b9545f9ff6454f00587ea54e52c29b56d8e60d4
    Reviewed-on: https://review.streamsets.net/14641
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

---
+++ wholefile-transformer-lib/src/main/java/com/streamsts/pipeline/stage/processor/transformer/WholeFileTransformerDProcessor.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsts.pipeline.stage.processor.transformer;
+
+import com.streamsets.pipeline.api.ConfigGroups;
+import com.streamsets.pipeline.api.ExecutionMode;
+import com.streamsets.pipeline.api.GenerateResourceBundle;
+import com.streamsets.pipeline.api.Processor;
+import com.streamsets.pipeline.api.StageDef;
+import com.streamsets.pipeline.api.base.configurablestage.DProcessor;
+
+@GenerateResourceBundle
+@StageDef(
+    version = 1,
+    label = "Whole File Transformer",
+    description = "Tranform Whole File Data Format into Different Types",
+    execution = {ExecutionMode.STANDALONE,},
+    icon = "whole_file_transformer.png",
+    onlineHelpRefUrl =""
+)
+
+@ConfigGroups(Groups.class)
+public class WholeFileTransformerDProcessor extends DProcessor {
+  @Override
+  protected Processor createProcessor() {
+    return new WholeFileTransformerProcessor();
+  }
+}
