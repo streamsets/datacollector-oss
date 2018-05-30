81f7007e6f782139d1332a9bb955d6fb46b0c324
Madhukar Devaraju <madhu@streamsets.com>
2018-05-30 16:35:31 -0700

    SDC-9026. Add new destination To Origin Response
    Change-Id: I3464f3c1ba9441b63bf7ca5a6e4bf4590824ee05
    Reviewed-on: https://review.streamsets.net/14601
    Reviewed-by: Alejandro Abdelnur <tucu@streamsets.com>

---
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/destination/tooriginresponse/ToOriginResponseDTarget.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2018 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.destination.tooriginresponse;
+
+import com.streamsets.pipeline.api.GenerateResourceBundle;
+import com.streamsets.pipeline.api.HideConfigs;
+import com.streamsets.pipeline.api.StageDef;
+import com.streamsets.pipeline.api.Target;
+import com.streamsets.pipeline.api.base.configurablestage.DTarget;
+
+@StageDef(
+    version = 1,
+    label = "Send to Origin Response",
+    description = "Sends records to the pipeline configured origin response handling",
+    icon="response.png",
+    onlineHelpRefUrl ="" // TODO
+)
+@HideConfigs(preconditions = true, onErrorRecord = true)
+@GenerateResourceBundle
+public class ToOriginResponseDTarget extends DTarget {
+
+  @Override
+  protected Target createTarget() {
+    return new ToOriginResponseTarget();
+  }
+
+}
