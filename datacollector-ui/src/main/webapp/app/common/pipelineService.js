495dec392ca76f142cb1035e1c237aa526145dea
Jarek Jarcec Cecho <jarcec@streamsets.com>
2018-06-09 09:38:19 -0700

    SDC-9233: Hide hidden stages from stage selector in UI
    Hide hidden stage in the stage selector on the right side of pipeline
    canvas and in the select boxes for "Select Processor to add in
    between..." and other similar menus.
    
    Change-Id: I406244079dd247caa59c0d6de29403622aa6ee59
    Reviewed-on: https://review.streamsets.net/14991
    Reviewed-by: Jeff Evans <jeff@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- datacollector-ui/src/main/webapp/app/common/pipelineService.js
+++ datacollector-ui/src/main/webapp/app/common/pipelineService.js
@@ -2001,6 +2001,7 @@ angular.module('dataCollectorApp.common')
           }
           alreadyAdded[s.name + s.version] = true;
           return s.type === type && !s.errorStage && !s.statsAggregatorStage &&
+            s.hideStage.length == 0 &&
             s.name.indexOf('_fragment_') === -1 &&
             s.library !== 'streamsets-datacollector-stats-lib' &&
             (!executionMode || s.executionModes.indexOf(executionMode) !== -1);
--- datacollector-ui/src/main/webapp/app/home/stageLibrary/stageLibrary.js
+++ datacollector-ui/src/main/webapp/app/home/stageLibrary/stageLibrary.js
@@ -50,6 +50,7 @@ angular
         angular.forEach($scope.stageLibraries, function(stageLibrary) {
           if (libraryFilter(stageLibrary) && !_.contains(stageNameList, stageLibrary.name) &&
             regex.test(stageLibrary.label) && !stageLibrary.errorStage && !stageLibrary.statsAggregatorStage &&
+            stageLibrary.hideStage.length == 0 &&
             stageLibrary.library !== 'streamsets-datacollector-stats-lib' &&
             stageLibrary.name.indexOf('_fragment_') === -1 &&
             ($scope.executionMode !== 'EDGE' || stageLibrary.executionModes.indexOf($scope.executionMode) !== -1)
