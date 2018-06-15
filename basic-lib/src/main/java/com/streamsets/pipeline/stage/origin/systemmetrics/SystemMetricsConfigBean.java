01eeef5ae06810ad7f71e4d8ce68d93e8dafb083
Madhukar Devaraju <madhu@streamsets.com>
2018-06-14 17:34:07 -0700

    SDC-9272. Add Server Metrics config definitions
    Change-Id: I49335f525a4dcee5cc3eccd80f2785c01a68cc28
    Reviewed-on: https://review.streamsets.net/15085
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Adam Kunicki <adam@streamsets.com>

---
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/origin/systemmetrics/SystemMetricsConfigBean.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2018 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.origin.systemmetrics;
+
+import com.streamsets.pipeline.api.ConfigDef;
+
+public class SystemMetricsConfigBean {
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.NUMBER,
+      defaultValue = "2000",
+      label = "Delay Between Batches",
+      description = "Milliseconds to wait before sending the next batch",
+      min = 0,
+      max = Integer.MAX_VALUE,
+      displayPosition = 10,
+      group = "SYSTEM_METRICS"
+  )
+  public long delay;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.BOOLEAN,
+      label = "Fetch CPU Stats",
+      description = "",
+      displayPosition = 20,
+      group = "SYSTEM_METRICS",
+      defaultValue = "true"
+  )
+  public boolean fetchCpuStats = true;
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.BOOLEAN,
+      label = "Fetch Memory Stats",
+      description = "",
+      displayPosition = 30,
+      group = "SYSTEM_METRICS",
+      defaultValue = "true"
+  )
+  public boolean fetchMemStats = true;
+
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.BOOLEAN,
+      label = "Fetch Disk Stats",
+      description = "",
+      displayPosition = 40,
+      group = "SYSTEM_METRICS",
+      defaultValue = "true"
+  )
+  public boolean fetchDiskStats = true;
+
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.BOOLEAN,
+      label = "Fetch Network Stats",
+      description = "",
+      displayPosition = 50,
+      group = "SYSTEM_METRICS",
+      defaultValue = "true"
+  )
+  public boolean fetchNetStats = true;
+}
---
+++ basic-lib/src/main/java/com/streamsets/pipeline/stage/origin/systemmetrics/SystemMetricsDSource.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.origin.systemmetrics;
+
+import com.streamsets.pipeline.api.ConfigDefBean;
+import com.streamsets.pipeline.api.ConfigGroups;
+import com.streamsets.pipeline.api.ExecutionMode;
+import com.streamsets.pipeline.api.GenerateResourceBundle;
+import com.streamsets.pipeline.api.Source;
+import com.streamsets.pipeline.api.StageDef;
+import com.streamsets.pipeline.api.base.configurablestage.DSource;
+
+@GenerateResourceBundle
+@StageDef(
+    version = 1,
+    label = "System Metrics",
+    description = "Reads information on system utilization (CPU, memory, disks, network)",
+    execution = {ExecutionMode.EDGE},
+    icon = "metrics.png",
+    onlineHelpRefUrl ="" //TODO: Update help URL
+)
+@ConfigGroups(Groups.class)
+public class SystemMetricsDSource extends DSource {
+
+  @ConfigDefBean
+  public SystemMetricsConfigBean conf;
+
+  @Override
+  protected Source createSource() {
+    return new SystemMetricsSource();
+  }
+}
