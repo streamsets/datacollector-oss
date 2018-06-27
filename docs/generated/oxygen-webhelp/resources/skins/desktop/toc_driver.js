40620cf6f7d1e0afdda42409748714d6ce1f150f
Loretta <loretta@streamsets.com>
2018-06-27 12:44:48 -0700

    [doc] first help drop for 3.3
    Change-Id: Id1195e9d1732256cdd05eec296fa76e4650a94cc
    Reviewed-on: https://review.streamsets.net/15238
    Reviewed-by: Alison Taylor <alison@streamsets.com>

--- docs/generated/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ docs/generated/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180523064315", "oxygen-webhelp/search/index-1.js?uniqueId=20180523064315", "oxygen-webhelp/search/index-2.js?uniqueId=20180523064315", "oxygen-webhelp/search/index-3.js?uniqueId=20180523064315"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180627115021", "oxygen-webhelp/search/index-1.js?uniqueId=20180627115021", "oxygen-webhelp/search/index-2.js?uniqueId=20180627115021", "oxygen-webhelp/search/index-3.js?uniqueId=20180627115021"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
