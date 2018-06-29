cbea4db8bfb6a914524cbb20d6a2a44005e30dc5
Alison Taylor <alison@streamsets.com>
2018-06-29 11:45:07 -0700

    [doc] updated help for 3.3.1
    Change-Id: Iaaee66d3bbc166fb9c5034d10605c084e91ec036
    Reviewed-on: https://review.streamsets.net/15277
    Reviewed-by: Loretta Chen <loretta@streamsets.com>

--- docs/generated/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ docs/generated/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180627115021", "oxygen-webhelp/search/index-1.js?uniqueId=20180627115021", "oxygen-webhelp/search/index-2.js?uniqueId=20180627115021", "oxygen-webhelp/search/index-3.js?uniqueId=20180627115021"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180629112457", "oxygen-webhelp/search/index-1.js?uniqueId=20180629112457", "oxygen-webhelp/search/index-2.js?uniqueId=20180629112457", "oxygen-webhelp/search/index-3.js?uniqueId=20180629112457"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
