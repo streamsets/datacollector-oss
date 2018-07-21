787a294ae200530a5bbca2e356d7428e653f5f93
Jarek Jarcec Cecho <jarcec@streamsets.com>
2018-07-20 17:55:45 -0700

    SDC-9573: SCH Registration: De-duplicate logic in SchAdmin
    Removing a lot of code duplication to make the class a bit easier to
    read and follow.
    
    Change-Id: I248d24fd7b8c314ad093647796bc0316dd31b0f0
    Reviewed-on: https://review.streamsets.net/15715
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Anthony McKeehan <anthony@streamsets.com>

--- container/src/main/java/com/streamsets/datacollector/cli/sch/SchAdmin.java
+++ container/src/main/java/com/streamsets/datacollector/cli/sch/SchAdmin.java
@@ -54,13 +54,13 @@ public class SchAdmin {
   private static final String APP_TOKEN_FILE = "application-token.txt";
   private static final String APP_TOKEN_FILE_PROP_VAL = "@application-token.txt@";
 
+  /**
+   * Enable Control Hub on this Data Collector.
+   */
   public static void enableDPM(DPMInfoJson dpmInfo, RuntimeInfo runtimeInfo, Configuration configuration) throws IOException {
     Utils.checkNotNull(dpmInfo, "DPMInfo");
 
-    String dpmBaseURL = dpmInfo.getBaseURL();
-    if (dpmBaseURL.endsWith("/")) {
-      dpmBaseURL = dpmBaseURL.substring(0, dpmBaseURL.length() - 1);
-    }
+    String dpmBaseURL = normalizeDpmBaseURL(dpmInfo.getBaseURL());
 
     // Since we support enabling/Disabling DPM, first check if token already exists for the given DPM URL.
     // If token exists skip first 3 steps
@@ -68,32 +68,11 @@ public class SchAdmin {
     String currentAppAuthToken = configuration.get(RemoteSSOService.SECURITY_SERVICE_APP_AUTH_TOKEN_CONFIG, "").trim();
     if (!currentDPMBaseURL.equals(dpmBaseURL) ||  currentAppAuthToken.length() == 0) {
       // 1. Login to DPM to get user auth token
-      Response response = null;
-      try {
-        Map<String, String> loginJson = new HashMap<>();
-        loginJson.put("userName", dpmInfo.getUserID());
-        loginJson.put("password", dpmInfo.getUserPassword());
-        response = ClientBuilder.newClient()
-            .target(dpmBaseURL + "/security/public-rest/v1/authentication/login")
-            .register(new CsrfProtectionFilter("CSRF"))
-            .request()
-            .post(Entity.json(loginJson));
-        if (response.getStatus() != Response.Status.OK.getStatusCode()) {
-          throw new RuntimeException(Utils.format("DPM Login failed, status code '{}': {}",
-              response.getStatus(),
-              response.readEntity(String.class)
-          ));
-        }
-      } finally {
-        if (response != null) {
-          response.close();
-        }
-      }
-
-      String userAuthToken = response.getHeaderString(SSOConstants.X_USER_AUTH_TOKEN);
+      String userAuthToken = retrieveUserToken(dpmBaseURL, dpmInfo.getUserID(), dpmInfo.getUserPassword());
       String appAuthToken = null;
 
       // 2. Create Data Collector application token
+      Response response = null;
       try {
         Map<String, Object> newComponentJson = new HashMap<>();
         newComponentJson.put("organization", dpmInfo.getOrganization());
@@ -125,105 +104,41 @@ public class SchAdmin {
           response.close();
         }
         // Logout from DPM
-        try {
-          response = ClientBuilder.newClient()
-              .target(dpmBaseURL + "/security/_logout")
-              .register(new CsrfProtectionFilter("CSRF"))
-              .request()
-              .header(SSOConstants.X_USER_AUTH_TOKEN, userAuthToken)
-              .cookie(SSOConstants.AUTHENTICATION_COOKIE_PREFIX + "LOGIN", userAuthToken)
-              .get();
-        } finally {
-          if (response != null) {
-            response.close();
-          }
-        }
+        logout(dpmBaseURL, userAuthToken);
       }
 
       // 3. Update App Token file
-      DataStore dataStore = new DataStore(new File(runtimeInfo.getConfigDir(), APP_TOKEN_FILE));
-      try (OutputStream os = dataStore.getOutputStream()) {
-        IOUtils.write(appAuthToken, os);
-        dataStore.commit(os);
-      } finally {
-        dataStore.release();
-      }
+      updateTokenFile(runtimeInfo, appAuthToken);
     }
 
     // 4. Update dpm.properties file
-    try {
-      FileBasedConfigurationBuilder<PropertiesConfiguration> builder =
-          new FileBasedConfigurationBuilder<>(PropertiesConfiguration.class)
-              .configure(new Parameters().properties()
-                  .setFileName(runtimeInfo.getConfigDir() + "/dpm.properties")
-                  .setThrowExceptionOnMissing(true)
-                  .setListDelimiterHandler(new DefaultListDelimiterHandler(';'))
-                  .setIncludesAllowed(false));
-      PropertiesConfiguration config = null;
-      config = builder.getConfiguration();
-
-      config.setProperty(RemoteSSOService.DPM_ENABLED, "true");
-      config.setProperty(RemoteSSOService.DPM_BASE_URL_CONFIG, dpmBaseURL);
-      config.setProperty(RemoteSSOService.SECURITY_SERVICE_APP_AUTH_TOKEN_CONFIG, APP_TOKEN_FILE_PROP_VAL);
-      if (dpmInfo.getLabels() != null && dpmInfo.getLabels().size() > 0) {
-        config.setProperty(RemoteEventHandlerTask.REMOTE_JOB_LABELS, StringUtils.join(dpmInfo.getLabels(), ','));
-      } else {
-        config.setProperty(RemoteEventHandlerTask.REMOTE_JOB_LABELS, "");
-      }
-
-      builder.save();
-    } catch (ConfigurationException e) {
-      throw new RuntimeException(Utils.format("Updating dpm.properties file failed: {}", e.getMessage()), e);
-    }
+    updateDpmProperties(runtimeInfo, dpmBaseURL, dpmInfo.getLabels(), true);
   }
 
+  /**
+   * Disable Control Hub on this Data Collector - with explicit login.
+   */
   public static void disableDPM(String username, String password, String organizationId, RuntimeInfo runtimeInfo, Configuration configuration) throws IOException {
-    String dpmBaseURL = configuration.get(RemoteSSOService.DPM_BASE_URL_CONFIG, "");
-    if (dpmBaseURL.endsWith("/")) {
-      dpmBaseURL = dpmBaseURL.substring(0, dpmBaseURL.length() - 1);
-    }
-
-    // 1. Login to DPM to get user auth token
-    Response response = null;
+    String dpmBaseURL = normalizeDpmBaseURL(configuration.get(RemoteSSOService.DPM_BASE_URL_CONFIG, ""));
+    String userToken = retrieveUserToken(dpmBaseURL, username, password);
     try {
-      Map<String, String> loginJson = new HashMap<>();
-      loginJson.put("userName", username);
-      loginJson.put("password", password);
-      response = ClientBuilder.newClient()
-          .target(dpmBaseURL + "/security/public-rest/v1/authentication/login")
-          .register(new CsrfProtectionFilter("CSRF"))
-          .request()
-          .post(Entity.json(loginJson));
-      if (response.getStatus() != Response.Status.OK.getStatusCode()) {
-        throw new RuntimeException(Utils.format("DPM Login failed, status code '{}': {}",
-            response.getStatus(),
-            response.readEntity(String.class)
-        ));
-      }
+      disableDPM(userToken, organizationId, runtimeInfo, configuration);
     } finally {
-      if (response != null) {
-        response.close();
-      }
+      logout(dpmBaseURL, userToken);
     }
-
-    String userAuthToken = response.getHeaderString(SSOConstants.X_USER_AUTH_TOKEN);
-    disableDPM(userAuthToken, organizationId, runtimeInfo, configuration);
   }
 
+  /**
+   * Disable Control Hub on this Data Collector - using existing auth token.
+   */
   public static void disableDPM(String userAuthToken, String organizationId, RuntimeInfo runtimeInfo, Configuration configuration) throws IOException {
     // check if DPM enabled
     if (!runtimeInfo.isDPMEnabled()) {
       throw new RuntimeException("disableDPM is supported only when DPM is enabled");
     }
 
-    String dpmBaseURL = configuration.get(RemoteSSOService.DPM_BASE_URL_CONFIG, "");
-    if (dpmBaseURL.endsWith("/")) {
-      dpmBaseURL = dpmBaseURL.substring(0, dpmBaseURL.length() - 1);
-    }
-
+    String dpmBaseURL = normalizeDpmBaseURL(configuration.get(RemoteSSOService.DPM_BASE_URL_CONFIG, ""));
     String componentId = runtimeInfo.getId();
-
-    // 1. User authentication token has been retrieved already
 
     // 2. Deactivate Data Collector System Component
     Response response = null;
@@ -293,15 +208,89 @@ public class SchAdmin {
     }
 
     // 5. Update App Token file
+    updateTokenFile(runtimeInfo, "");
+
+    // 4. Update dpm.properties file
+    updateDpmProperties(runtimeInfo, dpmBaseURL, null, false);
+  }
+
+  /**
+   * Normalize Control Hub URL - primarily drop training slash.
+   */
+  private static String normalizeDpmBaseURL(String url) {
+    if (url.endsWith("/")) {
+      url = url.substring(0, url.length() - 1);
+    }
+
+    return url;
+  }
+
+  /**
+   * Login user and retrieve authentication token.
+   */
+  private static String retrieveUserToken(String url, String username, String password) {
+    Response response = null;
+    try {
+      Map<String, String> loginJson = new HashMap<>();
+      loginJson.put("userName", username);
+      loginJson.put("password", password);
+      response = ClientBuilder.newClient()
+          .target(url + "/security/public-rest/v1/authentication/login")
+          .register(new CsrfProtectionFilter("CSRF"))
+          .request()
+          .post(Entity.json(loginJson));
+      if (response.getStatus() != Response.Status.OK.getStatusCode()) {
+        throw new RuntimeException(Utils.format("DPM Login failed, status code '{}': {}",
+            response.getStatus(),
+            response.readEntity(String.class)
+        ));
+      }
+    } finally {
+      if (response != null) {
+        response.close();
+      }
+    }
+
+   return response.getHeaderString(SSOConstants.X_USER_AUTH_TOKEN);
+  }
+
+  /**
+   * Logout given token.
+   */
+  private static void logout(String dpmBaseURL, String userAuthToken) {
+    Response response = null;
+    try {
+      response = ClientBuilder.newClient()
+          .target(dpmBaseURL + "/security/_logout")
+          .register(new CsrfProtectionFilter("CSRF"))
+          .request()
+          .header(SSOConstants.X_USER_AUTH_TOKEN, userAuthToken)
+          .cookie(SSOConstants.AUTHENTICATION_COOKIE_PREFIX + "LOGIN", userAuthToken)
+          .get();
+    } finally {
+      if (response != null) {
+        response.close();
+      }
+    }
+  }
+
+  /**
+   * Update token file with the SDC access token.
+   */
+  private static void updateTokenFile(RuntimeInfo runtimeInfo, String appAuthToken) throws IOException {
     DataStore dataStore = new DataStore(new File(runtimeInfo.getConfigDir(), APP_TOKEN_FILE));
     try (OutputStream os = dataStore.getOutputStream()) {
-      IOUtils.write("", os);
+      IOUtils.write(appAuthToken, os);
       dataStore.commit(os);
     } finally {
       dataStore.release();
     }
+  }
 
-    // 4. Update dpm.properties file
+  /**
+   * Update dpm.properties file with new configuration.
+   */
+  private static void updateDpmProperties(RuntimeInfo runtimeInfo, String dpmBaseURL, List<String> labels, boolean enableSch) {
     try {
       FileBasedConfigurationBuilder<PropertiesConfiguration> builder =
           new FileBasedConfigurationBuilder<>(PropertiesConfiguration.class)
@@ -312,10 +301,18 @@ public class SchAdmin {
                   .setIncludesAllowed(false));
       PropertiesConfiguration config = null;
       config = builder.getConfiguration();
-      config.setProperty(RemoteSSOService.DPM_ENABLED, "false");
+      config.setProperty(RemoteSSOService.DPM_ENABLED, Boolean.toString(enableSch));
+      config.setProperty(RemoteSSOService.DPM_BASE_URL_CONFIG, dpmBaseURL);
+      config.setProperty(RemoteSSOService.SECURITY_SERVICE_APP_AUTH_TOKEN_CONFIG, APP_TOKEN_FILE_PROP_VAL);
+      if (labels != null && labels.size() > 0) {
+        config.setProperty(RemoteEventHandlerTask.REMOTE_JOB_LABELS, StringUtils.join(labels, ','));
+      } else {
+        config.setProperty(RemoteEventHandlerTask.REMOTE_JOB_LABELS, "");
+      }
       builder.save();
     } catch (ConfigurationException e) {
       throw new RuntimeException(Utils.format("Updating dpm.properties file failed: {}", e.getMessage()), e);
     }
   }
+
 }
