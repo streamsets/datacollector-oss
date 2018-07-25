2d715adde4546d1bcf1407023707b4ffaa0f2627
Jarek Jarcec Cecho <jarcec@streamsets.com>
2018-07-25 09:38:46 -0700

    SDC-9609: some KAFKA_37 errors should stop the pipeline.
    The underlying trouble is that our dataformat library is catching
    Throwable - e.g. both "Exception" as well as "Error" classes. I don't
    think that it's right for the dataformat library to catch Errors as in
    this case, it was OutOfMemoryError that was caught and "swallowed" -
    which definitely looks wrong.
    
    Change-Id: Iaf3300440a27b5b2d5fe8b4e4de596b727be8754
    Reviewed-on: https://review.streamsets.net/15784
    Reviewed-by: Bob Plotts <bob@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- commonlib/src/main/java/com/streamsets/pipeline/lib/parser/WrapperDataParserFactory.java
+++ commonlib/src/main/java/com/streamsets/pipeline/lib/parser/WrapperDataParserFactory.java
@@ -97,7 +97,7 @@ public class WrapperDataParserFactory extends DataParserFactory {
     public Record parse() throws IOException, DataParserException {
       try {
         return CreateByRef.call( () -> dataParser.parse());
-      } catch (Throwable ex) {
+      } catch (Exception ex) {
         ExceptionUtils.throwUndeclared(normalizeException(ex));
       }
       return null; //unreacheable
@@ -107,7 +107,7 @@ public class WrapperDataParserFactory extends DataParserFactory {
     public String getOffset() throws DataParserException, IOException {
       try {
         return dataParser.getOffset();
-      } catch (Throwable ex) {
+      } catch (Exception ex) {
         ExceptionUtils.throwUndeclared(normalizeException(ex));
       }
       return null; //unreacheable
@@ -122,7 +122,7 @@ public class WrapperDataParserFactory extends DataParserFactory {
     public void close() throws IOException {
       try {
         dataParser.close();
-      } catch (Throwable ex) {
+      } catch (Exception ex) {
         ExceptionUtils.throwUndeclared(normalizeException(ex));
       }
     }
