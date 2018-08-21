cd3c40e1b34e1fa7734e9f09798a8630a97997e0
Adam Kunicki <adam@streamsets.com>
2018-08-21 09:57:17 -0700

    SDC-9841. Switch to es6 compatible minification
    Change-Id: I8654b32130e84c9542bb7499ee57b63091f72ee3
    Reviewed-on: https://review.streamsets.net/16309
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>

--- datacollector-ui/Gruntfile.js
+++ datacollector-ui/Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-ng-annotate');
