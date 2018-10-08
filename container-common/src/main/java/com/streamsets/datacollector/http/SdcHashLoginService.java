513ebcbe9c0bded25215624fd26ce1b4a178e7f7
Adam Kunicki <adam@streamsets.com>
2018-10-08 11:12:48 -0700

    SDC-10253. Upgrade Jetty to v9.4.12
    Change-Id: I63e0c6b9041d1a3d0d9d4c8ea77b459eaaa3fb99
    Reviewed-on: https://review.streamsets.net/17195
    Reviewed-by: Alejandro Abdelnur <tucu@streamsets.com>

--- container-common/src/main/java/com/streamsets/datacollector/http/SdcHashLoginService.java
+++ container-common/src/main/java/com/streamsets/datacollector/http/SdcHashLoginService.java
@@ -15,24 +15,188 @@
  */
 package com.streamsets.datacollector.http;
 
-import org.eclipse.jetty.security.HashLoginService;
+import com.streamsets.pipeline.api.impl.Utils;
+import org.eclipse.jetty.security.AbstractLoginService;
+import org.eclipse.jetty.security.PropertyUserStore;
+import org.eclipse.jetty.security.UserStore;
 import org.eclipse.jetty.server.UserIdentity;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.util.resource.Resource;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-public class SdcHashLoginService extends HashLoginService {
+public class SdcHashLoginService extends AbstractLoginService {
+  private static final Logger LOG = Log.getLogger(SdcHashLoginService.class);
 
-  SdcHashLoginService(String name, String config) {
-    super(name, config);
+  private String _config;
+  private boolean hotReload = false; // default is not to reload
+  private UserStore _userStore;
+  private boolean _userStoreAutoCreate = false;
+
+
+  /* ------------------------------------------------------------ */
+  public SdcHashLoginService()
+  {
+  }
+
+  /* ------------------------------------------------------------ */
+  public SdcHashLoginService(String name)
+  {
+    setName(name);
+  }
+
+  /* ------------------------------------------------------------ */
+  public SdcHashLoginService(String name, String config)
+  {
+    setName(name);
+    setConfig(config);
+  }
+
+  /* ------------------------------------------------------------ */
+  public String getConfig()
+  {
+    return _config;
+  }
+
+
+  /* ------------------------------------------------------------ */
+  @Deprecated
+  public Resource getConfigResource()
+  {
+    return null;
+  }
+
+  /* ------------------------------------------------------------ */
+  /**
+   * Load realm users from properties file.
+   * <p>
+   * The property file maps usernames to password specs followed by an optional comma separated list of role names.
+   * </p>
+   *
+   * @param config uri or url or path to realm properties file
+   */
+  public void setConfig(String config)
+  {
+    _config=config;
+  }
+
+  /**
+   * Is hot reload enabled on this user store
+   *
+   * @return true if hot reload was enabled before startup
+   */
+  public boolean isHotReload()
+  {
+    return hotReload;
+  }
+
+  /**
+   * Enable Hot Reload of the Property File
+   *
+   * @param enable true to enable, false to disable
+   */
+  public void setHotReload(boolean enable)
+  {
+    if (isRunning())
+    {
+      throw new IllegalStateException("Cannot set hot reload while user store is running");
+    }
+    this.hotReload = enable;
+  }
+
+  /**
+   * Configure the {@link UserStore} implementation to use.
+   * If none, for backward compat if none the {@link PropertyUserStore} will be used
+   * @param userStore the {@link UserStore} implementation to use
+   */
+  public void setUserStore(UserStore userStore)
+  {
+    Utils.checkArgument(userStore instanceof PropertyUserStore, "Only PropertyUserStore is supported.");
+    this._userStore = userStore;
+  }
+
+  /* ------------------------------------------------------------ */
+  @Override
+  protected String[] loadRoleInfo(UserPrincipal user)
+  {
+    UserIdentity id = _userStore.getUserIdentity(user.getName());
+    if (id == null)
+      return null;
+
+
+    Set<RolePrincipal> roles = id.getSubject().getPrincipals(RolePrincipal.class);
+    if (roles == null)
+      return null;
+
+    List<String> list = roles.stream()
+        .map(RolePrincipal::getName)
+        .collect(  Collectors.toList() );
+
+    return list.toArray(new String[roles.size()]);
+  }
+
+
+  /* ------------------------------------------------------------ */
+  @Override
+  protected UserPrincipal loadUserInfo(String userName)
+  {
+    UserIdentity id = _userStore.getUserIdentity(userName);
+    if (id != null)
+    {
+      return (UserPrincipal)id.getUserPrincipal();
+    }
+
+    return null;
+  }
+
+
+
+  /* ------------------------------------------------------------ */
+  /**
+   * @see org.eclipse.jetty.util.component.AbstractLifeCycle#doStart()
+   */
+  @Override
+  protected void doStart() throws Exception
+  {
+    super.doStart();
+
+    // can be null so we switch to previous behaviour using PropertyUserStore
+    if (_userStore == null)
+    {
+      if(LOG.isDebugEnabled())
+        LOG.debug("doStart: Starting new PropertyUserStore. PropertiesFile: " + _config + " hotReload: " + hotReload);
+      PropertyUserStore propertyUserStore = new PropertyUserStore();
+      propertyUserStore.setHotReload(hotReload);
+      propertyUserStore.setConfigPath(_config);
+      propertyUserStore.start();
+      _userStore = propertyUserStore;
+      _userStoreAutoCreate = true;
+    }
+  }
+
+  /* ------------------------------------------------------------ */
+  /**
+   * @see org.eclipse.jetty.util.component.AbstractLifeCycle#doStop()
+   */
+  @Override
+  protected void doStop() throws Exception
+  {
+    super.doStop();
+    if (_userStore != null && _userStoreAutoCreate)
+      _userStore.stop();
+    _userStore = null;
   }
 
   public UserIdentity getUserIdentity(String name) {
-    return _propertyUserStore.getUserIdentity(name);
+    return _userStore.getUserIdentity(name);
   }
 
   public Resource getResolvedConfigResource() throws IOException {
-    return _propertyUserStore.getConfigResource();
+    return ((PropertyUserStore)_userStore).getConfigResource();
   }
 
 }
