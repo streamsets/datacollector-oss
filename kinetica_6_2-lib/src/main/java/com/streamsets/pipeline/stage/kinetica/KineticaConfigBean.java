69b850039716dfe095ff168b4f0fa9ef3efe48ba
Mark Brooks <mark@streamsets.com>
2018-12-05 21:09:25 -0800

    SDC-10564 - Add support for Kinetica 6.2
    Kinetica broke compatibility between 6.1 and 6.2 so I'm not using the Kinetica protolib.
    Given that Kinetica's next version will be 7.0 with additional  breaking changes expected,
    I thought it best to not create another protolib for 6.2, but to instead create this one-off lib.
    
    Change-Id: I136fb146db9b51cc9ec13cdf1882aec62f5be466
    Reviewed-on: https://review.streamsets.net/c/18426
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

---
+++ kinetica_6_2-lib/src/main/java/com/streamsets/pipeline/stage/kinetica/KineticaConfigBean.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2018 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.kinetica;
+
+import com.streamsets.pipeline.api.ConfigDef;
+import com.streamsets.pipeline.api.credential.CredentialValue;
+
+import java.util.List;
+
+public class KineticaConfigBean {
+
+  // Connection Tab
+
+  @ConfigDef(
+    required = true,
+    type = ConfigDef.Type.STRING,
+    defaultValue = "http://127.0.0.1:9191",
+    label = "Kinetica URL",
+    description = "URL of the Kinetica Head Node",
+    displayPosition = 10, group = "CONNECTION"
+  )
+  public String url;
+
+  @ConfigDef(
+    required = false,
+    type = ConfigDef.Type.NUMBER,
+    defaultValue = "10000",
+    label = "Batch Size",
+    description = "Bulk Inserter batch size",
+    displayPosition = 20, group = "CONNECTION"
+  )
+  public int batchSize;
+
+  @ConfigDef(
+    required = false,
+    type = ConfigDef.Type.BOOLEAN,
+    label = "Transport Compression",
+    description = "Compress data sent to Kinetica",
+    displayPosition = 30,
+    group = "CONNECTION"
+  )
+  public boolean transportCompressionEnabled;
+
+  @ConfigDef(
+    required = false,
+    type = ConfigDef.Type.BOOLEAN,
+    label = "Disable Multihead Ingest",
+    description = "If set, all inserts wil be routed to the Kinetica Head Node. If not set, multihead ingest will be used when applicable for sharded tables",
+    displayPosition = 40,
+    group = "CONNECTION"
+  )
+  public boolean disableMultihead;
+
+  @ConfigDef(
+    required = false,
+    type = ConfigDef.Type.STRING,
+    label = "IP Regex",
+    description = "Regex expression to filter Kinetica Host names",
+    displayPosition = 50,
+    group = "CONNECTION"
+  )
+  public String ipRegex;
+
+  @ConfigDef(
+    required = false,
+    type = ConfigDef.Type.LIST,
+    label = "Custom Worker URL List",
+    description = "Worker node URLs that will override the default worker node URLs. Worker URLs must be added in order and all ranks must be included.",
+    displayPosition = 50,
+    group = "CONNECTION"
+  )
+  public List<String> customWorkerUrlList;
+
+  // Table Tab
+
+  @ConfigDef(
+    required = true,
+    type = ConfigDef.Type.STRING,
+    defaultValue = "",
+    label = "Table Name",
+    displayPosition = 10,
+    group = "TABLE"
+  )
+  public String tableName;
+
+  @ConfigDef(
+    required = true,
+    type = ConfigDef.Type.BOOLEAN,
+    label = "Update On Existing PK",
+    description = "If set, inserted rows with existing primary key will perform updates; if false, inserted rows with existing primary key will be ignored",
+    displayPosition = 20,
+    group = "TABLE"
+  )
+  public boolean updateOnExistingPk;
+
+  // Credentials Tab
+
+  @ConfigDef(
+    required = true,
+    type = ConfigDef.Type.CREDENTIAL,
+    defaultValue = "",
+    label = "Username",
+    displayPosition = 10,
+    group = "CREDENTIALS"
+  )
+  public CredentialValue username;
+
+  @ConfigDef(
+    required = true,
+    type = ConfigDef.Type.CREDENTIAL,
+    defaultValue = "",
+    label = "Password",
+    displayPosition = 20,
+    group = "CREDENTIALS"
+  )
+  public CredentialValue password;
+
+}
---
+++ kinetica_6_2-lib/src/main/java/com/streamsets/pipeline/stage/kinetica/util/KineticaBulkInserterUtils.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2018 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.kinetica.util;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.avro.generic.IndexedRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.gpudb.BulkInserter;
+import com.gpudb.GPUdb;
+import com.gpudb.GPUdbException;
+import com.gpudb.Type;
+import com.gpudb.WorkerList;
+import com.gpudb.protocol.InsertRecordsRequest;
+import com.streamsets.pipeline.stage.kinetica.KineticaConfigBean;
+
+public class KineticaBulkInserterUtils {
+
+  private static final Logger LOG = LoggerFactory.getLogger(KineticaBulkInserterUtils.class);
+
+  private GPUdb gpudb;
+  private Type type;
+  private int batchSize;
+  private String tableName;
+  private String ipRegex;
+  boolean isMultiheadIngestDisabled;
+  boolean isUpdateOnExistingPrimaryKey;
+  private List<String> customWorkerUrlList;
+
+  public KineticaBulkInserterUtils(GPUdb gpudb, Type type, KineticaConfigBean conf) {
+    this.gpudb = gpudb;
+    this.type = type;
+    this.tableName = conf.tableName;
+    this.batchSize = conf.batchSize;
+    this.ipRegex = conf.ipRegex;
+    this.isMultiheadIngestDisabled = conf.disableMultihead;
+    this.isUpdateOnExistingPrimaryKey = conf.updateOnExistingPk;
+    this.customWorkerUrlList = conf.customWorkerUrlList;
+  }
+
+  public BulkInserter<IndexedRecord> createBulkInserter() throws GPUdbException {
+
+    WorkerList workers = null;
+
+    // if multi-head-ingest is disabled, leave workers as null
+    if (isMultiheadIngestDisabled){
+      LOG.info("Kinetica multi-head ingest is disabled");
+    } else {
+      LOG.info("Kinetica multi-head ingest is enabled");
+
+      // Use a Custom Worker URL List if set
+      if(customWorkerUrlList != null && !customWorkerUrlList.isEmpty()){
+        LOG.info("Using customWorkerUrlList: {}", String.join(",", customWorkerUrlList));
+        workers = new WorkerList();
+        for (String workerUrl : customWorkerUrlList){
+          try{
+            workerUrl = workerUrl.trim();
+            if (workerUrl.length() == 0){
+              throw new GPUdbException("Error: Custom Worker URL entry cannot be an empty string");
+            }
+            URL url = new URL(workerUrl);
+            workers.add(url);
+          } catch(MalformedURLException e){
+            LOG.error("Malformed URL in customWorkerUrlList");
+            throw new GPUdbException(e.getMessage());
+          }
+        }
+      } else {
+        // see if we need to filter worker host names
+        if (ipRegex != null && ipRegex.trim().length() > 0) {
+          LOG.info("Using ipRegex: '" + ipRegex + "'");
+          Pattern pattern = Pattern.compile(ipRegex);
+          workers = new WorkerList(gpudb, pattern);
+        } else {
+          workers = new WorkerList(gpudb);
+        }
+      }
+    }
+
+    if (workers != null){
+      for (Iterator<URL> iter = workers.iterator(); iter.hasNext();) {
+        LOG.info("GPUdb BulkInserter worker: " + iter.next());
+      }
+    }
+
+    HashMap<String, String> options = new HashMap<String, String>();
+
+    if (isUpdateOnExistingPrimaryKey){
+      LOG.info("Setting update_on_existing_pk = true");
+      options.put("update_on_existing_pk", InsertRecordsRequest.Options.TRUE);
+    } else {
+      LOG.info("Setting update_on_existing_pk = false");
+      options.put("update_on_existing_pk", InsertRecordsRequest.Options.FALSE);
+    }
+
+    LOG.info("Creating BulkInserter with batch size: " + batchSize);
+
+    return new BulkInserter<IndexedRecord>(gpudb, tableName, type, batchSize, options, workers);
+  }
+}
