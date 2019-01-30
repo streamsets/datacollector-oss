9845c0ebc2b1b945532670d0fb2826a7ff52d793
Madhukar <madhu@streamsets.com>
2019-01-29 19:34:03 -0800

    SDC-10854. S3 / Internet connectivity should not be a dependency for Package Manager / External Libraries
    Change-Id: Ibee4ebb072115426573c61798164b80c3561ba7a
    Reviewed-on: https://review.streamsets.net/c/19169
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Madhukar Devaraju <madhu@streamsets.com>
    (cherry picked from commit 4c6c5bdeac96eb7fcf71b736379d23ecdc545405)
    Reviewed-on: https://review.streamsets.net/c/19189

--- container/src/main/java/com/streamsets/datacollector/stagelibrary/ClassLoaderStageLibraryTask.java
+++ container/src/main/java/com/streamsets/datacollector/stagelibrary/ClassLoaderStageLibraryTask.java
@@ -984,11 +984,9 @@ public class ClassLoaderStageLibraryTask extends AbstractTask implements StageLi
     RepositoryManifestJson repositoryManifestJson = null;
     try (Response response = ClientBuilder.newClient().target(repoUrl).request().get()) {
       InputStream inputStream = response.readEntity(InputStream.class);
-      try {
-        repositoryManifestJson = ObjectMapperFactory.get().readValue(inputStream, RepositoryManifestJson.class);
-      } catch (Exception ex) {
-        LOG.error("Failed to read repository manifest json", ex);
-      }
+      repositoryManifestJson = ObjectMapperFactory.get().readValue(inputStream, RepositoryManifestJson.class);
+    } catch (Exception ex) {
+      LOG.error("Failed to read repository manifest json", ex);
     }
     return repositoryManifestJson;
   }
@@ -997,11 +995,9 @@ public class ClassLoaderStageLibraryTask extends AbstractTask implements StageLi
     StageLibraryManifestJson stageLibManifestJson = null;
     try (Response response = ClientBuilder.newClient().target(stageLibManifestUrl).request().get()) {
       InputStream inputStream = response.readEntity(InputStream.class);
-      try {
-        stageLibManifestJson = ObjectMapperFactory.get().readValue(inputStream, StageLibraryManifestJson.class);
-      } catch (Exception ex) {
-        LOG.error("Failed to read stage-lib-manifest.json", ex);
-      }
+      stageLibManifestJson = ObjectMapperFactory.get().readValue(inputStream, StageLibraryManifestJson.class);
+    }  catch (Exception ex) {
+      LOG.error("Failed to read stage-lib-manifest.json", ex);
     }
     return stageLibManifestJson;
   }
