8e92a5d069e26ca155f70320c06519c98cc9b387
Madhukar <madhu@streamsets.com>
2019-05-19 14:30:30 -0700

    SDC-11587. Add Edge execution mode to Azure IoT and Event Hub Destination
    Change-Id: I315ab28369be4eca73070d7e5be86a878336b9f4

--- azure-lib/src/main/java/com/streamsets/pipeline/stage/destination/iothub/IotHubProducerDTarget.java
+++ azure-lib/src/main/java/com/streamsets/pipeline/stage/destination/iothub/IotHubProducerDTarget.java
@@ -17,6 +17,7 @@ package com.streamsets.pipeline.stage.destination.iothub;
 
 import com.streamsets.pipeline.api.ConfigDefBean;
 import com.streamsets.pipeline.api.ConfigGroups;
+import com.streamsets.pipeline.api.ExecutionMode;
 import com.streamsets.pipeline.api.GenerateResourceBundle;
 import com.streamsets.pipeline.api.StageDef;
 import com.streamsets.pipeline.api.Target;
@@ -28,6 +29,14 @@ import com.streamsets.pipeline.api.base.configurablestage.DTarget;
     description = "Writes data to Azure IoT Hub",
     icon = "azure-iot-hub.png",
     recordsByRef = true,
+    execution = {
+        ExecutionMode.STANDALONE,
+        ExecutionMode.CLUSTER_BATCH,
+        ExecutionMode.CLUSTER_YARN_STREAMING,
+        ExecutionMode.CLUSTER_MESOS_STREAMING,
+        ExecutionMode.EDGE,
+        ExecutionMode.EMR_BATCH
+    },
     onlineHelpRefUrl ="index.html?contextID=task_tqc_vkq_1bb"
 )
 @ConfigGroups(Groups.class)
