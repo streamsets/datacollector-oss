f1a80639b532d2b93930e653173505a3dbcb8412
Joan Sala <joan.sala@streamsets.com>
2019-05-27 20:54:57 +0200

    SDC-11672. Fix ADLS Gen1 origin icon
    Change-Id: I6cd3c50321e9033d95dc5c5285faff2423185af6

--- azure-lib/src/main/java/com/streamsets/pipeline/stage/destination/datalake/gen1/DataLakeDTarget.java
+++ azure-lib/src/main/java/com/streamsets/pipeline/stage/destination/datalake/gen1/DataLakeDTarget.java
@@ -27,7 +27,7 @@ import com.streamsets.pipeline.api.base.configurablestage.DTarget;
     version = 1,
     label = "Azure Data Lake Storage Gen1",
     description = "Reads data from Azure Data Lake Storage Gen1",
-    icon = "data-lake-store.png",
+    icon = "data-lake-store-gen1.png",
     producesEvents = true,
     onlineHelpRefUrl = "index.html?contextID=task_t13_ht5_5hb",
     beta = true
