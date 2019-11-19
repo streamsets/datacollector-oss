f040316f70df052105f459dca0520c28696885c0
jsalareixach <joan.sala@streamsets.com>
2019-11-19 12:11:06 +0100

    SDC-12949. Pipeline with Cassandra destination fails to validate after upgrade
    Change-Id: Ibc6ddf9073292989bcb067db745cb13810942834
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/27700
    Reviewed-by: Xavier Baqués <xavi@streamsets.com>
    (cherry picked from commit 69a0025cd8bf574e8dbaca5703c4a8442a0e2dec)
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/27702
    Reviewed-by: Joan Sala <joan.sala@streamsets.com>

--- cassandra-protolib/src/main/java/com/streamsets/pipeline/stage/destination/cassandra/CassandraDTarget.java
+++ cassandra-protolib/src/main/java/com/streamsets/pipeline/stage/destination/cassandra/CassandraDTarget.java
@@ -24,7 +24,7 @@ import com.streamsets.pipeline.api.base.configurablestage.DTarget;
 
 @GenerateResourceBundle
 @StageDef(
-    version = 5,
+    version = 6,
     label = "Cassandra",
     description = "Writes data to Cassandra",
     icon = "cassandra.png",
