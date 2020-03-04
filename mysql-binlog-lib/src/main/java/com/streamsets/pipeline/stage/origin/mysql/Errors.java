5af0f9d9cad3c99a785d6c2149ffc07fe4ee5470
Xavier Baques <xavi@streamsets.com>
2020-03-04 18:08:29 +0100

    SDC-13024. MySQL Binlog CDC not able to read json in proper format
    Change-Id: I4ec42353b9a5c4693d8673580f65007d264c4e8d
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/30097
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Joan Sala <joan.sala@streamsets.com>

--- mysql-binlog-lib/src/main/java/com/streamsets/pipeline/stage/origin/mysql/Errors.java
+++ mysql-binlog-lib/src/main/java/com/streamsets/pipeline/stage/origin/mysql/Errors.java
@@ -26,7 +26,9 @@ public enum Errors implements ErrorCode {
   MYSQL_004("Error processing MySql event {} at offset {}: {}"),
   MYSQL_006("MySql server error: {}"),
   MYSQL_007("Ignore tables format error: {}"),
-  MYSQL_008("Include tables format error: {}"),;
+  MYSQL_008("Include tables format error: {}"),
+  MYSQL_009("Couldn't parse JSON column value: {}"),
+  ;
   private final String msg;
 
   Errors(String msg) {
