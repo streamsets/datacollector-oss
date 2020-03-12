e8aa4d80bc88cfb1af9a45b7ae695992a97bfe80
Alejandro Abdelnur <tucu@streamsets.com>
2020-03-12 12:19:28 +0100

    SDC-13857. Retrofit FileUserGroupManager to use UsersManager
      Previous version was loading data on startup and
      not refreshing it.
    
    Change-Id: I7dd6e8525c1eaadcd2552577dd532df69970a799
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/30309
    Reviewed-by: Santhosh Kumar <Santhosh@streamsets.com>

--- container/src/main/java/com/streamsets/datacollector/main/FileUserGroupManager.java
+++ container/src/main/java/com/streamsets/datacollector/main/FileUserGroupManager.java
@@ -17,30 +17,28 @@ package com.streamsets.datacollector.main;
 
 import com.streamsets.datacollector.http.SdcHashLoginService;
 import com.streamsets.datacollector.restapi.bean.UserJson;
-import com.streamsets.pipeline.api.impl.Utils;
-import org.eclipse.jetty.security.AbstractLoginService;
+import com.streamsets.datacollector.security.usermgnt.FormRealmUsersManager;
+import com.streamsets.datacollector.security.usermgnt.User;
+import com.streamsets.datacollector.security.usermgnt.UserLineCreator;
 import org.eclipse.jetty.security.LoginService;
-import org.eclipse.jetty.server.UserIdentity;
+import org.eclipse.jetty.util.resource.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FileUserGroupManager implements UserGroupManager {
   private static final Logger LOG = LoggerFactory.getLogger(FileUserGroupManager.class);
-  private static final String GROUP_PREFIX = "group:";
-  private static final String USER_ROLE = "user";
   private SdcHashLoginService hashLoginService;
-  private Map<String, UserJson> usersMap;
-  private List<UserJson> userList;
-  private List<String> groupList;
+
+
+  FormRealmUsersManager usersManager = null;
 
   @Override
   public void setLoginService(LoginService loginService) {
@@ -50,74 +48,44 @@ public class FileUserGroupManager implements UserGroupManager {
   @Override
   public void setRoleMapping(Map<String, Set<String>> roleMapping) {}
 
+  FormRealmUsersManager getUsersManager() {
+    FormRealmUsersManager usersManager = null;
+    try {
+      Resource resource = hashLoginService.getResolvedConfigResource();
+      if (hashLoginService != null && resource.exists()) {
+        usersManager = new FormRealmUsersManager(UserLineCreator.getMD5Creator(), resource.getFile(), 0);
+      }
+    } catch (IOException ex) {
+
+    }
+    return usersManager;
+  }
+
   @Override
   public List<UserJson> getUsers() {
-    initialize();
-    return userList;
+    FormRealmUsersManager mgr = getUsersManager();
+    return (mgr == null) ? Collections.emptyList() :
+        mgr.listUsers().stream().map(u -> toUserJson(u)).collect(Collectors.toList());
   }
 
   @Override
   public List<String> getGroups() {
-    initialize();
-    return groupList;
+    FormRealmUsersManager mgr = getUsersManager();
+    return (mgr == null) ? Collections.emptyList() : mgr.listGroups();
   }
 
   @Override
   public UserJson getUser(Principal principal) {
-    initialize();
-    return usersMap.get(principal.getName());
+    FormRealmUsersManager mgr = getUsersManager();
+    return (mgr == null) ? null : toUserJson(mgr.get(principal.getName()));
   }
 
-  private synchronized void initialize() {
-    try {
-      fetchUserAndGroupList();
-    } catch (IOException e) {
-      LOG.warn(Utils.format("Exception when fetching users and groups: {}", e.getMessage()));
-    }
+  UserJson toUserJson(User user) {
+    UserJson userJson = new UserJson();
+    userJson.setName(user.getUser());
+    userJson.setGroups(user.getGroups());
+    userJson.setRoles(user.getRoles());
+    return userJson;
   }
 
-  private void fetchUserAndGroupList() throws IOException {
-    userList = new ArrayList<>();
-    usersMap = new HashMap<>();
-    groupList = new ArrayList<>();
-    groupList.add(ALL_GROUP);
-    if (hashLoginService != null) {
-      Properties properties = new Properties();
-      if ( hashLoginService.getResolvedConfigResource().exists()) {
-        properties.load(hashLoginService.getResolvedConfigResource().getInputStream());
-      }
-
-      for (Map.Entry<Object, Object> entry : properties.entrySet()) {
-        String userName = ((String)entry.getKey()).trim();
-        UserJson user = new UserJson();
-        user.setName(userName);
-        List<String> roles = new ArrayList<>();
-        List<String> groups = new ArrayList<>();
-        groups.add(ALL_GROUP);
-        UserIdentity userIdentity = hashLoginService.getUserIdentity(userName);
-        Set<Principal> principals = userIdentity.getSubject().getPrincipals();
-        for (Principal principal: principals) {
-          if (principal instanceof AbstractLoginService.RolePrincipal) {
-            if (principal.getName().startsWith(GROUP_PREFIX)) {
-              String groupName = principal.getName().replace(GROUP_PREFIX, "");
-              if (!groups.contains(groupName)) {
-                groups.add(groupName);
-              }
-              if (!groupList.contains(groupName)) {
-                groupList.add(groupName);
-              }
-            } else if (!roles.contains(principal.getName()) &&
-                !USER_ROLE.equals(principal.getName()) &&
-                !principal.getName().startsWith("email:")) {
-              roles.add(principal.getName());
-            }
-          }
-        }
-        user.setRoles(roles);
-        user.setGroups(groups);
-        userList.add(user);
-        usersMap.put(userName, user);
-      }
-    }
-  }
 }
