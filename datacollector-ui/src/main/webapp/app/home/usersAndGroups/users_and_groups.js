d3a088f9a9a2ee430cece6785dafabc33ba02db4
Olivier Giulieri <olivier@streamsets.com>
2020-03-13 16:11:30 -0700

    SDC-13681. User list page.
    Change-Id: I0af4c52bee0ae89071d0e101889c513f100ac2e5
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/30381
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Sam Meyer <sam@streamsets.com>

--- datacollector-ui/src/main/webapp/app/home/usersAndGroups/users_and_groups.js
+++ datacollector-ui/src/main/webapp/app/home/usersAndGroups/users_and_groups.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 StreamSets Inc.
+ * Copyright 2020 StreamSets Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-/**
- * Package Manager module for displaying Package Manager content.
  */
 
 angular
@@ -34,27 +31,102 @@ angular
         }
       });
   }])
-  .controller('UsersAndGroupsController', function ($scope, api) {
+  .controller('UsersAndGroupsController', function ($scope, $rootScope, api, $modal, $q) {
+    const userRolesMap = {
+      admin: 'Admin',
+      manager: 'Manager',
+      creator: 'Creator',
+      guest: 'Guest'
+    };
     angular.extend($scope, {
       fetching: false,
-      userList: []
+      userList: [],
+
+      getUserIdx: userName => $scope.userList.findIndex(user => user.id === userName),
+
+      getUserByIdx: idx => $scope.userList[idx],
+
+      getRoles: user => user.roles.map(role => userRolesMap[role]).join(', '),
+
+      showError: message => {
+        $rootScope.common.errors = [message];
+      },
+
+      openUserModal: (user, isNew) => $modal.open({
+        templateUrl: 'app/home/usersAndGroups/userDetails/user.tpl.html',
+        controller: 'UserModalController',
+        backdrop: 'static',
+        resolve: {
+          user: () => user,
+          isNew: () => isNew,
+          groupsList: () => $scope.groupList
+        }
+      }),
+
+      onAddUserClick: function() {
+        const modalInstance = this.openUserModal({}, true);
+        modalInstance.result.then(newUser => {
+          getUsersAndGroups();
+        }, function () {
+        });
+      },
+
+      onEditUserClick: function(userName) {
+        const idx = this.getUserIdx(userName);
+        if(idx > -1) {
+          const user = this.getUserByIdx(idx);
+          const modalInstance = this.openUserModal(user, false);
+          modalInstance.result.then(function(editedUser) {
+            $scope.userList[idx] = editedUser;
+            getUsersAndGroups();
+          }, function () {
+          });
+
+        } else {
+          this.showError('User not found "' + userName + '".');
+        }
+      },
+
+      onDeleteUserClick: function(userName) {
+        const modalInstance = $modal.open({
+          templateUrl: 'app/home/usersAndGroups/delete/delete.tpl.html',
+          controller: 'DeleteModalController',
+          backdrop: 'static',
+          resolve: {
+            userName: () => userName,
+          }
+        });
+        modalInstance.result.then(() => {
+          const idx = this.getUserIdx(userName);
+          $scope.userList.splice(idx, 1);
+          //getUsersAndGroups();
+        }, function () {
+        });
+      },
+
     });
 
-    var getUsers = function() {
+    var getUsersAndGroups = function() {
       $scope.fetching = true;
-      $scope.userList = [];
-      api.admin.getUsers()
-        .then(
-          function (res) {
-            $scope.fetching = false;
-            $scope.userList = _.sortBy(res.data, 'name');
-          },
-          function (res) {
-            $rootScope.common.errors = [res.data];
-          }
-        );
+      $q.all([
+        api.admin.getGroups(),
+        api.admin.getUsers2()
+      ]).then(
+        function (res) {
+          $scope.fetching = false;
+          // - list of groups
+          $scope.groupList = res[0].data;
+          // - list of users
+          $scope.userList = res[1].data.data.sort((a, b) => a.id.localeCompare(b.id));
+          $scope.userList.forEach(user => user.groups = user.groups.sort());
+        },
+        function (res) {
+          $rootScope.common.errors = [res.data];
+        }
+      );
     };
 
-    getUsers();
+    $rootScope.common.title = "Users & Groups";
+    getUsersAndGroups();
 
   });
