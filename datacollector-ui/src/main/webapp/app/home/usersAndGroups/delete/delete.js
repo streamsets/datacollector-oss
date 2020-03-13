3463ab5e1fbaaa798f32084d840c5d5a37bcefa3
Olivier Giulieri <olivier@streamsets.com>
2020-03-13 13:15:33 -0700

    SDC-13709. Delete user (w/ confirmation modal).
    Change-Id: Iabce0f7c800fbc44552bf6e02c903cc585553105
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/30375
    Reviewed-by: Sam Meyer <sam@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

---
+++ datacollector-ui/src/main/webapp/app/home/usersAndGroups/delete/delete.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Controller for Delete User Modal.
+ */
+
+angular
+  .module('dataCollectorApp.home')
+  .controller('DeleteModalController', function ($scope, $modalInstance, userName, api) {
+    angular.extend($scope, {
+      userName: userName,
+      operationInProgress: false,
+
+      yes: function() {
+        $scope.operationInProgress = true;
+        api.admin.deleteUser(userName)
+          .then(function() {
+            $modalInstance.close(userName);
+          })
+          .catch(function(res) {
+            $scope.operationInProgress = false;
+            let errorMessages = ['Error'];
+            if(res.data) {
+              if(res.data.type==='ERROR') {
+                errorMessages = res.data.messages.map(obj => obj.message);
+              } else {
+                errorMessages = [res.data];
+              }
+            } else {
+              errorMessages = [res.status + ' - ' + res.statusText];
+            }
+            $scope.common.errors = errorMessages;
+          });
+      },
+
+      no: function() {
+        $modalInstance.dismiss('cancel');
+      }
+    });
+  });
