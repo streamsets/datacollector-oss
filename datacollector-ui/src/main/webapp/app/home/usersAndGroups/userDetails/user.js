f3910e697ab8df371da130bcae9fe8b1d1095281
Olivier Giulieri <olivier@streamsets.com>
2020-03-13 15:35:58 -0700

    SDC-13682. User details modal.
    Change-Id: I6a2166d848724b919a6daf44f3903dddecb94e9e
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/30380
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Sam Meyer <sam@streamsets.com>

---
+++ datacollector-ui/src/main/webapp/app/home/usersAndGroups/userDetails/user.js
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2020 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Controller for modal to insert/edit a user.
+ */
+
+angular
+  .module('dataCollectorApp.home')
+  .controller('UserModalController', function ($scope, $modalInstance, user, isNew, api, groupsList) {
+    angular.extend($scope, {
+      operationInProgress: false,
+      groupsList: groupsList,
+      isNew: isNew,
+      userModel: {
+        groups: ['all']
+      },
+
+      init: () => {
+        if(isNew) {
+          $scope.modalTitle = 'Create User';
+        } else {
+          $scope.modalTitle = 'Edit User';
+          // - set user model
+          $scope.userModel.id = user.id;
+          $scope.userModel.email = user.email;
+          $scope.userModel.groups = user.groups;
+          user.roles.forEach(role => {
+            $scope.userModel[role] = user.roles.includes(role);
+          });
+        }
+      },
+
+      save: function() {
+        const allRoles = [ 'admin', 'manager', 'creator', 'guest'];
+        $scope.operationInProgress = true;
+
+        // - make user object for API from user model
+        var myUser = {
+          id: $scope.userModel.id,
+          email: $scope.userModel.email,
+          groups: $scope.userModel.groups,
+          roles: [],
+        };
+        allRoles.forEach(role => {
+          if($scope.userModel[role]) {
+            myUser.roles.push(role);
+          }
+        });
+
+        // - REST call to insert or update the user
+        api.admin[isNew ? 'insertUser' : 'updateUser'](myUser)
+          .then(function() {
+            $modalInstance.close(myUser);
+          })
+          .catch(function(res) {
+            $scope.operationInProgress = false;
+            $scope.common.errors = res.data ? [res.data] : [res.status + ' - ' + res.statusText];
+          });
+      },
+
+      cancel: function() {
+        $modalInstance.dismiss('cancel');
+      },
+
+    });
+
+    $scope.init();
+
+  });
