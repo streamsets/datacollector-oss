3de259257ecf3cad3c735509491dd6bbf8de3af6
santhoshkumarml <santhosh@streamsets.com>
2020-04-27 07:13:08 -0700

    SDC-14444. Telemetry: Bump Schema Version to 1.2.
    This patch bumps the schema version to 1.2. This is because we fixed a few bugs which changed the semantics of the data.
    
    Testing:
    
    1. Manual -> checked the stats json contains the version 1.2
    
    Change-Id: I8573c7a0c583fd4feb24245a476ca830678ac45c
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/31536
    Reviewed-by: Darren Lo <dlo@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- container/src/main/java/com/streamsets/datacollector/usagestats/ActiveStats.java
+++ container/src/main/java/com/streamsets/datacollector/usagestats/ActiveStats.java
@@ -36,7 +36,7 @@ import java.util.stream.Collectors;
 public class ActiveStats {
   private final Logger LOG = LoggerFactory.getLogger(ActiveStats.class);
 
-  public static final String VERSION = "1.1";
+  public static final String VERSION = "1.2";
 
   private long startTime;
   private long endTime;
