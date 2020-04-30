aa4e014ebf80ee22e02fa2233c46b7876bdd549a
Jarek Jarcec Cecho <jarcec@streamsets.com>
2020-04-30 12:35:41 -0700

    SDC-14521: JDBC Target Upgrader discards defaultOperation configuration
    It seems that the logic is that if a config defaultOperation exists,
    it's discarded which I believe is an error and should be that if it's
    empty, we default to INSERT operation.
    
    I've also added some logging that was helpful to me while doing the
    triage.
    
    Change-Id: Ife363a49182aaab38029694a90c456a1aa305364
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/31670
    Reviewed-by: Santhosh Kumar <Santhosh@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- jdbc-protolib/src/main/java/com/streamsets/pipeline/stage/destination/jdbc/JdbcTargetUpgrader.java
+++ jdbc-protolib/src/main/java/com/streamsets/pipeline/stage/destination/jdbc/JdbcTargetUpgrader.java
@@ -20,6 +20,8 @@ import com.streamsets.pipeline.api.StageException;
 import com.streamsets.pipeline.api.impl.Utils;
 import com.streamsets.pipeline.lib.jdbc.JDBCOperationType;
 import com.streamsets.pipeline.lib.jdbc.JdbcBaseUpgrader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -27,6 +29,7 @@ import java.util.List;
 
 /** {@inheritDoc} */
 public class JdbcTargetUpgrader extends JdbcBaseUpgrader{
+  private static final Logger LOG = LoggerFactory.getLogger(JdbcTargetUpgrader.class);
 
   @Override
   public List<Config> upgrade(String library, String stageName, String stageInstance, int fromVersion, int toVersion, List<Config> configs) throws StageException {
@@ -84,12 +87,14 @@ public class JdbcTargetUpgrader extends JdbcBaseUpgrader{
 
 
   private void upgradeV4toV5(List<Config> configs) {
+    LOG.info("Upgrading from 4 to 5");
     // added new max parameters feature - set to default
     configs.add(new Config("maxPrepStmtParameters", -1));
   }
 
   @SuppressWarnings("unchecked")
   private void upgradeV1toV2(List<Config> configs) {
+    LOG.info("Upgrading from 1 to 2");
     configs.add(new Config("changeLogFormat", "NONE"));
 
     Config tableNameConfig = null;
@@ -115,10 +120,12 @@ public class JdbcTargetUpgrader extends JdbcBaseUpgrader{
   }
 
   private void upgradeV2toV3(List<Config> configs) {
+    LOG.info("Upgrading from 2 to 3");
     configs.add(new Config("useMultiRowInsert", true));
   }
 
   private void upgradeV3toV4(List<Config> configs) {
+    LOG.info("Upgrading from 3 to 4");
     upgradeToConfigBeanV1(configs);
 
     Config tableNameConfig = null;
@@ -146,6 +153,7 @@ public class JdbcTargetUpgrader extends JdbcBaseUpgrader{
   }
 
   private void upgradeV5toV6(List<Config> configs) {
+    LOG.info("Upgrading from 5 to 5");
     // added default operation, unsupported operation action, and maxPrepStmtCache
     configs.add(new Config("defaultOperation", "INSERT"));
     configs.add(new Config("unsupportedAction", "DISCARD"));
@@ -153,6 +161,7 @@ public class JdbcTargetUpgrader extends JdbcBaseUpgrader{
   }
 
   private void upgradeV6toV7(List<Config> configs) {
+    LOG.info("Upgrading from 6 to 7");
     Config maxPrepStmtCache = null;
     for (Config config : configs) {
       if (config.getName().equals("maxPrepStmtCache")) {
@@ -167,10 +176,11 @@ public class JdbcTargetUpgrader extends JdbcBaseUpgrader{
   }
 
   private void upgradeV8ToV9(List<Config> configs) {
+    LOG.info("Upgrading from 8 to 9");
     List<Config> configsToRemove = new ArrayList<>();
     List<Config> configsToAdd = new ArrayList<>();
     for (Config config : configs) {
-     if (config.getName().equals("defaultOperation") && config.getValue() != null) {
+     if (config.getName().equals("defaultOperation") && config.getValue() == null) {
        configsToRemove.add(config);
        configsToAdd.add(new Config(config.getName(), JDBCOperationType.INSERT));
      }
