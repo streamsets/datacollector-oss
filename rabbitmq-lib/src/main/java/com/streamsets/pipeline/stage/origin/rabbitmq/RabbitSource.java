c6617798658437442c06ddd8532a7ebceff771fb
Martin Balzamo <martin@streamsets.com>
2020-05-09 17:22:00 +0200

    SDC-14643. RabbitMQ does not update the offset when process
    a wrong format record
    
    Change-Id: I0b73495b7df820203e979772b85dc16d23b58723
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/31989
    Reviewed-by: Hugo Prol <hugo@streamsets.com>
    (cherry picked from commit d23584ec50c501596a0f7ef55115347c009e12a0)
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/32629
    Reviewed-by: Santhosh Kumar <Santhosh@streamsets.com>

--- rabbitmq-lib/src/main/java/com/streamsets/pipeline/stage/origin/rabbitmq/RabbitSource.java
+++ rabbitmq-lib/src/main/java/com/streamsets/pipeline/stage/origin/rabbitmq/RabbitSource.java
@@ -133,8 +133,8 @@ public class RabbitSource extends BaseSource implements OffsetCommitter {
         }
         String recordId = message.getEnvelope().toString();
         List<Record> records = parseRabbitMessage(recordId, message.getBody());
+        Envelope envelope = message.getEnvelope();
         for (Record record : records){
-          Envelope envelope = message.getEnvelope();
           BasicProperties properties = message.getProperties();
           Record.Header outHeader = record.getHeader();
           if (envelope != null) {
@@ -165,8 +165,13 @@ public class RabbitSource extends BaseSource implements OffsetCommitter {
             }
           }
           batchMaker.addRecord(record);
-          nextSourceOffset = outHeader.getAttribute("deliveryTag");
           numRecords++;
+        }
+        if (envelope != null) {
+          nextSourceOffset = String.valueOf(envelope.getDeliveryTag());
+        } else {
+          nextSourceOffset = null;
+          LOG.warn("Message received with no envelope" );
         }
       } catch (InterruptedException e) {
         LOG.warn("Pipeline is shutting down.");
