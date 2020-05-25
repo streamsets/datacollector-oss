2ce94c8e9df3220184c617d60f7ea2b8af3a88ff
Sebastián Sánchez <sebas@streamsets.com>
2020-05-25 09:58:53 +0200

    SDC-14823. Increase Salesforce stages version for remote KeyStore and TrustStore option
    Change-Id: I06fe7fee832ed3cff9502d9af02395bc22422f8a
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/32317
    Reviewed-by: Hugo Prol <hugo@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- salesforce-lib/src/test/java/com/streamsets/pipeline/stage/destination/salesforce/TestForceTargetUpgrader.java
+++ salesforce-lib/src/test/java/com/streamsets/pipeline/stage/destination/salesforce/TestForceTargetUpgrader.java
@@ -13,17 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.streamsets.pipeline.stage.origin.salesforce;
+package com.streamsets.pipeline.stage.destination.salesforce;
 
 import com.streamsets.pipeline.api.Config;
-import com.streamsets.pipeline.api.StageException;
 import com.streamsets.pipeline.api.StageUpgrader;
 import com.streamsets.pipeline.config.upgrade.UpgraderTestUtils;
-import com.streamsets.pipeline.lib.salesforce.SubscriptionType;
-import com.streamsets.pipeline.lib.salesforce.TestForceInputUpgrader;
-import com.streamsets.pipeline.stage.processor.lookup.ForceLookupProcessorUpgrader;
 import com.streamsets.pipeline.upgrader.SelectorStageUpgrader;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -32,7 +27,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TestForceSourceUpgrader extends TestForceInputUpgrader {
+public class TestForceTargetUpgrader {
 
   private StageUpgrader upgrader;
   private List<Config> configs;
@@ -40,45 +35,16 @@ public class TestForceSourceUpgrader extends TestForceInputUpgrader {
 
   @Before
   public void setUp() {
-    URL yamlResource = ClassLoader.getSystemClassLoader().getResource("upgrader/ForceDSource.yaml");
-    upgrader = new SelectorStageUpgrader("stage", new ForceSourceUpgrader(), yamlResource);
+    URL yamlResource = ClassLoader.getSystemClassLoader().getResource("upgrader/ForceDTarget.yaml");
+    upgrader = new SelectorStageUpgrader("stage", null, yamlResource);
     configs = new ArrayList<>();
     context = Mockito.mock(StageUpgrader.Context.class);
   }
 
   @Test
-  public void testUpgradeV1toV2() throws StageException {
-    StageUpgrader.Context context = Mockito.mock(StageUpgrader.Context.class);
+  public void testV1ToV2Upgrade() {
     Mockito.doReturn(1).when(context).getFromVersion();
     Mockito.doReturn(2).when(context).getToVersion();
-
-    List<Config> configs = new ArrayList<>();
-
-    ForceSourceUpgrader forceSourceUpgrader = new ForceSourceUpgrader();
-    forceSourceUpgrader.upgrade(configs, context);
-
-    Assert.assertEquals(1, configs.size());
-    Config config = configs.get(0);
-    Assert.assertEquals("forceConfig.subscriptionType", config.getName());
-    Assert.assertEquals(SubscriptionType.PUSH_TOPIC, config.getValue());
-  }
-
-  @Override
-  public StageUpgrader getStageUpgrader() {
-    return new ForceSourceUpgrader();
-  }
-
-  @Test
-  public void testUpgradeV2toV3() throws StageException {
-    List<Config> configs = testUpgradeV2toV3Common();
-
-    Assert.assertEquals(0, configs.size());
-  }
-
-  @Test
-  public void testV3ToV4Upgrade() {
-    Mockito.doReturn(3).when(context).getFromVersion();
-    Mockito.doReturn(4).when(context).getToVersion();
 
     String configPrefix = "forceConfig.mutualAuth.";
     configs = upgrader.upgrade(configs, context);
