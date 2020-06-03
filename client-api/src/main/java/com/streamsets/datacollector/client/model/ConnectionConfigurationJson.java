d16cff6f4ff5a6b262575b9439136170b77c7e30
Robert Kanter <rkanter@streamsets.com>
2020-06-03 10:55:16 -0700

    SDC-14877. Add ConnectionConfigurationJson client class
    Change-Id: I6d3956cefca1f620a03b8b7e7925b85381d7ba4d
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/32508
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Pasindu Perera <pasindu@streamsets.com>

---
+++ client-api/src/main/java/com/streamsets/datacollector/client/model/ConnectionConfigurationJson.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2020 StreamSets Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.datacollector.client.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.streamsets.datacollector.client.StringUtil;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@ApiModel(description = "")
+public class ConnectionConfigurationJson {
+
+  private String library = null;
+  private String type = null;
+  private String version = null;
+  private List<ConfigConfigurationJson> configuration = new ArrayList<>();
+
+  /**
+   **/
+  @ApiModelProperty(value = "")
+  @JsonProperty("library")
+  public String getLibrary() {
+    return library;
+  }
+  public void setLibrary(String library) {
+    this.library = library;
+  }
+
+  /**
+   **/
+  @ApiModelProperty(value = "")
+  @JsonProperty("type")
+  public String getType() {
+    return type;
+  }
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  /**
+   **/
+  @ApiModelProperty(value = "")
+  @JsonProperty("version")
+  public String getVersion() {
+    return version;
+  }
+  public void setVersion(String version) {
+    this.version = version;
+  }
+
+  /**
+   **/
+  @ApiModelProperty(value = "")
+  @JsonProperty("configuration")
+  public List<ConfigConfigurationJson> getConfiguration() {
+    return configuration;
+  }
+  public void setConfiguration(List<ConfigConfigurationJson> configuration) {
+    this.configuration = configuration;
+  }
+
+
+  @Override
+  public String toString()  {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ConnectionConfigurationJson {\n");
+
+    sb.append("    library: ").append(StringUtil.toIndentedString(library)).append("\n");
+    sb.append("    type: ").append(StringUtil.toIndentedString(type)).append("\n");
+    sb.append("    version: ").append(StringUtil.toIndentedString(version)).append("\n");
+    sb.append("    configuration: ").append(StringUtil.toIndentedString(configuration)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+}
