58c4427849510cd8078613c9090f879944b9bd03
Samuel Meyer <sam@streamsets.com>
2020-07-31 08:38:28 -0700

    SDC-15246. Basic/Advanced: Add annotations to kinetica_6_0-lib library
    Change-Id: Ib44a11a105fb191c04a0ea6fc27e57b1ed6b03c0
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/33724
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

--- kinetica-protolib/src/main/java/com/streamsets/pipeline/stage/kinetica/KineticaConfigBean.java
+++ kinetica-protolib/src/main/java/com/streamsets/pipeline/stage/kinetica/KineticaConfigBean.java
@@ -25,106 +25,118 @@ public class KineticaConfigBean {
   // Connection Tab
 
   @ConfigDef(
-    required = true,
-    type = ConfigDef.Type.STRING,
-    defaultValue = "http://127.0.0.1:9191",
-    label = "Kinetica URL",
-    description = "URL of the Kinetica Head Node",
-    displayPosition = 10, group = "CONNECTION"
+      required = true,
+      type = ConfigDef.Type.STRING,
+      defaultValue = "http://127.0.0.1:9191",
+      label = "Kinetica URL",
+      description = "URL of the Kinetica Head Node",
+      displayPosition = 10,
+      displayMode = ConfigDef.DisplayMode.BASIC,
+      group = "CONNECTION"
   )
   public String url;
 
   @ConfigDef(
-    required = false,
-    type = ConfigDef.Type.NUMBER,
-    defaultValue = "10000",
-    label = "Batch Size",
-    description = "Bulk Inserter batch size",
-    displayPosition = 20, group = "CONNECTION"
+      required = false,
+      type = ConfigDef.Type.NUMBER,
+      defaultValue = "10000",
+      label = "Batch Size",
+      description = "Bulk Inserter batch size",
+      displayPosition = 20,
+      displayMode = ConfigDef.DisplayMode.ADVANCED,
+      group = "CONNECTION"
   )
   public int batchSize;
 
   @ConfigDef(
-    required = false,
-    type = ConfigDef.Type.BOOLEAN,
-    label = "Transport Compression",
-    description = "Compress data sent to Kinetica",
-    displayPosition = 30,
-    group = "CONNECTION"
+      required = false,
+      type = ConfigDef.Type.BOOLEAN,
+      label = "Transport Compression",
+      description = "Compress data sent to Kinetica",
+      displayPosition = 30,
+      displayMode = ConfigDef.DisplayMode.ADVANCED,
+      group = "CONNECTION"
   )
   public boolean transportCompressionEnabled;
 
   @ConfigDef(
-    required = false,
-    type = ConfigDef.Type.BOOLEAN,
-    label = "Disable Multihead Ingest",
-    description = "If set, all inserts wil be routed to the Kinetica Head Node. If not set, multihead ingest will be used when applicable for sharded tables",
-    displayPosition = 40,
-    group = "CONNECTION"
+      required = false,
+      type = ConfigDef.Type.BOOLEAN,
+      label = "Disable Multihead Ingest",
+      description = "If set, all inserts wil be routed to the Kinetica Head Node. If not set, multihead ingest will be used when applicable for sharded tables",
+      displayPosition = 40,
+      displayMode = ConfigDef.DisplayMode.ADVANCED,
+      group = "CONNECTION"
   )
   public boolean disableMultihead;
 
   @ConfigDef(
-    required = false,
-    type = ConfigDef.Type.STRING,
-    label = "IP Regex",
-    description = "Regex expression to filter Kinetica Host names",
-    displayPosition = 50,
-    group = "CONNECTION"
+      required = false,
+      type = ConfigDef.Type.STRING,
+      label = "IP Regex",
+      description = "Regex expression to filter Kinetica Host names",
+      displayPosition = 50,
+      displayMode = ConfigDef.DisplayMode.ADVANCED,
+      group = "CONNECTION"
   )
   public String ipRegex;
 
   @ConfigDef(
-    required = false,
-    type = ConfigDef.Type.LIST,
-    label = "Custom Worker URL List",
-    description = "Worker node URLs that will override the default worker node URLs. Worker URLs must be added in order and all ranks must be included.",
-    displayPosition = 50,
-    group = "CONNECTION"
+      required = false,
+      type = ConfigDef.Type.LIST,
+      label = "Custom Worker URL List",
+      description = "Worker node URLs that will override the default worker node URLs. Worker URLs must be added in order and all ranks must be included.",
+      displayPosition = 50,
+      displayMode = ConfigDef.DisplayMode.BASIC,
+      group = "CONNECTION"
   )
   public List<String> customWorkerUrlList;
 
   // Table Tab
 
   @ConfigDef(
-    required = true,
-    type = ConfigDef.Type.STRING,
-    defaultValue = "",
-    label = "Table Name",
-    displayPosition = 10,
-    group = "TABLE"
+      required = true,
+      type = ConfigDef.Type.STRING,
+      defaultValue = "",
+      label = "Table Name",
+      displayPosition = 10,
+      displayMode = ConfigDef.DisplayMode.BASIC,
+      group = "TABLE"
   )
   public String tableName;
 
   @ConfigDef(
-    required = true,
-    type = ConfigDef.Type.BOOLEAN,
-    label = "Update On Existing PK",
-    description = "If set, inserted rows with existing primary key will perform updates; if false, inserted rows with existing primary key will be ignored",
-    displayPosition = 20,
-    group = "TABLE"
+      required = true,
+      type = ConfigDef.Type.BOOLEAN,
+      label = "Update On Existing PK",
+      description = "If set, inserted rows with existing primary key will perform updates; if false, inserted rows with existing primary key will be ignored",
+      displayPosition = 20,
+      displayMode = ConfigDef.DisplayMode.BASIC,
+      group = "TABLE"
   )
   public boolean updateOnExistingPk;
 
   // Credentials Tab
 
   @ConfigDef(
-    required = true,
-    type = ConfigDef.Type.CREDENTIAL,
-    defaultValue = "",
-    label = "Username",
-    displayPosition = 10,
-    group = "CREDENTIALS"
+      required = true,
+      type = ConfigDef.Type.CREDENTIAL,
+      defaultValue = "",
+      label = "Username",
+      displayPosition = 10,
+      displayMode = ConfigDef.DisplayMode.BASIC,
+      group = "CREDENTIALS"
   )
   public CredentialValue username;
 
   @ConfigDef(
-    required = true,
-    type = ConfigDef.Type.CREDENTIAL,
-    defaultValue = "",
-    label = "Password",
-    displayPosition = 20,
-    group = "CREDENTIALS"
+      required = true,
+      type = ConfigDef.Type.CREDENTIAL,
+      defaultValue = "",
+      label = "Password",
+      displayPosition = 20,
+      displayMode = ConfigDef.DisplayMode.BASIC,
+      group = "CREDENTIALS"
   )
   public CredentialValue password;
 
