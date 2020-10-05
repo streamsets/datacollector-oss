511c895d6af1c871b25d8131762e81317a3e065b
Jarek Jarcec Cecho <jarcec@streamsets.com>
2020-10-05 09:55:10 -0700

    SDC-15901: Health Inspector: Add additional inspections for Memory into SDC Instance inspector
    Change-Id: Ia430f76b2f52cf9afed8420cc1965a9f500aed1f
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/35254
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Alex Sanchez Cabana <alex.sanchez@streamsets.com>

--- container/src/main/java/com/streamsets/datacollector/inspector/inspectors/SdcServerInstanceInspector.java
+++ container/src/main/java/com/streamsets/datacollector/inspector/inspectors/SdcServerInstanceInspector.java
@@ -18,10 +18,14 @@ package com.streamsets.datacollector.inspector.inspectors;
 import com.streamsets.datacollector.inspector.HealthInspector;
 import com.streamsets.datacollector.inspector.model.HealthInspectorResult;
 import com.streamsets.datacollector.inspector.model.HealthInspectorEntry;
+import org.apache.commons.io.FileUtils;
 
 import java.lang.management.ManagementFactory;
 
 public class SdcServerInstanceInspector implements HealthInspector {
+
+  private static final long GB = 1024*1024*1024;
+
   @Override
   public String getName() {
     return "Data Collector Process";
@@ -43,6 +47,31 @@ public class SdcServerInstanceInspector implements HealthInspector {
     builder.addEntry("Deadlocked threads", deadlockedThreads == 0 ? HealthInspectorEntry.Severity.GREEN : HealthInspectorEntry.Severity.RED)
         .withValue(deadlockedThreads)
         .withDescription("Number of threads that are deadlocked (waiting on a resource they can't get).");
+
+    // Various memory related information
+    Runtime runtime = Runtime.getRuntime();
+    long maxMemory = runtime.maxMemory();
+    builder.addEntry("JVM Memory Max", HealthInspectorEntry.Severity.higherIsBetter(maxMemory, 3*GB, 1*GB))
+        .withValue(FileUtils.byteCountToDisplaySize(maxMemory))
+        .withDescription("Total memory that JVM can allocate as returned by Runtime.getRuntime() - which will differ from -Xmx argument on command line.");
+
+    long freeMemory = runtime.freeMemory();
+    long memoryUtilization = (long)((double)(maxMemory - freeMemory)/maxMemory*100);
+    builder.addEntry("JVM Memory Utilization", HealthInspectorEntry.Severity.smallerIsBetter(memoryUtilization, 80, 90))
+        .withValue(memoryUtilization +" %")
+        .withDescription("How much percent of the max memory is currently allocated inside the JVM.");
+
+    com.sun.management.OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean();
+    long maxSystemMemory = bean.getTotalPhysicalMemorySize();
+    builder.addEntry("System Memory Max", HealthInspectorEntry.Severity.higherIsBetter(maxSystemMemory, 4*GB, 2*GB))
+        .withValue(FileUtils.byteCountToDisplaySize(maxSystemMemory))
+        .withDescription("Total memory available in the operating system.");
+
+    long systemMemoryUtilization = (long)(((double)maxMemory / maxSystemMemory) * 100);
+    builder.addEntry("System Memory Utilization", HealthInspectorEntry.Severity.smallerIsBetter(systemMemoryUtilization, 80, 90))
+        .withValue(systemMemoryUtilization + " %")
+        .withDescription("How much percent of OS memory can the JVM allocate.");
+
 
     return builder.build();
   }
