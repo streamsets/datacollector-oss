4b90ef6b1b9a1811a087880014e179d49cd2c6f5
Konstantin Golub <konstantin@streamsets.com>
2020-12-14 18:10:32 -0300

    SDC-16320: FTP connector fails to close input stream
    We will ignore the failure to close the input stream.
    The servers we tested return an error code which causes an exception
    to be thrown and caught here.
    
    Normally we would need to issue an ABOR command, unfortunately
    1) it's not exposed by the FTP client wrapper
    2) anyway the current implementation in the library we use doesn't
    send the command but closes a connection to the FTP server.
    
    We do not expect any negative effect here, since in reality the data
    connection is closed, the error the client throws is because the
    server returns an error code for the previous command, that is RETR.
    This is expected since we do not want to download the whole file,
    we just wanted to see if we could read the content. This is the most
    reliable way to test access permissions.
    
    The worst case scenario: server losses a data connection and doesn't
    finish (why wouldn't it?) the RETR command. In this case we wouldn't
    see any exception here but the server would ignore any subsequent
    control commands and the client may get stuck next time we try to
    open a data connection. This is just a theoretical possibility which
    wasn't yet seen in live. Since a workaround requires a lot of effort
    and it's not clear if any of the customers uses incompatible FTP
    servers, it was decided not to overcomplicate the solution but only
    to log debug messages to help to identify if we hit the case. If that
    happens we will get back to this issue to find another solutions.
    
    Change-Id: Id9ce4be74f5f640ffcc503b744037d4c05388db2
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/36579
    Reviewed-by: Hugo Prol <hugo@streamsets.com>
    Reviewed-by: Santhosh Kumar <santhosh@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- basic-lib/src/main/java/com/streamsets/pipeline/lib/remote/FTPRemoteFile.java
+++ basic-lib/src/main/java/com/streamsets/pipeline/lib/remote/FTPRemoteFile.java
@@ -43,7 +43,25 @@ public class FTPRemoteFile extends RemoteFile {
 
   @Override
   public InputStream createInputStream() throws FileSystemException {
-    return fileObject.getContent().getInputStream();
+    // If a data connection has been unexpectedly closed by the client
+    // it's possible (though we haven't found examples of it)
+    // that an FTP server maintains the last command in progress.
+    // For some FTP servers it means that no more control commands will be
+    // accepted, thus it's possible that the client gets blocked here.
+    // In case if a customer experiences any issues with the FTP connector
+    // which gets stuck, we recommend to analyse TRACE log messages.
+    // If there is no corresponding message saying that a stream
+    // has been opened, it means the client hit the issue.
+    // Unfortunately, there is no known workaround, our recommendation
+    // would be to migrate to a different FTP server.
+    // Please also notices, we do not know what FTP servers are broken
+    // thus this possibility is theoretical and this kind of behaviour
+    // of FTP servers (to maintain a command opened while losing a data
+    // connection) is not expected at all.
+    LOG.trace("Trying to open an input stream on {}", fileObject);
+    InputStream inputStream = fileObject.getContent().getInputStream();
+    LOG.trace("Opened an input stream on {}", fileObject);
+    return inputStream;
   }
 
   @Override
@@ -62,16 +80,44 @@ public class FTPRemoteFile extends RemoteFile {
 
   @Override
   public boolean isReadable() throws IOException {
-    try (InputStream ignored = createInputStream()){
+    InputStream ignored = null;
+    try {
+      ignored = createInputStream();
       LOG.trace("File is readable");
-      return true;
-    } catch (IOException e) {
+    } catch (final IOException e) {
       LOG.error("Error reading file", e);
-      if (e instanceof FileNotFoundException) {
-        // File is not found because its either deleted or don't have permissions
-        return false;
+      if (!(e instanceof FileNotFoundException)) {
+        throw e;
       }
-     throw e;
+      // File is not found because its either deleted or we don't have permissions
     }
+    if (ignored != null) {
+      try {
+        ignored.close();
+      } catch (final IOException ex) {
+        LOG.debug("Failed to close an input stream while checking if we can read a file {}", fileObject, ex);
+        // We're ignoring the failure to close the input stream.
+        // The servers we tested return an error code which causes an exception to be thrown and caught here.
+        // Normally we would need to issue an ABOR command, unfortunately
+        // 1) it's not exposed by the FTP client wrapper
+        // 2) anyway the current implementation in the library we use doesn't send the command but
+        // closes a connection to the FTP server.
+        // We do not expect any negative effect here, since in reality the data connection is
+        // closed, the error the client throws is because the server returns an error code
+        // for the previous command, that is RETR. This is expected since we do not want to
+        // download the whole file, we just wanted to see if we could read the content
+        // This is the most reliable way to test access permissions.
+        // The worst case scenario was already described: server losses a data connection
+        // and doesn't finish (why wouldn't it?) the RETR command. In this case we wouldn't see any
+        // exception here but the server would ignore any subsequent control commands and the client
+        // may get stuck next time we try to open a data connection.
+        // As explained above this is just a theoretical possibility which wasn't yet seen in live.
+        // Since a workaround requires a lot of effort and it's not clear if any of the customers uses
+        // incompatible FTP servers, it was decided not to overcomplicate the solution but only
+        // to debug messages to help to identify if we hit the case. If that happens we will
+        // get back to this issue to find another solutions.
+      }
+    }
+    return ignored != null;
   }
 }
