4e30e2d51c471da3b835104be70c751ccf8a9661
Robert Kanter <rkanter@streamsets.com>
2021-03-16 14:00:38 -0700

    SDC-16812. Wire bootstrap csp env sourcing logic to SDC Startup script
    Change-Id: I98b3199066b5b98771aa81fae3441cfff8f3b748
    Reviewed-on: https://review.streamsets.net/c/datacollector/+/40123
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Santhosh Kumar <santhosh@streamsets.com>

---
+++ dist/src/main/libexec/csp-envs/ec2-env.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Copyright 2021 StreamSets Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [[ ! -x "$(which aws)" ]]; then
+  echo "AWS CLI is needed to run EC2 Deployment Bootstrap"
+  exit -1
+fi
+
+data=$(curl --silent http://169.254.169.254/latest/user-data --connect-timeout 10)
+
+IFS=$'\n' data=($data)
+
+for value in ${data[@]}
+do
+  IFS='=' pair=($value)
+  if [ "${pair[0]}" = "STREAMSETS_DEPLOYMENT_SCH_URL" ]; then
+    export STREAMSETS_DEPLOYMENT_SCH_URL="${pair[1]}"
+  elif [ "${pair[0]}" = "STREAMSETS_DEPLOYMENT_ID" ]; then
+    export STREAMSETS_DEPLOYMENT_ID="${pair[1]}"
+  elif [ "${pair[0]}" = "STREAMSETS_DEPLOYMENT_TOKEN_PARAM_NAME" ]; then
+    region=$(curl --silent http://169.254.169.254/latest/meta-data/placement/region --connect-timeout 10)
+    export STREAMSETS_DEPLOYMENT_TOKEN=$(aws ssm get-parameter --region "$region" --name "${pair[1]}" --with-decryption --query Parameter.Value | tr -d '"')
+  fi
+done
